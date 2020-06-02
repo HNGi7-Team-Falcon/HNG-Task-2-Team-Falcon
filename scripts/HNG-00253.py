eee48256587e85dd016e07007e708f83bff1c9e3
Abdulmalik Bello <adedotunomomeji@gmail.com>
2020-06-02 20:29:38 +0000

    HNG-00253

--- scripts/HNG-00253.py
+++ scripts/HNG-00253.py
@@ -1,1 +1,1 @@
-print("Hello World,this is Bello Abdulmalik Kehinde with HNG ID HNG-00253 using python for stage 2 task")
+print("Hello World,this is Bello Abdulmalik Kehinde with HNGi7 ID HNG-00253 using python for stage 2 task")
