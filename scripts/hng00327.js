3ed88035e83b9afc782bfaabb509100738c463bd
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-06-02 18:39:32 +0100

    my script in js

---
+++ scripts/hng00327.js
@@ -0,0 +1,5 @@
+const printMessage = (firstName, lastName, id, lang) => {
+  console.log(`Hello World, this is ${firstName} ${lastName} with HNGi7 ID: ${id} using ${lang} for stage 2 task.`);
+};
+
+printMessage('Zimuzo', 'Obiechina', 'HNG-00327', 'JavaScript');
