e8b874e018949be8390c26efc19bf4dd0d735584
Bolaji <51162350+Bilijin@users.noreply.github.com>
2020-06-02 21:03:54 +0100

    updated the code

--- scripts/HNG-00998.py
+++ scripts/HNG-00998.py
@@ -4,9 +4,9 @@ hng_id = "00998"
 language ="Python"
 
 
-def printData (fName,lName,id):
-    full_name = fName+" "+ lName;
-    print("Hello World, this is "+ full_name + "with HNG ID" + hng_id + "using" + language +"for stage 2 task")
+def printData ():
+    full_name = (first_name+" "+ last_name);
+    print("Hello World, this is "+ full_name + " with HNG ID " + hng_id + " using " + language +" for stage 2 task")
     return;
 
-printData(fName=full_name,lName=last_name,id=hng_id)
+printData()
