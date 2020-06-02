f1bb458747bece34706e5cd87e42be9fb7d10fb2
abdul-lahidris <44320047+abdul-lahidris@users.noreply.github.com>
2020-06-02 20:08:35 +0100

    Initial Upload

---
+++ scripts/hng3730.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HNGi7_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, this is Abdullah Idris with HNGi7 ID HNG-3730 using C# for stage 2 task");
+        }
+    }
+}
