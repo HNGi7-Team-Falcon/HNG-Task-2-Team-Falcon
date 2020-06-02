acc92dc2c0481d039f444a50a3fe0fa8544e0631
Komolafe <akinwande.komolafe@Terra-017.local>
2020-06-03 00:03:01 +0100

    added Flush arguement

--- scripts/HNG-02876.py
+++ scripts/HNG-02876.py
@@ -3,4 +3,4 @@ HNGi7ID = '02876'
 PreferredLanguage = 'Python'
 Stage = '2'
 
-print(f"Hello World, this is {Name} with HNGi7 ID HNG-{HNGi7ID} using {PreferredLanguage} for stage {Stage}")
+print(f"Hello World, this is {Name} with HNGi7 ID HNG-{HNGi7ID} using {PreferredLanguage} for stage {Stage}", flush=True)
