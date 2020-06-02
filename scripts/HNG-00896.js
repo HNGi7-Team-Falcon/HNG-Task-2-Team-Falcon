e518228a042652760b9e004bd4220200a3c3e79e
kruzabasi <50505811+kruzabasi@users.noreply.github.com>
2020-06-02 19:12:49 +0100

    Create HNG-00896.js
    task 2 script by (HNG-00896)

---
+++ scripts/HNG-00896.js
@@ -0,0 +1,5 @@
+//a simple "hello world" script for task2 spelling out my name and hngid for hnginternship_i7 task2
+
+console.log("Hello World, this is Ukponoabasi Nnah with HNGi7 ID HNG-00896 using javascript for stage 2 task")
+
+//this script was written in javascript
