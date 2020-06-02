e72b85c2942313e46ab1de7fcb4e344429737643
Milindkrishna <40000951+Milindkrishna98@users.noreply.github.com>
2020-06-02 22:46:04 +0530

    Create hng00344.php

---
+++ scripts/hng00344.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = 'Milind krishna';
+
+$hngi7_id = 'HNG-00344';
+
+$language = 'PHP' 
+
+$concat = 'Hello world! this is '.$name.' with '.$hngi7_id.'using'.$language. 'for stage 2 task' ;
+
+echo " $concat ";
+
+?>
