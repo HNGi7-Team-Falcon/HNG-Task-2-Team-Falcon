f7827d8f98819bf5aa8e04ccefc5e2019197e4d0
olucheye <seyefree2rhyme@yahoo.com>
2020-06-02 19:59:27 +0100

    oluseyeo task 2 submission

--- scripts/hng02018.node.js
+++ scripts/hng02018.node.js
@@ -8,4 +8,4 @@ const server = http.createServer((req, res) => {
     res.status(200).json(`Hello World, this is ${fullname} with HNGi7 ID ${ID} using ${language} for stage 2 task`)
 });
 
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
