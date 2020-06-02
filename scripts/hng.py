9b80802c8b94c927518f1df9e2ec57c027503814
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-06-02 19:03:57 +0100

    HNG7 Stage 2

--- scripts/hng.py
+++
@@ -1,8 +0,0 @@
-# HNG7 TASK 2
-
-full_name = 'Tomiwa Obanla'
-hng_id = 'HNG-00056'
-language = 'python'
-
-print('Hello WOrld, this is {} with HNGi7 ID {} using {} for stage 2 task.'.format(full_name, hng_id, language))
-
