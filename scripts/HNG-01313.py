cc0cc60b4aeeb5195e4cbd2061002355b877a487
Praise <opraise139@gmail.com>
2020-06-02 23:07:32 +0100

    changed Boal to Bola

--- scripts/HNG-01313.py
+++ scripts/HNG-01313.py
@@ -1,2 +1,2 @@
-print("Hello World, this is Boal-Rotimi Praise.O with HNGi7 ID HNG-01313 using Python for Stage 2 task", flush = True)
+print("Hello World, this is Bola-Rotimi Praise.O with HNGi7 ID HNG-01313 using Python for Stage 2 task", flush = True)
 
