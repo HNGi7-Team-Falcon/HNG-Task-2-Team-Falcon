5f05fc6b96427a2e32e43e36f3ef06eea6f943e8
azeezat123 <sogbesanteniola@gmail.com>
2020-06-02 21:15:46 +0100

    added a new file

--- scripts/HNG-01551.js
+++ scripts/HNG-01551.js
@@ -2,4 +2,4 @@
 
 let fullName = 'Sogbesan Teniloluwa'
 let language = 'Javascript'
-console.log(`"Hello World, this is ${fullName} with HNGi7ID HNG-01551 using ${language} for stage 2 task"`);
+console.log(`"Hello World, this is ${fullName} with HNGi7 ID HNG-01551 using ${language} for stage 2 task"`);
