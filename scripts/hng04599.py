2ca071ca7ea0ea48aad70a86960d1d9d2c6cd5ad
ade-damola <66269309+ade-damola@users.noreply.github.com>
2020-06-02 23:22:24 +0100

    Final implementation

--- scripts/hng04599.py
+++ scripts/hng04599.py
@@ -1,6 +1,5 @@
-fullname = "Alege Adedamola"
-hng_id = "HNG-04599"
-language = "python"
+def announce(name, hng_id, language):
+    message = f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task"
+    print(message, flush=True)
 
-message = f"Hello World, this is {fullname} with HNGi7 ID {hng_id} using {language} for stage 2 task"
-print(message, flush=True)
+announce('Adedamola Alege', 'HNG-04599', 'python')
