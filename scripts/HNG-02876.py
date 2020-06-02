d12df1f8874617d2bd57866bf3224175bfd26e92
Komolafe <akinwande.komolafe@Terra-017.local>
2020-06-02 23:53:12 +0100

    added my .py file

---
+++ scripts/HNG-02876.py
@@ -0,0 +1,6 @@
+Name = 'Akinwande Komolafe'
+HNGi7ID = '02876'
+PreferredLanguage = 'Python'
+Stage = '2'
+
+print(f"Hello World, this is {Name} with HNGi7 ID HNG-{HNGi7ID} using {PreferredLanguage} for stage {Stage}")
