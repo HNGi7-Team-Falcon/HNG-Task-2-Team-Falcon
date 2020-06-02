c30edf0d90f2c75e56ac90be52519f9239e4de5c
timmyCoder <olatoyedan@gmail.com>
2020-06-02 19:13:03 +0100

    submission from intern (hng-01361)

---
+++ scripts/hng01361.kt
@@ -0,0 +1,11 @@
+fun main(){
+    secondTask()
+}
+
+private fun secondTask(){
+    val name = "Olatoye Daniel Timilehin"
+    val hngID = "HNG-01361"
+    val language = "Kotlin"
+
+    println("Hello World, this is ${name} with HNGi7 ID ${hngID} using ${language} for stage 2 task")
+}
