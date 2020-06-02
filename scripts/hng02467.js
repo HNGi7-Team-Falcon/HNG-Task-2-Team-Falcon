333274f19299fab2f95a6047e271159075d087b7
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-03 00:20:54 +0100

    Update hng02467.js

--- scripts/hng02467.js
+++ scripts/hng02467.js
@@ -1,1 +1,1 @@
-console.log('Hello World, this is Ahmed Bankole with HNGi7 ID 02467 using nodejs for stage 2 task')
+console.log('Hello World, this is Ahmed Bankole with HNGi7 ID HNG-02467 using nodejs for stage 2 task')
