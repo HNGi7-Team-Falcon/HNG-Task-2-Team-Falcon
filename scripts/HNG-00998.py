b2c6a65a7be25c16f804c6c35d68802ede19576b
Bolaji <51162350+Bilijin@users.noreply.github.com>
2020-06-02 20:52:31 +0100

    created a new python file to print out my details

---
+++ scripts/HNG-00998.py
@@ -0,0 +1,12 @@
+first_name = "Bolaji"
+last_name ="Alabi"
+hng_id = "00998"
+language ="Python"
+
+
+def printData (fName,lName,id):
+    full_name = fName+" "+ lName;
+    print("Hello World, this is "+ full_name + "with HNG ID" + hng_id + "using" + language +"for stage 2 task")
+    return;
+
+printData(fName=full_name,lName=last_name,id=hng_id)
