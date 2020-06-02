5ca741ea1edc12e725029453f7989bb5f714bda7
ayaanrizv <66327527+ayaanrizv@users.noreply.github.com>
2020-06-03 01:02:35 +0530

    Update HNG01531.py

--- scripts/HNG01531.py
+++ scripts/HNG01531.py
@@ -1,1 +1,1 @@
-print("Hello world, this is Ayaan Kamaal Rizvi HNGi7 id 01531 using python for stage 1 task")
+print("Hello world, this is Ayaan Kamaal Rizvi HNGi7 id HNG-01531 using python for stage 1 task")
