3a3c897cc920591b7b124772cb2729c6c07d1f35
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-06-02 20:25:57 +0100

    Delete redundant comment

--- scripts/hng00327/hng00327.js
+++ scripts/hng00327/hng00327.js
@@ -1,5 +1,3 @@
-// console.log(JSON.stringify('Hello World, this is Zimuzo Obiechina with HNGi7 ID: HNG-00327 using JavaScript for stage 2 task.'));
-
 document.write(
   JSON.stringify(
     'Hello World, this is Zimuzo Obiechina with HNGi7 ID: HNG-00327 using JavaScript for stage 2 task.'
