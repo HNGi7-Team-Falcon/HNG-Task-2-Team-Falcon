352fb04f176b2003593a30c7199edd75c1ae4d2a
Samuel Adekoya <46329891+damildrizzy@users.noreply.github.com>
2020-06-03 00:42:36 +0100

    Fixed a typo

--- scripts/hng-04339.py
+++ scripts/hng-04339.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Samuel Adekoya with HNG7 ID HNG-4399 using python for stage 2 task")
+print("Hello World, this is Samuel Adekoya with HNG7 ID HNG-04399 using python for stage 2 task")
