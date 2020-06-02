885800f782bae9e4fad0124ce6173e8b42c35a1c
Oluwafemi <Icekidz1997@gmail.com>
2020-06-02 21:50:34 +0100

    A script that print out my name and HNG ID

--- scripts/HNG-02048.py
+++ scripts/HNG-02048.py
@@ -1,1 +1,1 @@
-print("Hello World, this is oluwafemi sopade,with HNGi7 ID-02048 ,using python for stage 2 task", flush = True)
+print("Hello World,this is oluwafemi sopade,with HNGi7 ID-02048,using python for stage 2 task", flush = True)
