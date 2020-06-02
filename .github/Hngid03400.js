002f92b409736bc2bbd5bc0ad9478dbb28aab3df
OnolexTim <66238465+OnolexTim@users.noreply.github.com>
2020-06-02 18:38:34 +0100

    Update Hngid03400.js

--- .github/Hngid03400.js
+++ .github/Hngid03400.js
@@ -1,4 +1,4 @@
 var input =["Abasiono" +" Timothy"]
-alert(Hello world, this is + input + with + id)
+alert(Hello world, this is + input + with + id + using javascript)
 var id= ["HNGi7" + "ID" + "03400"]
 
