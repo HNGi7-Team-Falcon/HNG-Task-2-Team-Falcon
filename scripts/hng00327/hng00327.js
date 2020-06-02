fa7cf7c35ac13f855007dcecfbf724746e32c3e0
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-06-02 20:24:48 +0100

    Add html doc and stringify output

---
+++ scripts/hng00327/hng00327.js
@@ -0,0 +1,7 @@
+// console.log(JSON.stringify('Hello World, this is Zimuzo Obiechina with HNGi7 ID: HNG-00327 using JavaScript for stage 2 task.'));
+
+document.write(
+  JSON.stringify(
+    'Hello World, this is Zimuzo Obiechina with HNGi7 ID: HNG-00327 using JavaScript for stage 2 task.'
+  )
+);
