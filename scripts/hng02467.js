6bd3b60e871a90e1d5d085b3cb399094d1a09771
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-03 00:58:51 +0100

    task 2 submisson

--- scripts/hng02467.js
+++ scripts/hng02467.js
@@ -1,1 +1,14 @@
-console.log('Hello World, this is Ahmed Bankole with HNGi7 ID HNG-02467 using nodejs for stage 2 task')
+const http = require('http');
+
+const host = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World, this is bankole ahmed with HNGi7 ID HNG-02467 using Nodejs');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
