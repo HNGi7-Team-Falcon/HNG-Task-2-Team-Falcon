e55012988b194dcbc777bf0293fc0100bab35a28
Lazarus Chisom Nwankwo <lazicah@gmail.com>
2020-06-02 20:39:15 +0100

    Delete hng02943.js

--- scripts/hng02943.js
+++
@@ -1,9 +0,0 @@
-// my information
-const userData = {
-  fullName: 'Lazarus Nwankwo',
-  id: 'HNG-02943',
-  language: 'JavaScript'
-}
-
-
-console.log(`Hello World, this is ${userData.fullName} with HNGi7 ID ${userData.id} using ${userData.language} for stage 2 task`)
