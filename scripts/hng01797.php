dc81576b9f94545b211e21708c9f1fcaddece556
Ejiroghene Obiuwevbi <ejiroghene15@gmail.com>
2020-06-02 18:32:10 +0100

    Ejiroghene, Task 2 submission

--- scripts/hng01797.php
+++ scripts/hng01797.php
@@ -1,2 +1,3 @@
-
-"Hello World, this is [Ejiroghene Obiuwevbi] with HNGi7 ID [HNG-01797] using [PHP] for stage 2 task"
+<?php
+echo "Hello World, this is [Ejiroghene Obiuwevbi] with HNGi7 ID [HNG-01797] using [PHP] for stage 2 task";
+?>
