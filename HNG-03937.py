85a8203b24f5f60126bd68d4ce03ae1f1c65ce2b
AlahiraJ <63448638+AlahiraJ@users.noreply.github.com>
2020-06-03 10:46:14 +0100

    Updated script

---
+++ HNG-03937.py
@@ -0,0 +1,14 @@
+
+def print_details():
+    """
+    This function takes my Name, HNG ID and Language of choice
+    as parameters prints it out
+    """
+    name = "Alahira Jeffrey Calvin"
+    hng_id = "HNG-03937"
+    language= "Python"
+
+    print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language +" for stage 2")
+
+
+print_details()
