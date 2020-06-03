81dc8fe29a99e8fd06b574c516e6fafa36db5103
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-06-03 21:21:22 +0100

    chore:added description for intern 00809

---
+++ scripts/HNG-00809.js
@@ -0,0 +1,11 @@
+let name = "Moronfolu Olufunke";
+let email = "moronfolu.motunrayo1@gmail.com";
+let hngId  = "HNG-00809";
+let lang = "javaScript";
+
+function description (){
+    return `Hello world, this is ${name} with HNGi7 ID of ${hngId} using ${lang} for stage 2 task. ${email}.`;
+};
+console.log(description());
+
+
