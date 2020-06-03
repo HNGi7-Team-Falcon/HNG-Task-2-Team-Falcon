0945f4b5c6110b89dbefa4f10c7877af4dd78027
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2020-06-03 14:01:34 +0100

    created an intern class

--- scripts/hng00142.py
+++ scripts/hng00142.py
@@ -2,5 +2,18 @@ full_name = 'Olayinka Olasunkanmi'
 hng_id = 'HNG-00142'
 language = 'Python'
 email = 'sj.olayinka@gmail.com'
-introduction = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
-print(introduction)
+
+
+class Intern:
+    def __init__(self, name, id, language, email):
+        self.name = name
+        self.id = id
+        self.language = language
+        self.email = email
+
+    def introduction(self):
+        return f'Hello World, this is {self.name} with HNGi7 ID {self.id} using {self.language} for stage 2 task'
+
+
+Olasunkanmi = Intern(full_name, hng_id, language, email)
+print(Olasunkanmi.introduction())
