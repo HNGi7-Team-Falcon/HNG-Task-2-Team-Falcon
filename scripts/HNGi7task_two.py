4a3f628c6a899ba9466ed8f347e9c574a3aa58e4
Anna Ugwu <annakelechi@gmail.com>
2020-06-02 21:36:08 -0700

    Revert "Revert "Create HNGi7task_two.py""
    This reverts commit 9abe483dfdba61ca93ac1eec3558b4e57751d5b4.

---
+++ scripts/HNGi7task_two.py
@@ -0,0 +1,5 @@
+full_name = "Anna Kenechi"
+id_name = "Kenanna"
+language_name = "Python"
+
+print(f"Hello World, this is {full_name} with HNGi7 ID {id_name} using {language_name} for stage 2 task")
