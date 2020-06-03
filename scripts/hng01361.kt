b9e13ddb491208943825656921dd67ebdcec5e91
timmyCoder <olatoyedan@gmail.com>
2020-06-03 20:03:49 +0100

    submission for intern hng-01361

--- scripts/hng01361.kt
+++ scripts/hng01361.kt
@@ -2,5 +2,5 @@ fname = "Olatoye Daniel Timilehin"
 id = "HNG-01361"
 language = "python"
 email = "olatoyedan@gmail.com"
-fianl_result = "Hello world, this is " + fname + " with HNGi7 ID " + id + " using " + language + " for stage 2 Task " + email
-print(fianl_result)
+final_result = "Hello world, this is " + fname + " with HNGi7 ID " + id + " using " + language + " for stage 2 Task " + email
+print(final_result)
