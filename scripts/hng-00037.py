e68447c8ae0b2cc16318ce89f6c3ee963fda6e62
Philip Okiokio <philipokiokio@gmail.com>
2020-06-03 09:55:49 +0100

    This is the task that was given to interns for promotion to stage2.

---
+++ scripts/hng-00037.py
@@ -0,0 +1,13 @@
+"""This is the second task that qualifies an intern to stage 2."""
+
+def task(fullname, id, lang):
+    return f"Hello world, this is {fullname} with HNGi7 ID {id} using {lang} for stage 2 task."
+
+
+full= 'Philip Ireoluwa Okiokio'
+id= 'HNG-00037'
+lang= 'python'
+
+
+
+print(task(full,id,lang))
