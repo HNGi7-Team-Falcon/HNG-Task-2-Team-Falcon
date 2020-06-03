c0987158c61ecfa350d4ac9e584ec6ac4eb3ca61
Eleanya-David <65508991+Eleanya-David@users.noreply.github.com>
2020-06-03 13:51:42 -0700

    Delete HNG-01887.js

--- scripts/HNG-01887.js
+++
@@ -1,5 +0,0 @@
-var a ="test"
-var davidsString="Hello World, this is Eleanya-Alichi-David with HNGi7 ID HNG-01887 using Javascript for stage 2 task. davideleanyaalichi@gmail.com"
-if(a="test"){
-console.log(davidsString);
-}
