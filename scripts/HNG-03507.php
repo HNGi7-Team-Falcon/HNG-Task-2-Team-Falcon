9a7a5b31ecebdb90882fa3e549a9a75dd713e4a4
AbdullahiAzeez <52878383+AbdullahiAzeez@users.noreply.github.com>
2020-06-03 14:42:43 +0100

    updated with my email

--- scripts/HNG-03507.php
+++ scripts/HNG-03507.php
@@ -1,3 +1,3 @@
 <?php  
-echo "Hello World, this is Abdulazeez Abdullahi with HNGi7 ID HNG-03507 using php for stage 2";
+echo "Hello World, this is Abdulazeez Abdullahi with HNGi7 ID HNG-03507 using php for stage 2. abdulazeezabdullahi57@gmail.com.";
 ?>
