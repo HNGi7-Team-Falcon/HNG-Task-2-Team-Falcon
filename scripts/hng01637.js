fc5535b488ab7df50060c30cc1c4ecd98777d8b3
Adetayo <akinsanyaadetayo7@gmail.com>
2020-06-03 10:52:10 +0100

    add my script

---
+++ scripts/hng01637.js
@@ -0,0 +1,7 @@
+const myInfo = (_name, _id, _language) => {
+  let description = `Hello world, this is ${_name} with HNGi7 ID ${_id} using ${_language} for stage 2 task`;
+
+  return console.log(description);
+};
+
+myInfo('Adetayo Akinsanya', 'HNG-01637', 'JavaScript');
