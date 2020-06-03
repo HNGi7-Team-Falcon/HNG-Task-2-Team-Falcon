a21711427e149762c32a4fc2534b4a5201662e0b
omogbemeh <onosithena@gmail.com>
2020-06-03 22:52:03 +0100

    Update hng05697.js
    updated to fix error

--- scripts/hng05697.js
+++ scripts/hng05697.js
@@ -3,5 +3,5 @@ const HNG_ID = 'HNG-05697';
 const language = 'Java Script';
 const email = 'onosithena@gmail.com';
 
-console.log(`Hello World, this is ${fullName} with HNGI7 ID ${HNG_ID} using ${language} for stage 2 task. ${email}`)
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${HNG_ID} using ${language} for stage 2 task. ${email}`)
 
