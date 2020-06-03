13e74b59e1f18309fdbd8b24f701217aaea939d7
Babscode <scotteyawo@gmail.com>
2020-06-03 22:33:21 +0100

    Updated email hng04199.py

--- scripts/hng04199.py
+++ scripts/hng04199.py
@@ -4,5 +4,7 @@ fullName = 'Eyawo Okiemute Scott'
 ID = 'HNG-04199'
 # language
 language = 'python'
+# email address
+emailAddress = 'Scotteyawo@gmail.com'
 
-print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task.")
+print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task. {emailAddress}")
