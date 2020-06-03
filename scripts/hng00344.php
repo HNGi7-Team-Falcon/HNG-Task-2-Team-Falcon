9fa541c123b8140c41c2e047b37bf8da7a05d5a0
Milindkrishna <40000951+Milindkrishna98@users.noreply.github.com>
2020-06-03 17:03:36 +0530

    Update hng00344.php

--- scripts/hng00344.php
+++ scripts/hng00344.php
@@ -2,7 +2,8 @@
 $name = "Milind krishna";
 $language = "PHP";
 $HNG_id = "00344";
-$c = "Hello World! this is ". $name." with hngi7id = ". $HNG_id. " using ". $language. " for stage 2 task" ;
+$email = "milindkrishna69233@gmail.com" ;
+$c = "Hello World! this is ". $name." with hngi7id = ". $HNG_id. " using ". $language. " for stage 2 task. ". "email_id is ". $email;
 echo " $c ";
 ?>
 
