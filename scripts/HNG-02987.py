d37205534789c01bf978ee9e3a1c83a7dedf2410
Chihurumnanya Nwanevu <32125152+cnndev@users.noreply.github.com>
2020-06-03 11:14:57 +0100

    Added email to my script.

--- scripts/HNG-02987.py
+++ scripts/HNG-02987.py
@@ -1,1 +1,1 @@
-print ("Hello World, this is Chihurumnanya Nwanevu with HNGi7 ID HNG-02987 using PYTHON for stage 2 task")
+print ("Hello World, this is Chihurumnanya Nwanevu with HNGi7 ID HNG-02987 and email nwanevunkem.chihurumnanya@gmail.com using PYTHON for stage 2 task")
