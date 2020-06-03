51944b9e45a82caf2b01470715e7412758268d60
Christian Chukwu <52824325+chriscrea8@users.noreply.github.com>
2020-06-04 00:06:13 +0100

    HNG-05107.js
    Used JavaScript to output my details to the console

---
+++ scripts/HNG-05107.js
@@ -0,0 +1,5 @@
+var Fullname = 'Christian Chukwu';
+var id = 'HNG-05107';
+var language = 'JavaScript';
+
+console.log(`Hello World, this is ${Fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`);
