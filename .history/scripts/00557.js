5c798b158b4cc51f90db747dd3212b1501b7f3a9
toriola998 <66295251+toriola998@users.noreply.github.com>
2020-06-04 00:15:22 +0100

    Create 00557.js
    Create 00557.js task 2

---
+++ .history/scripts/00557.js
@@ -0,0 +1,13 @@
+const intern = {
+    "firstName": "Faidat",
+    "lastName": "Toriola",
+    "ID": "HNG-00557",
+    "language": "NodeJS"
+}
+
+console.log(intern)
+fullname = intern.firstName + " " + intern.lastName;
+id = intern.ID;
+language = intern.language;
+
+console.log("Hello World , this is " + fullname + " " + " with HNGi7 ID: " + id + " using " + language + " for stage 2 task");
