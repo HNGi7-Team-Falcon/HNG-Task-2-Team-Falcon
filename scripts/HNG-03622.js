b2f3e8677c43c3ae34ba3d2e007b43ceaf786872
Comfort Nyatsine <tinashenyati@yahoo.com>
2020-06-03 10:07:12 +0200

    HGN-03622 Script

---
+++ scripts/HNG-03622.js
@@ -0,0 +1,12 @@
+<script>
+var fullName = "Comfort Nyatsine";
+var HNGi7_ID = "HNG-03622";
+var language = "JavaScript";
+
+function myTask(){
+		let stageTwo_Task = "Hello world, this is " + fullName + " with HNGi7 ID " + HNGi7_ID + " using " + language; 
+        return stageTwo_Task;
+}
+
+console.log(myTask()) 
+</script>
