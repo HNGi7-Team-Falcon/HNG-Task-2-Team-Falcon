506a369636894bd12a15f7dfd362e7d9d7e4b9ab
FishouttawaterCodes <65810644+FishouttawaterCodes@users.noreply.github.com>
2020-06-03 19:43:53 +0100

    Create hng01712.py

---
+++ hng01712.py
@@ -0,0 +1,5 @@
+name = "Oluwaseun Olu-Young"
+id = "01712"
+language = "python"
+email = "oluwaseunoluyoung@gmail.com"
+print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task. {email}" flush=True)
