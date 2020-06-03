144abae2d7e627ca75c4598f8916022d71596f56
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-03 20:14:24 +0100

    added new submissions

--- scripts/hng-00442.py
+++ scripts/hng-00442.py
@@ -1,10 +1,1 @@
-
-Name = "Jayasree Kurmannagari"
-Id = "hng-00442"
-emailId = "kjsree1998@gmail.com"
-language = "python"
- 
-def hello():
-  return "Hello World, this is " + Name + " with HNGi7 ID " + Id + " using " + language + " for stage 2 task. " + emailId
-
-print(hello(), flush = True)
+print("Hello World, this is Jayasree Kurmannagari with HNGi7 ID HNG-00442 using python for stage 2")
