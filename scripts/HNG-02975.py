c40742bd4b06adb080dcc73ec52858e33489e01f
aveirahabeeb <aveirahabeeb@gmail.om>
2020-06-03 11:10:50 +0100

    Updated

--- scripts/HNG-02975.py
+++ scripts/HNG-02975.py
@@ -1,5 +1,3 @@
 
-print("Hello World, this is Motolani Ajibola with HNGi7 ID HNG-02975 using Python for stage 2 task")
-=======
-print("Hello World, this is Motolani Ajibola with HNGi7 ID HNG-02975 using Python for stage 2", flush = True)
 
+print("Hello World, this is Motolani Ajibola with HNGi7 ID HNG-02975 using Python for stage 2 task")
