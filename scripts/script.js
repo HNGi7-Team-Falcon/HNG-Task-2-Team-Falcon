b8c764bf3e20005af7945249b9e785d89fd32fbb
Ighogboja Odiri <odizeeplatform@gmail.com>
2020-06-03 01:59:33 +0100

    Odiri_Ighogboja Stage_2 Commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function odiri () {
+       
+       var hello = "Hello World, this is Ighogboja Odiri with HNGi7 ID: HNG-01021 using JavaScript for stage 2 task."
+       return hello;
+
+   } 
+          
+document.write(odiri());
