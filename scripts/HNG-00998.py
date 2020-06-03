400e5ccf27c4055e87c42c509cdecb977644359e
Bolaji <51162350+Bilijin@users.noreply.github.com>
2020-06-03 09:15:19 +0100

    added "i7" to the HNG ID part of the output

--- scripts/HNG-00998.py
+++ scripts/HNG-00998.py
@@ -7,7 +7,7 @@ flush = true;
 
 def printData ():
     full_name = (first_name+" "+ last_name);
-    print("Hello World, this is "+ full_name + " with HNG ID " + hng_id + " using " + language +" for stage 2 task")
+    print("Hello World, this is "+ full_name + " with HNGi7 ID " + hng_id + " using " + language +" for stage 2 task")
     return;
 
 printData()
