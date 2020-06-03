a605dfe327f9e624f7b1795ec6b13448dead1f32
I F E DI L I ⚡ <OnyegbuIfedili@gmail.com>
2020-06-03 21:03:10 +0100

    Update HNG-02430.js

--- scripts/HNG-02430.js
+++ scripts/HNG-02430.js
@@ -1,6 +1,6 @@
-const name = "Onyegbu-Ifedili";
+const name = "Onyegbu Ifedili";
 const email = "onyegbuifedili@gmail.com";
-const HNGid = "HHG-02430";
+const HNGid = "HNG-02430";
 
 let outputString = () => {
   let str = `Hello World, this is ${name} with HNGi7 ID ${HNGid} using Javascript for stage 2 task. ${email}`
