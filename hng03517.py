e8b1aae84cd3b1b8949e8354e99160f60ade529e
musharaf <sarafaolalekan7@gmail.com>
2020-06-03 12:10:58 +0100

    editted the script to contan email address

--- hng03517.py
+++ hng03517.py
@@ -1,2 +1,9 @@
-
-print("Hello World, this is Olalekan Sarafa  with HNGi7ID HNG-03517 using Python for stage 2 task")
+def printInfo(my_name, my_id, my_language, my_email_address):
+     print "Hello World ! \n"
+     print "My Name is : ", my_name
+     print "My Id is:", my_id
+     print "the Language used for the task is: ", my_language
+     print "My email Addressis :", my_email_address
+     return;
+     
+printInfo('Olalekan Sarafa','HNG-03517', 'Python', 'Sarafaolalekan7@gmail.com')
