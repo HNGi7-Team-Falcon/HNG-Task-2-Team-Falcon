9a00b3376694dabdbcd033b83f6bc2f000a70459
meduna femi <femimeduna@gmail.com>
2020-06-03 14:46:32 +0100

    task 2 written in javascript

---
+++ scripts/HNG-01155.js
@@ -0,0 +1,10 @@
+function introduction() {
+  let myName = "Meduna Oluwafemi";
+  let id = "HNG-01155";
+  let email = "femimeduna@gmail.com";
+  let language = "Javascript";
+  let intro = `Hello World this is ${myName} with HNGI7-ID ${id} and email ${email} using ${language} for stage 2 task.`;
+
+  return intro;
+}
+console.log(introduction());
