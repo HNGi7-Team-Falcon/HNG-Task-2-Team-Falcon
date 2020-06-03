1dc3026045a74c4288dd5001fd9e16b91c39894f
Abiodun Olanlokun <34736423+ah3eyy@users.noreply.github.com>
2020-06-03 23:54:26 +0100

    HGN-Task 2 script

---
+++ scripts/HNG-01918.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$name = 'Abiodun Olanlokun Bakinde';
+
+$hng_id = 'HNG-01918';
+
+$language = 'PHP';
+
+$email = 'abiodunolanlokunbakinde@gmail.com';
+
+echo("Hello word, this is " . $name . " with HNGi7 ID " . $hng_id . " using " . $language . " fir stage 2 task. " . $email);
