54c2ee27b1f81abeca5ed1b7a74abdfa99c115d4
Oluwatobi  Adeyemi <46340386+DevTobii@users.noreply.github.com>
2020-06-03 14:33:55 +0100

    Updated my HNG task(DevTobi)

--- scripts/HNG-00869.py
+++ scripts/HNG-00869.py
@@ -1,3 +1,6 @@
+#HNG Task-2 for team falcon
+#DevTobi's HNG Details
+
 Hng_Details = input(
     "Hello world this is Adeyemi Oluwatobi with HNGi7 ID of HNG-00869, using python language for stage 2 task. my email is tobinuel155@gmail.com ")
 
