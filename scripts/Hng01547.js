513d7f852e56c48cc6c62d1cd72624c1d534337d
Shubham Thakre <thakres955@.com>
2020-06-03 23:31:27 +0530

    task2 updated

---
+++ scripts/Hng01547.js
@@ -0,0 +1,23 @@
+function Hng_task2() {
+  const name = "Shubham Thakre";
+  const id = "HNG-01547";
+  const language = "JavaScript";
+  const Email = "thakres955@gmail.com";
+  console.log(
+    "Hello World, this is" +
+      " " +
+      name +
+      " " +
+      "with HNGi7_ID:" +
+      id +
+      " " +
+      "using" +
+      " " +
+      language +
+      " " +
+      "for stage 2 task." +
+      " " +
+      Email
+  );
+}
+Hng_task2();
