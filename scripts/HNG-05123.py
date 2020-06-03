b0baa99f5a828733c242bf61b7b65a1a63471e2d
Temitope <45837388+Blazskills@users.noreply.github.com>
2020-06-03 13:07:59 +0100

    Stage 2 completed with email update (#296)
    * Stage 2 task updated with email
    
    * Stage 2 task updated with email

--- scripts/HNG-05123.py
+++ scripts/HNG-05123.py
@@ -1,1 +1,9 @@
-print('Hello world, this is Temitope Ilesanmi with HNGi7 ID HNG-05123 using Python for stage 2 task')
+name = 'Temitope Ilesanmi'
+hngi7Id = 'HNG-05123'
+language = 'Python'
+email = 'ilesanmiisaac@gmail.com'
+
+print(
+    f'Hello world, this is {name} with HNGi7 ID {hngi7Id} using {language} for stage 2 task. {email}'
+    )
+    
