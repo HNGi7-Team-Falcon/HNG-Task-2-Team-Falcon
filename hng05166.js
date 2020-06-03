7d63cac414be8f3167f3a28d03c2495468426b8c
susanspecs <49983795+susanspecs@users.noreply.github.com>
2020-06-03 20:39:51 +0100

    I created a file with the name "hng05166.js"
    I committed a file with my basic information as an intern in HNG

---
+++ hng05166.js
@@ -0,0 +1,6 @@
+let name = "Ayomide Oso Susan";
+let ID = "HNG-05166";
+let language = "JavaScript";
+let email = "ayomideosho13@gmail.com";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + ID +  " using " + language + " for stage 2 task. " + email);
