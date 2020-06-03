378ef18a2214a5cb02d3ab6379d218fb83392e73
Comfort Nyatsine <tinashenyati@yahoo.com>
2020-06-03 10:37:48 +0200

    adding email address

--- scripts/HNG-03622.js
+++ scripts/HNG-03622.js
@@ -1,10 +1,11 @@
 <script>
 var fullName = "Comfort Nyatsine";
 var HNGi7_ID = "HNG-03622";
+var email = "comfynyatsine@gmail.com"
 var language = "JavaScript";
 
 function myTask(){
-		let stageTwo_Task = "Hello world, this is " + fullName + " with HNGi7 ID " + HNGi7_ID + " using " + language; 
+		let stageTwo_Task = "Hello world, this is " + fullName + " with HNGi7 ID " + HNGi7_ID + " using " + language + " for stage 2 task"; 
         return stageTwo_Task;
 }
 
