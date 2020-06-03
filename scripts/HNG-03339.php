6339c0402096db32023af3c0e2c97064b5b641fa
MeAwadh <awadhsalim470@gmail.com>
2020-06-03 23:25:07 +0300

    Update HNG-03339.php

--- scripts/HNG-03339.php
+++ scripts/HNG-03339.php
@@ -5,7 +5,7 @@ $ID = "HNG-03339";
 $language = "PHP";
 $email = "awadhsalim470@gmail.com";
 
-$script = "Hello World, this is ".$full_name." with HNGi7 ID ".$ID."  using ".$language." for stage 2 task ".$email;
+$script = "Hello World, this is ".$full_name." with HNGi7 ID ".$ID." using ".$language." for stage 2 task ".$email;
 
 echo $script;
 ?>
