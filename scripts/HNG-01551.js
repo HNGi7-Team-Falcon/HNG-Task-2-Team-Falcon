ca3dd1f58128d4abbd5747e1536c48cb64c3153e
azeezat123 <sogbesanteniola@gmail.com>
2020-06-03 21:25:00 +0100

    added new file

--- scripts/HNG-01551.js
+++ scripts/HNG-01551.js
@@ -3,4 +3,4 @@
 let fullName = 'Sogbesan Teniloluwa';
 let myLanguage = 'Javascript';
 let emailAdress = 'sogbesanteniola@gmail.com';
-console.log(`"Hello World, this is ${fullName} with HNGi7 ID HNG-01551 using ${myLanguage} for stage 2 task. ${emailAdress}"`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID HNG-01551 using ${myLanguage} for stage 2 task. ${emailAdress}`);
