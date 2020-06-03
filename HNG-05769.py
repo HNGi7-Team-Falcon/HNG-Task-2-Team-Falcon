c3aa8a51328ca421e26ab7e4f2e9ba56cd9ea98d
Dapo Anjorin <femidpo3@gmail.com>
2020-06-03 03:06:58 +0100

    Delete HNG-05769.py (#161)
    "deleted script in wrong folder

--- HNG-05769.py
+++
@@ -1,5 +0,0 @@
-fullName = "Adedapo Anjorin"
-Id = "HNG-05769"
-language = "Python"
-
-print("Hello World, this is " + fullName + " with HNGi7 ID " + Id + " using " + language + " for stage 2 task")
