e8866b4e4af13fc52dd35e490655f109551d9f5b
Bolaji <51162350+Bilijin@users.noreply.github.com>
2020-06-03 22:07:35 +0100

    Updated flush

--- scripts/HNG-00998.py
+++ scripts/HNG-00998.py
@@ -7,7 +7,7 @@ email = "alabibolaji78@gmail.com"
 
 def printData ():
     full_name = (first_name+" "+ last_name);
-    print("Hello World, this is "+ full_name + " with HNGi7 ID " + hng_id + " using " + language +" for stage 2 task." +email, flush=true)
+    print("Hello World, this is "+ full_name + " with HNGi7 ID " + hng_id + " using " + language +" for stage 2 task." +email, flush=True)
     return;
 
 printData()
