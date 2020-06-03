658ca1450aaf2e5f7ba6ca106dbd001ad8412549
stefanpgr <stefanpongrz@gmail.com>
2020-06-03 19:42:51 +0100

    fixed submission

--- scripts/HNG-05368.js
+++ scripts/HNG-05368.js
@@ -2,7 +2,6 @@ const name = "Daniel Agoziem"
 const hngID = "HNG-05368"
 const message = `Hello World, this is ${name} with HNGi7 ID ${hngID} using JavaScript for stage 2 task. stefanpongrz@gmail.com`
 
-
 const printOut = () => {
     return message
 }
