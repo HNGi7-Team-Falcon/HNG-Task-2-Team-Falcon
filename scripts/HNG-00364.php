8cf19302c25ac492d12063a5adc533a3d241d680
Sajjad A Khalid <44890673+Sajj96@users.noreply.github.com>
2020-06-04 00:23:33 +0300

    Update HNG-00364

--- scripts/HNG-00364.php
+++ scripts/HNG-00364.php
@@ -1,16 +1,8 @@
 <?php
-    ob_start();
-
-    function myDetails()
-    {
         $name = "Sajjad Khalid Abubakari";
         $id = "HNG-00364";
-        $language = "Php";
+        $language = "PHP";
         $email = "captainsajjad4@gmail.com";
-        $output = "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2. ".$email."";
-        echo json_encode($output);
-    }
-
-    myDetails();
-    ob_flush();
-    
+        $output = "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task. ".$email."";
+        echo $output;
+?>
