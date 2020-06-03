8067377fe9fab888ad0084b72b0f8805421bc17e
Alajiki Temilola <alajikitemilola@gmail.com>
2020-06-03 11:16:28 +0100

    Update HNG00444.py with email
    Update HNG00444.py with email

--- scripts/HNG00444.php
+++ scripts/HNG00444.php
@@ -4,6 +4,6 @@ $id = "HNG-00444";
 $language = "PHP";
 $email = "alajikitemilola@gmail.com";
 
-echo "Hello World, this is " .$name.  " with HNGi7 ID ".$id. " using " .$language. " for stage 2 task";
+echo "Hello World, this is " .$name.  " with HNGi7 ID ".$id. " using " .$language. " for stage 2 task. " .$email ;
 
 ?>
