c6fd87a72df22dcdc384c89167b8f4d6d26cb430
Praise <opraise139@gmail.com>
2020-06-03 23:58:05 +0100

    changed S to s

--- scripts/HNG-01313.py
+++ scripts/HNG-01313.py
@@ -3,7 +3,7 @@
 # CREATED BY ROTIMI
 ################################
 
-my_details = "Hello World, this is {full_name} with HNGi7 ID {id} using {language} for Stage 2 task. {email}".format(
+my_details = "Hello World, this is {full_name} with HNGi7 ID {id} using {language} for stage 2 task. {email}".format(
 	full_name = 'Bola-Rotimi Praise.O',
 	id = 'HNG-01313',
 	language = 'Python',
