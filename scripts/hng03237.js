6827150965bf7500c75ac62ceb359ed4a2a9d993
kiran_mate <54542660+kr0179@users.noreply.github.com>
2020-06-03 16:59:09 +0530

    hng03237.js
    email added

--- scripts/hng03237.js
+++ scripts/hng03237.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Kiran Mate with HNGi7 ID [HNG-03237] using [javascript] for stage 2 task");
+console.log("Hello World, this is Kiran Mate with HNGi7 ID HNG-03237 using Javascript for stage 2 task. kiranmate12161@gmail.com");
