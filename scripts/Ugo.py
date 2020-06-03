d1f59fb4fee87baa856ff95479157c4565d7b46b
kelrob <test>
2020-06-03 17:01:11 +0100

    New Scripts Added

--- scripts/Ugo.py
+++
@@ -1,7 +0,0 @@
-full_name = "Ugochukwu Umeh"
-HNG_ID = "HNG-03398"
-email = "uumeh428@gmail.com"
-lang = "Python"
-
-output = f"Hello World, this is {full_name} with HNGi7 ID {HNG_ID} using {lang} for stage 2 task. {email}"
-print(output)
