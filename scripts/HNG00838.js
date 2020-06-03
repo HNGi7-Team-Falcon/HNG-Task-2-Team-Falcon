28a8924063514428b71d6d2567ebf7b5e05f3255
Simeon Fadahunsi <adedayosimeon@gmail.com>
2020-06-03 10:59:05 +0100

    output format updated

--- scripts/HNG00838.js
+++ scripts/HNG00838.js
@@ -1,3 +1,13 @@
-/** Introduction */
 
-document.write("Hello World, this is Simeon Fadahunsi with HNGi7 ID HNG-00838 using Javascript for stage 2 task");
+
+/** Variable Declaration */
+
+var name = "Simeon Fadahunsi";
+var id = "HNG-00838";
+var email = "adedayosimeon@gmail.com";
+var language = "JavaScript";
+
+
+/** Console Output */
+
+console.log("Hello World, this is " + name + " with HNGi7 ID "+ id + " using " + language + " for stage 2 task. " +email);
