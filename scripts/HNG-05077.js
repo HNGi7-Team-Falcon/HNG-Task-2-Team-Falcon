452673b4a2a51f43910a2f6305a3e5e2aa65e21f
Jezeh Priesten <jepriesten@gmail.com>
2020-06-03 21:39:14 +0100

    Fixed issues with my print out

--- scripts/HNG-05077.js
+++ scripts/HNG-05077.js
@@ -3,6 +3,10 @@ const email = "jepriesten@gmail.com";
 const HNGi7_ID = "HNG-05077";
 const language = "javascript";
 
-console.log(
-  `Hello World, this is ${name} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2. ${email}`
-);
+function detailsPrintOut() {
+  console.log(
+    `Hello World, this is ${name} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2. ${email}`
+  );
+}
+
+detailsPrintOut();
