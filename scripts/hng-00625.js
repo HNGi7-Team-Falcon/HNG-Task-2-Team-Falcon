5f7c14aabba7d4a2fdd3e2a50c082311be1f8995
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2020-06-03 15:17:06 +0100

    Update hng-00625.js (#239)
    Update email to stage 2 task

--- scripts/hng-00625.js
+++ scripts/hng-00625.js
@@ -6,8 +6,8 @@
 */
 
 function intro() {
-  let introduction = "Hello World, this is Adeola Adediran with HNGi7 ID HNG-00625 using javascript for stage 2 task";
-  return introduction;
+  let output = "Hello World, this is Adeola Adediran with HNGi7 ID HNG-00625 using Javascript for stage 2 task. adexdsamson@gmail.com";
+  return output;
 };
 
 intro();
