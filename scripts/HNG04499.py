c90621f7e93fd7de700618e4c9fee2fd8a032451
alishaJ-tech <66273195+alishaJ-tech@users.noreply.github.com>
2020-06-03 13:42:48 +0100

    Create HNG04499.py

---
+++ scripts/HNG04499.py
@@ -0,0 +1,15 @@
+my_dict = {
+   "name" : "Juma Aisha"
+   "hng_id" : "04499"
+   "language" : "Python"
+   "email" : "alishadesua1@gmail.com"
+   "status" : "Pass"
+}
+d1 = my_dict.get("name", " ")
+d2 = my_dict.get("hng_id", " ")
+d3 = my_dict.get("language", " ")
+d4 = my_dict.get("email", " ")
+d5 = my_dict.get("status", " ")
+If "name" and "hng_id" and "language" and "email" and "status" in my_dict:
+   print("Hello world, this is " , d1, "with HNGi7 ID " , d2, "using " , d3, "for Stage 2 task. My mail is " , d4, "and status" , d5 )
+   
