8ea324f833cbcc8a724d65bc487576127223ccae
Oluwafemi <Icekidz1997@gmail.com>
2020-06-03 21:07:13 +0100

    fixed error

--- scripts/HNG-02048.py
+++ scripts/HNG-02048.py
@@ -1,8 +1,8 @@
 name ="oluwafemi sopade"
-ID = "02048"
+ID = "HNG-02048"
 Email= "icekidz1997@gmail.com"
 language = "python"
 
 
 
-print(f"Hello World,this is {name} with HNGi7 ID {ID} using {language} for stage 2 task email- {Email}", flush = True)
+print(f"Hello World,this is {name} with HNGi7 ID {ID} using {language} for stage 2 task. email- {Email}", flush = True)
