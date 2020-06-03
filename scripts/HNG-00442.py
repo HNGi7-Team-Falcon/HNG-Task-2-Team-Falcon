b0eb9cd168ccc1066d36f61dc1e4f9df8691b2f7
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-03 12:43:52 +0100

    added

--- scripts/HNG-00442.py
+++ scripts/HNG-00442.py
@@ -1,1 +1,1 @@
-print("Hello World, this is Taonashe Trevor Madziva with HNGi7 ID HNG-00240 using python for stage 2")
+print("Hello World, this is Jayasree Kurmannagari with HNGi7 ID hng-00442 using Python for stage 2 task", flush=True)
