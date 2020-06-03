fd80129f1eb0f6b527e88249f3f5c704781eda88
孙名扬 <sunmingyang@sunmingyangdeMacBook-Pro.local>
2020-06-03 10:59:05 +0100

    update email

--- scripts/00353/src/App.js
+++ scripts/00353/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-            Hello, World, this is SUN Mingyang with HNGi7 ID[HNG-00353] using react.js for stage 2 task.
+            Hello World, this is SUNMingynag with HNGi7 ID HNG-00353 and email sunmi@tcd.ie using react.js for stage 2 task".
         </p>
       </header>
     </div>
