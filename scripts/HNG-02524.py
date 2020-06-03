2346b975338c7e54cbe586d34f202672f4669f43
Abdulmalik Giwa <giwa.omotola@gmail.com>
2020-06-03 18:14:20 +0100

    Create HNG-02524.py
    HNG Internship stage 1 task

---
+++ scripts/HNG-02524.py
@@ -0,0 +1,4 @@
+full_name = 'Abdulmalik Giwa'
+HNG_ID = 'HNG-02524'
+language = 'Python'
+print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task'.format(full_name,HNG_ID,language))
