10148025bbed2854a27f7d9944eb69ad76131c69
odizee <66241926+odizee@users.noreply.github.com>
2020-06-03 12:08:50 +0100

    Ighogboja_Odiri Stage_2 Commit

--- scripts/HNG-01021.js
+++ scripts/HNG-01021.js
@@ -1,7 +1,7 @@
 function odiri () {
        
-       var hello = "Hello World, this is Ighogboja Odiri with HNGi7 ID HNG-01021 and email odizeeplatform@gmail.com using JavaScript for stage 2 task."
-       return hello;
+       var helloWorld = "Hello World, this is Ighogboja Odiri with HNGi7 ID HNG-01021 and email odizeeplatform@gmail.com using JavaScript for stage 2 task."
+       return helloWorld;
 
    } 
           
