6adb3c211f566539908dbbb861759419aafac9a3
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-06-03 21:33:46 +0100

    chore: description for intern 00809

--- scripts/HNG-00809.js
+++ scripts/HNG-00809.js
@@ -3,7 +3,7 @@ let email = "moronfolu.motunrayo1@gmail.com";
 let hngId  = "HNG-00809";
 let lang = "javaScript";
 
-function description (){
+function description(){
     return `Hello world, this is ${name} with HNGi7 ID of ${hngId} using ${lang} for stage 2 task. ${email}.`;
 };
 console.log(description());
