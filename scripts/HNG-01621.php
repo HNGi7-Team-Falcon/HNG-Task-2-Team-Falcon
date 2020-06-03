4687485eb9da57007e50abbddda2bd2dfed0af91
Sophie Obomighie <sophiecodes@gmail.com>
2020-06-03 20:33:14 +0100

    Update HNG-01621.php

--- scripts/HNG-01621.php
+++ scripts/HNG-01621.php
@@ -1,9 +1,9 @@
 <?php
-$Full_Name = "Sophie Obomighie";
+$My_Name = "Sophie Obomighie";
 $Id = "HNG-01621";
 $language = "php";
 
-$script = "Hello World, this is ".$Full_Name." with HNGi7 ID ".$Id."  using ".$language." for stage 2 task";
+$script = "Hello World, this is {$My_Name} with HNGi7 ID {$Id}  using {$language} for stage 2 task";
 
 echo $script;
 ?>
