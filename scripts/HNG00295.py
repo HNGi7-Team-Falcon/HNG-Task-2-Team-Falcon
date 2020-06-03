fe7a585f2a8aa7e1258f7a1410bf203e145cd869
Ifeoluwa <64071631+IfeAkins@users.noreply.github.com>
2020-06-03 16:21:57 +0100

    Updated HNG-00295
    Task for stage 2, with my email

--- scripts/HNG00295.py
+++ scripts/HNG00295.py
@@ -2,4 +2,4 @@ name = "Akintayo Ifeoluwa Janet"
 ID="HNG-00295"
 language="python"
 email="akintayoife94@gmail.com"
-print ("Hello World,this is", name, "with HNGi7 ID", ID, "using", language, "for stage 2 task.", flush= True)
+print ("Hello World,this is", name, "with HNGi7 ID", ID, "using", language, "for stage 2 task.", email, flush=True)
