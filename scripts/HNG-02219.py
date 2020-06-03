3145b7e134a06fe75006eb597107c67e55f9b275
Afahakani <iniobong.afahakan@gmail.com>
2020-06-03 22:02:43 +0100

    initial commit

--- scripts/HNG-02219.py
+++ scripts/HNG-02219.py
@@ -3,4 +3,4 @@ id = "HNG-02219"
 language = "Python"
 email = "iniobong.afahakan@gmail.com"
 
-print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task.{email}", flush = True)
+print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task. {email}", flush = True)
