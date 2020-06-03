fd21f1c7409aea08645336efef978cf3067aff50
westernwells <rajiorazaq@gmail.com>
2020-06-03 23:44:13 +0100

    hope it works

--- scripts/HNG-01809.js
+++ scripts/HNG-01809.js
@@ -1,11 +1,11 @@
-(function(){
+
+function logg(){
     var fullname ="Raji Abdulrazaq";
     var id ="HNG-01809";
     var language="Javascript";
     var email="rajiorazaq@gmail.com";
 
-     const output = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
-    console.log(output);
-
+    const output = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
+         return  console.log(output);
 }
-)();
+logg()
