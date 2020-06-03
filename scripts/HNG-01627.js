a06bcef51010fff143d587b57a23dfc50c904a5c
pin4eva <pin4eva@hotmail.com>
2020-06-03 23:13:58 +0100

    HNG-01627

--- scripts/HNG-01627.js
+++ scripts/HNG-01627.js
@@ -6,7 +6,7 @@ const stage = "stage 2";
 
 const intro = () => {
   console.log(
-    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for ${stage} task. ${email}`
+    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
   );
 };
 
