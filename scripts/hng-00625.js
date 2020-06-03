b2b13aa4c7b254a0fae5851f7f78cd55e91e7c05
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2020-06-03 19:41:05 +0100

    Update hng-00625.js (#267)
    latest update, restructure of code

--- scripts/hng-00625.js
+++ scripts/hng-00625.js
@@ -6,8 +6,13 @@
 */
 
 function intro() {
-  let output = "Hello World, this is Adeola Adediran with HNGi7 ID HNG-00625 using Javascript for stage 2 task. adexdsamson@gmail.com";
-  return output;
+  let name, id, email, language;
+  name = "Adeola Adediran"
+  id = "HNG-00625";
+  email = "adexdsamson@gmail.com";
+  language = "Javascript";
+  let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}";
+  console.log(output)
 };
 
 intro();
