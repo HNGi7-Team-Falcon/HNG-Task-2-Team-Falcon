14d9aad98cdeb11ae082544edc2a15e7bf978caf
Samuel Onyijne <samuelord1@gmail.com>
2020-06-03 21:07:52 +0100

    changed to javascript

---
+++ scripts/HNG-03739.js
@@ -0,0 +1,13 @@
+const name = "Samuel Onyijne";
+const id = "HNG-03739";
+const language = "JavaScript";
+const email = "samuelonyijne@gmail.com";
+HNGi7 = () => {
+  task1 = () => `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`,
+  perform = () => {
+    console.log(task1());
+  }
+  perform();
+};
+
+HNGi7();
