d988386913ad794ccd4fa0f2789dc49c93b0e4a0
O-Midey <46810599+O-Midey@users.noreply.github.com>
2020-06-04 00:01:30 +0100

    Create hng-01650.js

---
+++ scripts/hng-01650.js
@@ -0,0 +1,6 @@
+var fullName = "Omotosho David"
+var id = "HNG-01650"
+var language = "JavaScript"
+var email = talk2adeoluwa2310@gmail.com
+
+console.log("Hello world this is " + fullName + " with HNG ID " + id + " using " + language + " for stage two task. " + email);
