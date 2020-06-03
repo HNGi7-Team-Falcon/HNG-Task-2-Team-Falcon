9c7033f98dfd7063cb7e110b2993655487b9fde6
Techiefer <adedokunoluwaferanmi@gmail.com>
2020-06-03 01:39:56 +0100

    commited my name

---
+++ scripts/hng02188.js
@@ -0,0 +1,7 @@
+let hngId = "hng02188";
+let name = "Oluwaferanmi Adedokun";
+let stack = "Javascript";
+
+console.log(
+  `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${stack} for stage 2 task`
+);
