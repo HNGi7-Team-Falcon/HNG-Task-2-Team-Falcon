fe5cdf791f0b843e0374fdebe6ea6c1f8e07ad00
Osoraa <sochi98.sn@gmail.com>
2020-06-03 21:10:35 +0100

    Performed the necessary tasks

--- scripts/hng01369.py
+++ scripts/hng01369.py
@@ -1,6 +1,10 @@
+# hng stage1 task
+
+# Declaring variables
 fullName = "Nwankwo Osoraa"
-hngId = "HNGi7 ID 01369"
+hngId = "HNG-01369"
 language = "Python"
 email = "sochi98.sn@gmail.com"
 
-print("Hello World, this is " + fullName + " with " + hngId + " using " + language + " for stage 2 task. " + email)
+# Printing to screen
+print("Hello World, this is " + fullName + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task. " + email)
