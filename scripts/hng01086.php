2fe2fb8e96adb82547185ff6e6f4a9a397c97b16
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-03 16:54:25 +0100

    Added more scripts

---
+++ scripts/hng01086.php
@@ -0,0 +1,7 @@
+<?php
+echo "Hello World,
+this is Adim Ekene, 
+with HNGi7 ID[HNG-01086],
+using[PHP] for stage 2 task.
+ekeneadim@gmail.com"
+?>
---
+++ scripts/hng01637.js
@@ -0,0 +1,13 @@
+let name = 'Adetayo Akinsanya',
+  id = 'HNG-01637',
+  email = 'akinsanyaadetayo@gmail.com',
+  language = 'JavaScript';
+
+const myInfo = () => {
+  let description = `Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
+
+  console.log(description);
+  return description;
+};
+
+myInfo();
