d627891b4a88569a4eae91e11fe6f7be8828a4f9
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2020-06-03 22:09:16 +0100

    Update hng-00625.js
    error fixed

--- scripts/hng-00625.js
+++ scripts/hng-00625.js
@@ -5,14 +5,14 @@
 *
 */
 
-function intro() {
-  let name, id, email, language;
-  name = "Adeola Adediran"
-  id = "HNG-00625";
-  email = "adexdsamson@gmail.com";
-  language = "Javascript";
-  let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}";
-  console.log(output)
-};
+function introduction() {
+    let name, id, email, language
+    name = 'Adeola Adediran'
+    id = 'HNG-00652'
+    email = 'adexdsamson@gmail.com'
+    language = 'Javascript'
+    let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
+    console.log(output)
+}
 
-intro();
+introduction()
