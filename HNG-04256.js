1bd773148fc0194bae547b901a5fbfc5497afdf0
devphase <62597014+devphaseX@users.noreply.github.com>
2020-06-03 11:45:26 +0100

    Add files via upload

---
+++ HNG-04256.js
@@ -0,0 +1,8 @@
+'use strict';
+const introduceMyself = (fullName, id, preferredLang) => {
+  console.log(
+    `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${preferredLang} for stage 2 task.`
+  );
+};
+
+introduceMyself('Ayomide', '04256', 'javascript');
