f9f540bb0dfe2b98c15958140bd99beba475f388
Oluwatoni Ogunfusika <ogunfusika64@gmail.com>
2020-06-03 16:47:18 +0100

    Update HNG-00419.php

--- scripts/HNG-00419.php
+++ scripts/HNG-00419.php
@@ -4,5 +4,5 @@
   $myHNGID = "HNG-00419";
   $myEmail = "ogunfusika64@gmail.com";
   $myLangUsed = "PHP";
-  echo "Hello World, this is ".$myLastName." ".$myFirstName." with HNGi7 ID ".$myHNGID." using ".$myLangUsed." for stage 2 task. ".$myEmail."";
+  echo "Hello World, this is ".$myLastName." ".$myFirstName." with HNGi7 ID ".$myHNGID." using ".$myLangUsed." for stage 2 task. ".$myEmail;
 ?>
