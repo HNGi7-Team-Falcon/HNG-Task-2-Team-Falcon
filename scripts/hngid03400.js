2a94dd999d1ae230f7a1a6f43dbbdca47c534383
OnolexTim <66238465+OnolexTim@users.noreply.github.com>
2020-06-03 11:03:14 +0100

    Update hngid03400.js

--- scripts/hngid03400.js
+++ scripts/hngid03400.js
@@ -1,4 +1,4 @@
 var input = ["Abasiono" + " Timothy"];
 var id = ["hngid-03400"];
-alert ("Hello World," + " this is" + input + " " + "with HNGi7 I'd " + id + " using javascript for stage 2 task");
-console.log("Welcome to stage 2" + " "+ input + " & email abasionotimothy@gmail.com");
+console.log("Hello World," + " this is" + input + " " + "with HNGi7 I'd " + id + " using javascript for stage 2 task" + " email@abasionotimothy@gmail.com);
+alert("Welcome to stage 2" + " "+ input);
