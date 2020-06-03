4d267100f7c2a3fe0b0462dd5d6c416ffc9aafe1
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-06-03 21:47:35 +0100

    Fix conflict

--- scripts/hng00327.js
+++ scripts/hng00327.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const printMessage = () => {
-  const name = 'Zimuzo Obiechina';
-  const id = 'hng00327';
-  const lang = 'JavaScript';
-  const email = 'zimobie@gmail.com';
-
-  console.log(
-    `Hello World, this is ${name} with HNG ID: ${id} using ${lang} for stage 2 task.`
-  );
-};
-=======
-
-
 const printMessage = () => {
   const name = 'Zimuzo Obiechina';
   const id = 'hng00327';
@@ -23,4 +9,3 @@ const printMessage = () => {
   );
 };
 
->>>>>>> 7edf47b8deca4cc57c45f1aec37c35400f280bb8
