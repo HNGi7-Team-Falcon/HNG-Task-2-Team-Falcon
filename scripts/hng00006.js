01ad101f1174a70a6ecae92ded079e1cc7bc39a3
Azubuike Nnamdi Clement <35131360+CtrlAltFix-Softwares@users.noreply.github.com>
2020-06-03 11:38:04 -0700

    Create hng00006.js

---
+++ scripts/hng00006.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HNG</title>
+    <link rel="stylesheet" href="">
+</head>     
+<body>
+    <p></p>
+
+    <script>
+
+        var info =
+        "{
+            "fullname" : Azubuike Nnamdi Clement",
+            "id": "HNG-03331",
+            "language": "Javascript"
+            }"
+
+            var display = document.querySelector("p");
+            display.textContent = "Hello World, this is ${json.fullname} with HNGi7 ID ${json.id} using ${json.language} for stage 2 task";
+            
+    </script>
+
+</body>
+</html>
