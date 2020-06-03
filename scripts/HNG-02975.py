be757a5c40ab7424663f8dfb62fced1859911e7c
aveirahabeeb <aveirahabeeb@gmail.om>
2020-06-03 11:48:49 +0100

    Added email

--- scripts/HNG-02975.py
+++ scripts/HNG-02975.py
@@ -1,3 +1,9 @@
 
 
-print("Hello World, this is Motolani Ajibola with HNGi7 ID HNG-02975 using Python for stage 2 task")
+name = 'Motolani Ajibola'
+id = 'HNG-02975'
+language = 'Python'
+email = 'aveirahabeeb@gmail.com'
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. " + email)
+
+
