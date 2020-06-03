c082dc229e203dcd7a5a656d77a048fe68c851f8
gbolahan146 <gbolahanoduyemi1@gmail.com>
2020-06-03 12:58:40 +0100

    Update HNG-04552.py added email

--- scripts/HNG-04552.py
+++ scripts/HNG-04552.py
@@ -1,1 +1,2 @@
-print("Hello World, This is Gbolahan Oduyemi with HNGi7 ID HNG-04552 using Python for stage 2")
+email = "gbolahanoduyemi1@gmail.com"
+print("Hello World, this is Gbolahan Oduyemi with HNGi7 ID HNG-04552 using Python for stage 2 task. Email:"+email)
