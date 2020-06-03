7900d7a56cf42564cad8b3975093ba79dba18945
Kofi Mokome <kofimokome10@gmail.com>
2020-06-03 14:31:23 +0100

    Update hng03600.php
    Add email to output

--- scripts/hng03600.php
+++ scripts/hng03600.php
@@ -2,5 +2,5 @@
 $id = "HNG-03600";
 $name = "Kofi Mokome";
 $language = "PHP";
-
-echo sprintf("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task",$name,$id,$language);
+$email = "kofimokome10@gmail.com";
+echo sprintf("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s",$name,$id,$language,$email);
