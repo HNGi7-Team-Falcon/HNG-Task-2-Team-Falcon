ca4b749f9edb624d8932b9f70f5babb8efa40b31
devmajor <victor.mrcertified@gmail.com>
2020-06-03 14:53:15 +0100

    Added my file to script folder

---
+++ scripts/hng01183.js
@@ -0,0 +1,12 @@
+
+
+
+{
+file: "myScript.py",
+output: "Hello World, this is Olagesin Samuel with HNGi7 ID HNG-04817 using Python for stage 2 task",
+name: "Olagesin Samuel",
+id: "HNG-04817",
+email: "seyi@hng.tech",
+language: "Python",
+status: "Pass"
+},
