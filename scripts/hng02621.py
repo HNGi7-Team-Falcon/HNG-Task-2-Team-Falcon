ab5c52cda19f16adfd34fd0b9cb67dc9480b2ae6
McChizzy <66224357+McChizzy@users.noreply.github.com>
2020-06-03 01:07:39 +0100

    Update hng02621.py

--- scripts/hng02621.py
+++ scripts/hng02621.py
@@ -2,4 +2,4 @@ Name = 'Chikezie Ikeche'
 Language = 'Python'
 id = "02621"
 
-print("Hello World, this is", (Name), "with HNGi7 ID", (id), "using", (Language), "for stage 2 test")
+print("Hello World, this is", (Name), "with HNGi7 ID", (id), "using", (Language), "for stage 2 test", flush=True)
