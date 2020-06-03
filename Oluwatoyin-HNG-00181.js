f157a5e67a073d035652af3cde958a26057c3d06
oluwatoyin-Ty <66219838+oluwatoyin-Ty@users.noreply.github.com>
2020-06-03 16:57:42 -0700

    Add files via upload

---
+++ Oluwatoyin-HNG-00181.js
@@ -0,0 +1,13 @@
+
+<h2> Task 2 </h2> 
+
+function PersonalDetails(name, id, lan, email) {
+  this.myName = Oluwatoyin;
+  this.myId = HNG-OO181;
+  this.myLan = JavaScript;
+  this.myEmail = futoyinbakare@gmail.com;
+}
+
+var myF = new PersonDetails("Oluwatoyin Bakare", "HNG-OO181", "Javascript", "futoyinbakare@gmail.com");
+
+console.log("Hello World!," "This is Oluwatoyin Bakare" "with ID: HNG-00181 using JavaScript for task 2") 
