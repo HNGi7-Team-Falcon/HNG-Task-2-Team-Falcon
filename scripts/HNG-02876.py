0055e1b7b0c29c73154090e223f47353716ebc56
Akinwande Komolafe <47945512+Sensei-akin@users.noreply.github.com>
2020-06-03 14:34:12 +0100

    added HNG id

--- scripts/HNG-02876.py
+++ scripts/HNG-02876.py
@@ -1,7 +1,7 @@
 Name = 'Akinwande Komolafe'
-HNGi7ID = '02876'
+HNGi7ID = 'HNG-02876'
 PreferredLanguage = 'Python'
 Stage = '2'
 email = 'akinwandekomolafe@gmail.com'
 
-print(f"Hello World, this is {Name} with HNGi7 ID HNG-{HNGi7ID} using {PreferredLanguage} for stage {Stage} task. {email}", flush=True)
+print(f"Hello World, this is {Name} with HNGi7 ID {HNGi7ID} using {PreferredLanguage} for stage {Stage} task. {email}", flush=True)
