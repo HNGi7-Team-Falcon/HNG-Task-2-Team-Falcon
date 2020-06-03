24694e2f8147d89f2fa72252270dba4018d0b5a9
mustapha sani <mustapha3341@gmail.com>
2020-06-03 22:17:10 +0100

    corrected string output

--- scripts/HNG-00134.js
+++ scripts/HNG-00134.js
@@ -1,7 +1,7 @@
 const myIntroScript = (fullName, hng_ID, lang, email) => {
 	console.log(
-		`Hello World, this is ${fullName} with HNGI7 ID ${hng_ID} using ${lang} for Stage 2 task ${email}`
+		`Hello World, this is ${fullName} with HNGi7 ID ${hng_ID} using ${lang} for Stage 2 task ${email}`
 	);
 };
 
-myIntroScript("Mustapha Sani", "HNG00134", "nodejs", "mustapha3341@gmail.com");
+myIntroScript("Mustapha Sani", "HNG-00134", "javascript", "mustapha3341@gmail.com");
