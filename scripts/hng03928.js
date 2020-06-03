9a9cd74c23a51131d97a0bbaeb68bb965edcc5b8
dayon3 <aliasdayone@gmail.com>
2020-06-03 12:03:42 +0300

    added email address

--- scripts/hng03928.js
+++ scripts/hng03928.js
@@ -2,10 +2,11 @@ const davis = {
 	name: 'Davis Gitonga',
 	id: 'HNG-03928',
 	language: 'JavaScript',
+	email: 'davisgitonganyaga@gmail.com',
 };
 
-const outputMessage = (name, id, language) => {
-	return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+const outputMessage = (name, id, email, language) => {
+	return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
 };
 
-console.log(outputMessage(davis.name, davis.id, davis.language));
+console.log(outputMessage(davis.name, davis.id, davis.email, davis.language));
