5b1181e3f21a740e047a93d1eb3d57058e1e2bff
Collins4u1 <61602762+Collins4u1@users.noreply.github.com>
2020-06-03 23:11:19 +0100

    Update hng05372.js

--- scripts/hng05372.js
+++ scripts/hng05372.js
@@ -3,4 +3,4 @@ fullName = 'Anyaebosi Uchenna';
 id = 'HNG-05372';
 language = 'Javascript';
 email = 'ucheanyaebosi@gmail.com';
-console.log(`Hello world, this is ${fullName} with HNGi7 ID ${id} and  using ${language} for stage 2 task. ${email}`); 
+console.log(`Hello world, this is ${fullName} with HNGi7 ID ${id} and using ${language} for stage 2 task. ${email}`); 
