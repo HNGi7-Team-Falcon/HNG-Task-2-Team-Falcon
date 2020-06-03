c7fadac2b6c6bf052a909c08115bbb1680f57179
Zahradeen Muazu <20259753+itxDeeni@users.noreply.github.com>
2020-06-03 09:51:42 +0000

    Update HNG03769.py

--- HNG03769.py
+++ HNG03769.py
@@ -2,8 +2,8 @@ name ="Zahradeen Muazu"
 hngId = "HNG03769"
 language = "Python" 
 
-email = "Zahradeenmuazu@yahoo.com"
+
 
  
-print("Hello World, this is " + name + " with  HNGi7 ID :" + hngId + " using " + language + " for stage 2 task",flush=True);
+print("Hello World, this is " + name + " with  HNGi7 ID :" + hngId + " using " + language + " for stage 2 task, Zahradeenmuazu@yahoo.com",flush=True);
  
