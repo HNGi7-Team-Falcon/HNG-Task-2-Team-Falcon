b6cb816e870a577bf81c5019004934ee84495e28
Eleanya-David <65508991+Eleanya-David@users.noreply.github.com>
2020-06-03 11:26:36 -0700

    HNG-Task 2 script

---
+++ scripts/HNG-01887.js
@@ -0,0 +1,8 @@
+var call="Hello World, this is Eleanya-Alichi-David with HNGi7 ID HNG-01887 using Javascript for stage 2 task. davideleanyaalichi@gmail.com"
+function HNGi7(){
+    console.log (call);
+}
+var a="true"
+if(a="true"){
+console.log (HNGi7());
+};
