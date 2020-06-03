e70719a3a0bca4bb293fb72842951ae78508f4f0
mustapha sani <mustapha3341@gmail.com>
2020-06-03 10:30:46 +0100

    added email address to script

--- scripts/HNG-00134.js
+++ scripts/HNG-00134.js
@@ -1,7 +1,7 @@
-const myIntroScript = (fullName, hng_ID, lang) => {
+const myIntroScript = (fullName, hng_ID, lang, email) => {
 	console.log(
-		`Hello World, this is ${fullName} with HNGI7 ID ${hng_ID} using ${lang} for Stage 2 task`
+		`Hello World, this is ${fullName} with HNGI7 ID ${hng_ID} using ${lang} for Stage 2 task ${email}`
 	);
 };
 
-myIntroScript("Mustapha Sani", "HNG00134", "nodejs");
+myIntroScript("Mustapha Sani", "HNG00134", "nodejs", "mustapha3341@gmail.com");
