b4d42a1ca400a705eb5b798f1003355986c00b75
Victor Onuchukwu <chisomonuchukwu91@gmail.com>
2020-06-03 20:03:23 +0100

    Create HNG-1012.php

---
+++ hng1012.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$greeting = "Hello World";
+$fullname = "Onuchukwu Victor";
+$id = "HNG-1012";
+$email = "victoronuchukwu16@gmail.com";
+$language = "PHP";
+
+
+echo "Hello World , this is: " . $fullname .  " with HNGi7 ID " . $id . " and email " . $email . " ,using "  . $language . " for stage 2 task ";
+
+
+?>
+</body>
+</HTML>
