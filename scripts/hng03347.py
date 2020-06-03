0f605e6b2938ab99bbb58e7e6c90a48b21309003
Chiagozie Jacqueline Robert-Maduekwe <cj4rober@uwaterloo.ca>
2020-06-03 13:05:47 -0400

    Update hng03347.py

--- scripts/hng03347.py
+++ scripts/hng03347.py
@@ -1,5 +1,4 @@
-import time
 import sys
 
-print("Hello World, this is Chiagozie Robert-Maduekwe with HNGi7 ID 03347 using Python for stage 2 task")
+print("Hello World, this is Chiagozie Robert-Maduekwe with HNGi7 ID 03347 using Python for stage 2 task. robertchiagozie@gmail.com")
 sys.stdout.flush()
