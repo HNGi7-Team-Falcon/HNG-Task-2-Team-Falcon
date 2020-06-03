c8e0ba5d2ec57d5cd2b969e05c500c0118ef325c
0m0hsam <60308962+0m0hsam@users.noreply.github.com>
2020-06-03 13:23:31 +0100

    Update HNG-02502.py
    Email update and output json

--- scripts/HNG-02502.py
+++ scripts/HNG-02502.py
@@ -1,17 +1,19 @@
 import re
-#print HNG user detail
-open= True
-def Open(open):
-	global Open
-	global cmd
-	cmd=re.sub(" ", "",input(('Program to check my HNG user details\n To check Input : Yes\n To End input: End\n >>')).lower())
-	global user	
+import json
+#
 	def user(cmd):
+		comment='Program Closed thanks for checking\nPlease grade me well!'
 		if cmd == 'yes':
 			print('Hello world')
-			print('Full name: Samuel Jonathan\nUser ID :02502\nProgramming Language: Python\nEmail:Omohsam81@gmail.com')
+			my_data={'Full name': 'Samuel Jonathan',
+			'User ID': '02502',
+			'Programming Language': 'Python',
+			'Email': 'omohsam81@gmail.com'}
+			to_json=json.dumps(my_data)
+			print(to_json)
+			print(comment)	
 		elif cmd == 'end':
-			print('Program Closed thanks for checking\nPlease grade me well')
+			print(comment)	
 		else :
 			print('Oops wrong entry!')
 			exit()
