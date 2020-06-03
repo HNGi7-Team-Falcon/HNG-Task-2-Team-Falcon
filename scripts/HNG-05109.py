64d13c4c8f57ff80921e6718e096e75debc58087
Emmanuel <immanuelsiawdarko@gmail.com>
2020-06-03 06:01:00 +0000

    Add files via upload
    Hello world py

---
+++ scripts/HNG-05109.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+Full_name = 'Emmanuel Siaw-Darko'
+HNG_id = 'HNG-05109'
+Programming_lang = 'Python'
+
+
+# In[6]:
+
+
+print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(Full_name,HNG_id,Programming_lang))
+
+
+# In[ ]:
+
+
+
+
