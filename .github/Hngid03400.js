fac0d0e299541f764bbe6ec8398ea42dfa45adb7
OnolexTim <66238465+OnolexTim@users.noreply.github.com>
2020-06-03 11:21:53 +0100

    Update Hngid03400.js

--- .github/Hngid03400.js
+++ .github/Hngid03400.js
@@ -1,6 +1,6 @@
 var input =["Abasiono" +" Timothy"];
-alert("Hello world, this is" + " " + input + " with" + " id" + " " + "using javascript");
-var id= ["HNG" + "ID" + "03400"];
+console.log("Hello world, this is" + " " + input + " with" + " id" + " " + "using javascript for stage 2 task " + "email@abasionotimothy@gmail.com" );
+var id= ["HNGi7" + "ID-" + "03400"];
 
 
 
