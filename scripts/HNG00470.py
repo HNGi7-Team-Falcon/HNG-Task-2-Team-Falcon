4524ed2ca05b4b383209a99d8a3e421e142f4cf5
Enoredia <38014615+Enoredia@users.noreply.github.com>
2020-06-03 22:52:11 +0100

    Update HNG00470.py

--- scripts/HNG00470.py
+++ scripts/HNG00470.py
@@ -1,1 +1,1 @@
-print('Hello world, this is Osarumwense Blessing Eseosa with HNGi7 ID HNG00470 using python for stage 2 task')
+print('Hello world, this is Osarumwense Blessing Eseosa with HNGi7 ID HNG-00470 using python for stage 2 task. blessingosarumwense2@gmail.com')
