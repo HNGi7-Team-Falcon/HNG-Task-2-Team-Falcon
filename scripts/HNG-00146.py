780a799ac998e6dc8d97bf68c1719b9873c27f4d
youthtrouble <47859940+youthtrouble@users.noreply.github.com>
2020-06-03 23:15:10 +0100

    minor fix

--- scripts/HNG-00146.py
+++ scripts/HNG-00146.py
@@ -3,6 +3,6 @@ id = "HNG-00146"
 language = "python"
 email = "harjhibolar@gmail.com"
 
-stuff = "Hello world, this is {} with HNGi7 ID {} using {} for stage 2 task. {}"
+stuff = "Hello world, this is {} with HNGi7 ID {} using {} for stage 2 task"
 
-print(stuff.format(fullname, id, language, email),flush = True)
+print(stuff.format(fullname, id, language,),flush = True)
