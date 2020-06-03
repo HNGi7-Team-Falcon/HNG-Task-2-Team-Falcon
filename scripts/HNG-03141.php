bc2fecd99e130896ae3438636b52ede1531b86c4
adtrex <you@example.com>
2020-06-03 10:39:04 +0100

    update

--- scripts/HNG-03141.php
+++ scripts/HNG-03141.php
@@ -2,8 +2,6 @@
 $adtrex = "Hello World, this is Adekunte Tolulope with HNGi7 ID HNG-03141 using PHP for stage 2 task";
 echo $adtrex;
 
-$name ="Adekunte Tolulope";
-$id = "HNG-03141";
 $email = "toluadekunte@gmail.com"
-$language = "PHP";
+
 ?>
