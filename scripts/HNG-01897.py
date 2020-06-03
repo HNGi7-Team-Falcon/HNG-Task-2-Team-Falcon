a5a0367a6dcdc92dddefb6184ab36f7fe0305706
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-06-03 10:57:56 +0100

    Second commit
    Add email address

--- scripts/HNG-01897.py
+++ scripts/HNG-01897.py
@@ -1,1 +1,1 @@
-print("hello world, this is umar muhammad zakari with hngi7 id hng-01897 using python for stage 2 task")
+print("hello world, this is Umar Muhammad Zakari with HNGi7 id HNG-01897 using python for stage 2 task.umarfarouqft@gmail.co")
