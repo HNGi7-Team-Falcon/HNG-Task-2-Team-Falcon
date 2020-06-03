e6ee04e13773c355f5d29fcb03095c6c19d14ebd
Luwafemi A <aladefemi1@gmail.com>
2020-06-03 13:33:55 +0100

    Update HNG-02667.js

--- scripts/HNG-02667.js
+++ scripts/HNG-02667.js
@@ -1,1 +1,7 @@
 
+const name = "Luwafemi Alade";
+const id = "HNG-02667";
+const language = "javascript";
+const email = "aladefemi1@gmail.com";
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task `;
+console.log(output);
