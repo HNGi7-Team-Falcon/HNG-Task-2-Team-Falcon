4f80b02ac14192446e99ecf63ea4256a2d982bf7
Emmanuel <immanuelsiawdarko@gmail.com>
2020-06-03 10:46:12 +0000

    Task2: 2nd commit output updated with email string

--- scripts/HNG-05109.py
+++ scripts/HNG-05109.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[4]:
 
 
-Full_name = 'Emmanuel Siaw-Darko'
-HNG_id = 'HNG-05109'
-Programming_lang = 'Python'
-
-
-# In[6]:
-
-
-print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(Full_name,HNG_id,Programming_lang))
+name = 'Emmanuel Siaw-Darko'
+Id = 'HNG-05109'
+language = 'Python'
+email = 'immanuelsiawdarko@gmail.com'
+print("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name, Id, email, language))
 
 
 # In[ ]:
