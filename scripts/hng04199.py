517a77d8e967678e606b119ffb51bd7c7600d29b
Babscode <scotteyawo@gmail.com>
2020-06-03 22:02:50 +0100

    Create hng04199.py
    I added hng04199.py

---
+++ scripts/hng04199.py
@@ -0,0 +1,8 @@
+# full name
+fullName = 'Eyawo Okiemute Scott'
+# id name
+ID = 'HNG-04199'
+# language
+language = 'python'
+
+print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task.")
