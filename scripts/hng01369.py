14a2a690674fe7a16b934ae9aa5c9c289d36922d
Osoraa <sochi98.sn@gmail.com>
2020-06-03 20:55:37 +0100

    Made the requested changes to display the output

---
+++ scripts/hng01369.py
@@ -0,0 +1,6 @@
+fullName = "Nwankwo Osoraa"
+hngId = "HNGi7 ID 01369"
+language = "Python"
+email = "sochi98.sn@gmail.com"
+
+print("Hello World, this is " + fullName + " with " + hngId + " using " + language + " for stage 2 task. " + email)
