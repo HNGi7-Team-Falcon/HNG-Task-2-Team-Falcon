336ec0d8c08b37704e355ac1f487b0d691f833b6
olawolu <omg.tosine@gmail.com>
2020-06-03 21:37:37 +0100

    Edits made. Changed email position in string.

--- scripts/HNG-00872.py
+++ scripts/HNG-00872.py
@@ -4,4 +4,4 @@ hng_id = "HNG-00872"
 email = "gbemigaolawolu@gmail.com"
 language = "Python"
 
-print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s"%(full_name, hng_id, language, email ))
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s"%(full_name, hng_id, language, email), flush=True)
