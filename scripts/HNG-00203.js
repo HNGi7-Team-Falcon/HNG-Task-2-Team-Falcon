165511b68b7a9dd326928f2f74fbe984d1dcf833
iyanushow <iyanusowande@gmail.com>
2020-06-03 23:59:34 +0100

    completed-task(new)

--- scripts/HNG-00203.js
+++ scripts/HNG-00203.js
@@ -7,11 +7,11 @@ const myDetails =
 
     fullName: firstName + " " +lastName,
     ID: "HNG-00203",
-    language: "Javascript",
+    language: "JavaScript",
     mail: "iyanusowande@gmail.com",
     displayDetails: function ()
         {
-            console.log(`Hello World, this is ${this.fullName} with HNGi7 ID ${this.ID} using ${this.lang} for stage 2 task. ${this.mail}`);
+            console.log(`Hello World, this is ${this.fullName} with HNGi7 ID ${this.ID} using ${this.language} for stage 2 task. ${this.mail}`);
         }   
 
 
