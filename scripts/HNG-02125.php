9a4136fb691acc3f5ab2ed30ad10c682d426586e
shotolu <65109833+shotolu@users.noreply.github.com>
2020-06-03 14:19:45 +0100

    Update HNG-02125.php

--- scripts/HNG-02125.php
+++ scripts/HNG-02125.php
@@ -1,10 +1,10 @@
-        var name = "Shotolu Paul Oluwabukunmi"
+        var name = "Shotolu Paul"
         var hngid = "HNG-02125"
         var lang = "javascript"
-        var email= "paulshotolu@gmail.com"
+        var mail= "paulshotolu@gmail.com"
 
 		function paulfunc() {
-		   return ('Hello World, this '+ name +' with HNGi7 ID '+ hngid +' using '+ lang +' for stage 2 task. '+ email +'');
+		   return ('Hello World, this '+ name +' with HNGi7 ID '+ hngid +' using '+ lang +' for stage 2 task. '+ mail +'');
 		}
 		console.log(paulfunc());
 
