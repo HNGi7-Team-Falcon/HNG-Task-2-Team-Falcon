38484302008138ac11abfe5de83231f66d5d12c8
kelrob <test>
2020-06-03 16:55:41 +0100

    Bug Fixex on Index

--- scripts/hng-00625.js
+++ scripts/hng-00625.js
@@ -1,10 +1,3 @@
-/*
-*
-* @author Adeola Adediran
-* function intro() 
-*
-*/
-
 function intro() {
   let introduction = "Hello World, this is Adeola Adediran with HNGi7 ID HNG-00625 using javascript for stage 2 task";
   return introduction;
