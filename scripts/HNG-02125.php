b5c2d03283e56d8e6fad6ba00d3af14f01e2cea9
shotolu <65109833+shotolu@users.noreply.github.com>
2020-06-03 10:27:35 +0100

    Update HNG-02125.php

--- scripts/HNG-02125.php
+++ scripts/HNG-02125.php
@@ -1,5 +1,5 @@
 <p id="profile"></p>
 
 <script>
-document.getElementById("profile").innerHTML = "Hello World, this is Shotolu Paul Oluwabukunmi with HGNi7 ID HNG-02125 using javascript";
+document.getElementById("profile").innerHTML = "Hello World, this is Shotolu Paul Oluwabukunmi email adderess paulshotolu@gmail.com with HGNi7 ID HNG-02125 using javascript for stage 2 task";
 </script>
