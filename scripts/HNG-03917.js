8a211a0ab52b28103e4211801c8be1240f8c679a
Basit <basitajao96@gmail.com>
2020-06-03 13:19:21 +0100

    made minor changes

--- scripts/HNG-03917.js
+++ scripts/HNG-03917.js
@@ -4,6 +4,6 @@ const lang = "javascript";
 const email = "Basitajao96@gmail.com"
 
 function task () {
-    return ` Hello World,this is ${userName}, with HNGi7 ID ${hngId}, using ${lang} for stage 2 task   `  
+    return `Hello World,this is ${userName} with HNGi7 ID ${hngId} using ${lang} for stage 2 task`  
 }
 console.log(task()) 
