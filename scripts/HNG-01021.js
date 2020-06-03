8ea3533edf08fec81f220d313d3f4161f8809a2e
odizee <66241926+odizee@users.noreply.github.com>
2020-06-03 21:07:46 +0100

    Update HNG-01021.js

--- scripts/HNG-01021.js
+++ scripts/HNG-01021.js
@@ -1,6 +1,6 @@
 function odiri () {
        
-       var helloWorld = "Hello World, this is Ighogboja Odiri with HNGi7 ID HNG-01021 and email odizeeplatform@gmail.com using JavaScript for stage 2 task."
+       var helloWorld = "Hello World, this is Ighogboja Odiri with HNGi7 ID HNG-01021 using JavaScript for stage 2 task. odizeeplatform@gmail.com"
        return helloWorld;
 
    } 
