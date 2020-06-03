c1e0e29bfa3db0e80393492ce43e9a5a472b4c63
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-06-03 14:29:23 +0100

    Taiwo Ogunola Task 2 Submission using JS

---
+++ scripts/HNG-02175.js
@@ -0,0 +1,9 @@
+(() => {
+	const fullName = 'Taiwo Ogunola';
+	const myId = 'HNG-02175';
+	const lang = 'JavaScript';
+
+	const returnStr = `Hello World, this is ${fullName} with HNGi7 ID ${myId} using ${lang} for stage 2 task`;
+
+	console.log(returnStr);
+})();
