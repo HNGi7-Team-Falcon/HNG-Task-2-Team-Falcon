10b1750eddb93d49441c8a4b8449998372b37890
Toyeeb Ganiu <39214719+TokyoTG@users.noreply.github.com>
2020-06-03 17:55:58 +0100

    fixed the string error

--- scripts/HNG-04240.php
+++ scripts/HNG-04240.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello world, this is Toyeeb Ganiu with HNGi7 ID HNG-04240 using PHP for stage 2 task";
+echo "Hello World, this is Toyeeb Ganiu with HNGi7 ID HNG-04240 using PHP for stage 2 task";
 ?>
