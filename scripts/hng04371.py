3dc7700b28d42211c9d5122092b3e44798a58696
khira67 <58661198+khira67@users.noreply.github.com>
2020-06-03 03:46:31 +0100

    I created hng04371

---
+++ scripts/hng04371.py
@@ -0,0 +1,5 @@
+name="Rasaq Eniola Mariam"
+hngid ="HNG-04371"
+language ="python"
+
+print("hello world,this is "+ name +"with HNGi7id:"+hndid + " using" + language+"for stage 2 task", flush=True);
