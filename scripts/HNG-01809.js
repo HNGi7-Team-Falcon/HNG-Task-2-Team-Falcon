98d78779dfc4fe15858a7e2c1a43f8691322fb25
westernwells <rajiorazaq@gmail.com>
2020-06-03 18:26:57 +0100

    Another push

---
+++ scripts/HNG-01809.js
@@ -0,0 +1,11 @@
+(function(){
+    var fullname ="Raji Abdulrazaq";
+    var id ="HNG-01809";
+    var language="Javascript";
+    var email="rajiorazaq@gmail.com";
+
+     const output = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
+    console.log(output);
+
+}
+)();
