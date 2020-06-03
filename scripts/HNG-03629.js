b925f0a4e08e3e06a47bbf05f4bd10587587dbb3
ayodeji07 <ayodejiakande2107@gmail.com>
2020-06-03 15:10:16 +0100

    my task 2 commit

---
+++ scripts/HNG-03629.js
@@ -0,0 +1,3 @@
+var name = "Akande Ayodeji";
+var id = "HNG-03629";
+console.log("Hello World, this is" + " " + name + " " + "with HNGi7 ID" + " " + id +  " " + "using Javascript for stage 2 task");
