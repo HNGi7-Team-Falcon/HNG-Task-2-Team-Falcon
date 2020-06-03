0e480fddd84210614ee36518f0df22c7e2ba5b6b
odafe94 <63509729+odafe94@users.noreply.github.com>
2020-06-03 19:22:44 +0100

    Update hng00854.py
    added email and 'flush=True'

--- scripts/hng00854.py
+++ scripts/hng00854.py
@@ -1,9 +1,9 @@
 full_name = 'Odafe Akpoborie'
 hng_id = 'HNG-00854'
-language = 'Python'
-email: 'ohdahphae@gmail.com'
+lang = 'Python'
+email = 'ohdahphae@gmail.com'
 
-def hng_function(full_name, hng_id, language):
-    print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task'.format(full_name, hng_id,language))
+def func(full_name, id, lang, email):
+    return f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {lang} for stage 2 task. {email}'
 
-hng_function(full_name, hng_id, language)
+print(func(full_name, hng_id, lang, email), flush=True)
