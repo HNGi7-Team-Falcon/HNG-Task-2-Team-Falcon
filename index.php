8b594ea82887481561a7fb1b1cdd48088d54ccf1
kelrob <test>
2020-06-03 18:52:48 +0100

    Json Added

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+$url = $_SERVER['REQUEST_URI'];
+$response_type = substr(strrchr($url, "?"), 1);
+
 $directory = 'scripts';
 
 function get_string_between($string, $start, $end){
@@ -18,7 +22,77 @@ function extract_email($string) {
 
 // str_replace($search, $replace, $subject)
 
+$files = array_slice(scandir($directory),3);
+$length = count($files);
+$noOfPasses = 0;
+$noOfFailures = 0;
+
 ?>
+
+<?php
+if ($response_type == 'json') {
+    $result = array();
+    $result['length'] = $length;
+
+
+    foreach (glob($directory . "/*.*") as $file) {
+
+
+        # Get file Extension
+        $filename = str_ireplace('scripts/', '', $file);
+        $ext = strtolower(substr(strrchr($filename, "."), 1));
+
+        # Command for different
+        if ($ext == 'php') {
+            $response = exec("$ext $file 2>&1", $output);
+        } else if ($ext == 'js') {
+            $response = exec("node $file 2>&1", $output);
+        } else if ($ext == 'py') {
+            $response = exec("python $file 2>&1", $output);
+        } else {
+            $reason = 'Invalid file type';
+        }
+
+        $internName = trim(get_string_between($response, 'this is', 'with'));
+        $internID = $id = trim(get_string_between($response, 'ID', 'using'));
+        $language = trim(get_string_between($response, 'using', 'for'));
+        $email = extract_email($response);
+
+        $newResponse = (string)str_replace($email, "", $response);
+
+        # Check status of response
+        $passCondition1 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task ";
+        $passCondition2 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task.";
+        $passCondition3 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task";
+        $passCondition4 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task. ";
+
+        if (($passCondition1 == $newResponse) || ($passCondition2 == $newResponse) || ($passCondition3 == $newResponse) || ($passCondition4 == $newResponse)) {
+            $status = 'pass';
+            $noOfPasses++;
+        } else {
+            $noOfFailures++;
+            $status = 'fail';
+        }
+
+
+        $result['Pass'] = $noOfPasses;
+        $result['Fail'] = $noOfFailures;
+        $data = new stdClass();
+        //$data->file = $value;
+        $data->output = $newResponse;
+        $data->owner = $internName;
+        $data->id = $internID;
+        $data->email = $email;
+        $data->language = $language;
+        $data->status = @$status;
+        $result[] = $data;
+
+    }
+    echo json_encode($result);
+    die();
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -105,7 +179,7 @@ function extract_email($string) {
 
                     if (($passCondition1 == $newResponse) || ($passCondition2 == $newResponse) || ($passCondition3 == $newResponse) || ($passCondition4 == $newResponse)) {
                         $status = '<b class="text-success">pass</b>';
-                        $passed++; 
+                        $passed++;
                     } else {
                         $failed++;
                         $status = '<b class="text-danger">Incorrect string passed</b>';
@@ -117,7 +191,7 @@ function extract_email($string) {
                         <td><?php echo $internName;  ?></td>
                         <td><?php echo $newResponse;  ?></td>
                         <td><?php echo $status;  ?></td> <!-- -->
-                        <td><?php echo $internID;  ?></td> <!-- -->
+                        <td><?php echo $internID;  ?></td>
                     </tr>
                     <?
                 }
