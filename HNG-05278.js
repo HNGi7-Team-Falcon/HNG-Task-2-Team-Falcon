ff7b4ca96c33c703e71ba2b8d1132d6029188b5a
keloncode <46829890+keloncode@users.noreply.github.com>
2020-06-03 13:00:11 -0700

    Add files via upload

---
+++ HNG-05278.js
@@ -0,0 +1,7 @@
+
+ let myName ="this is Emmanuel Kel Obi"
+ let myId = "HNG-05278"
+ let myEmail = "emmanuelobika5@gmail.com"
+ let myData = "Hello World," + myName + "," + myId + ','+ myEmail+ "," +"using javascript for step 2 task."
+
+  console.log(myData);
