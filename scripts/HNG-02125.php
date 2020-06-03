280b4485e81293d1411dbce2083237c8785a1708
shotolu <65109833+shotolu@users.noreply.github.com>
2020-06-03 12:07:30 +0100

    Update HNG-02125.php

--- scripts/HNG-02125.php
+++ scripts/HNG-02125.php
@@ -1,5 +1,4 @@
-<p id="profile"></p>
-
-<script>
-document.getElementById("profile").innerHTML = "Hello World, this is Shotolu Paul Oluwabukunmi email adderess paulshotolu@gmail.com with HGNi7 ID HNG-02125 using javascript for stage 2 task";
-</script>
+function paulfunc() {
+		   return ('Hello World, this Shotolu Paul Oluwabukumi with HNGi7 ID HNG-02125 using javascript for stage 2 task. paulshotolu@gmail.com');
+		}
+		console.log(paulfunc());
