5e0dcefde9fda9d9e36b9395dcd2171366b1b640
nwagodobed <51857252+nwagodobed@users.noreply.github.com>
2020-06-03 14:05:15 +0100

    Update HNG-02326.py
    I opened my forked repo on the patch-2 branch
    I clicked on the script folder to have access to my file
    I clicked on the pen icon on the right-hand corner to edit my file
    Finally, I clicked on the Commit changes button

--- scripts/HNG-02326.py
+++ scripts/HNG-02326.py
@@ -1,1 +1,2 @@
-print("Hello World, this is Nwachukwu Obed Onyekachi with HNGi7 ID HNG-02326 using python for stage 2 task, flush=True")
+newbie = "Hello World, this is Nwachukwu Obed Onyekachi with HNGi7 ID HNG-02326 and email nwagodobed24@gmail.com using python for stage 2 task"
+print(newbie, flush=True)
