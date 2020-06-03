75220ed2e4e0a3facfa5b2196160246c6d0f76b9
Rihsabmohd <65131390+Rihsabmohd@users.noreply.github.com>
2020-06-03 13:26:06 +0100

    Add files via upload

---
+++ scripts/HNG-02012.js
@@ -0,0 +1,5 @@
+let printHello = () => {
+	return "Hello world, this is Mohammed Bashir Oladimeji with HNGi7  ID HNG-02012 using javascript for stage 2 task. rihsabmohd@gmail.com";
+}
+
+printHello();
