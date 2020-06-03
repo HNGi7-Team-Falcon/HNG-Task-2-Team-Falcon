8079c78f9a34c1175294871aae437d5dcaba6082
chia gabriel <gabrielsonchia@gmail.com>
2020-06-03 11:18:13 +0100

    code refactored to conatin variables for each data needed

--- scripts/HNG-05448.js
+++ scripts/HNG-05448.js
@@ -1,4 +1,9 @@
-(function(){
-    const output = "Hello World, this is Chia Gabriel with HNGi7 ID HNG-05448 using JavaScript for stage 2 task";
+const name = "Chia Gabriel";
+const id = "HNG-05448";
+const email = "gabrielsonchia@gmail.com";
+const language = "JavaScript";
+
+(function(name,HNGId,email,language){
+    const output = `Hello World, this is ${name} with HNGi7 ID ${HNGId} and email ${email} using ${language} for stage 2 task`
     console.log(output);
-})();
+})(name,id,email,language);
