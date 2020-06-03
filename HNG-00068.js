6ba07864b6d6ba7579ed43ebb3258ed42384323d
Adeola <boluoguna@gmail.com>
2020-06-03 15:22:07 +0100

    added email address

--- HNG-00068.js
+++ HNG-00068.js
@@ -1,4 +1,4 @@
 const fullName = "Adeola Boluogun";
 const id = "HNG-00068"; 
 const language = "Javascript";
-console.log(`Hello World,this is ${fullName} with HNG ID ${id} using ${language} for stage 2 task`);
+console.log(`Hello World,this is ${fullName} with HNG ID ${id} using ${language} for stage 2 task.boluoguna@gmail.comm`);
