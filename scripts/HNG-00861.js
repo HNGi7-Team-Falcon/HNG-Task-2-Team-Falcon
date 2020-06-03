505d9998894c8bb840dd029a52d62d20d28fbdb3
Alao-Abiodun <abiodunDev@gmail.com>
2020-06-03 09:27:27 +0100

    initial-commit

--- scripts/HNG-00861.js
+++ scripts/HNG-00861.js
@@ -1,6 +1,6 @@
 const myHNGAccountDetails = () => {
     const fullName = `Alao Abiodun AbdulRahman`;
-    const hngID = `00861`;
+    const hngID = `HNG-00861`;
     const programmingLanguage = `JavaScript`;
     const myhngAccountDetails = `Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${programmingLanguage} for stage 2 task`;
     return myhngAccountDetails;
