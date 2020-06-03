1e1b4510f39fe7385802602b3ef3f151200d1a0b
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-03 09:46:07 +0100

    resolve notice

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	$template = "/Hello World, this is ([\w+\s]+) with HNGi7 ID (HNG-[\d]+) using (\w+) for stage 2 task/";
-	$idRegex = "/(HNG[-{0,}][\d]+)|(\d{2,}+)/";
+	$template = "/Hello World, this is ([\w+\s]+) with HNGi7 ID (HNG-[\d]+) using (\w+) for stage 2 task\s?/";
+	$idRegex = "/(HNG[-{0,}][\d]+)/";
 	# Retrive the runtime engine name
 	function getRuntime($fileName) {;
 
@@ -8,8 +8,13 @@
 		$supported_map = json_decode($supported_json, true); # convert to json object to work with
 
 		$tokens = explode(".", $fileName); // split file name into [fileName, extension];
-		$ext = $tokens[1]; // extension
-		$runtime = $supported_map[strtolower($ext)]; // Get the name of the runtime
+		$runtime;
+		if ($tokens) {
+			$ext = $tokens[1]; // extension
+			if ($ext) {
+				$runtime = $supported_map[strtolower($ext)]; // Get the name of the runtime
+			}
+		}
 
 		return $runtime;
 	}
@@ -37,24 +42,46 @@
 
 				$item["status"] = "fail";
 				$item["message"] = "unable to run script";
-				$item["fileName"] = $fileName;
 
 			} else {
 
-				preg_match_all($template, $output, $matches) === 1;
+				preg_match_all($template, $output, $matches);
 				preg_match($idRegex, $output, $idMatches);
 
-				$isMatched = $matches[0][0] === $output;
+				$isMatched;
+
+				$name;
+				$language;
+				$email;
+
+				if (!is_null($matches)){
+					if ($matches[0] && $matches[0][0]) {
+						$fullOutput = $matches[0][0];
+						if ($fullOutput) {
+							preg_match_all($template, $fullOutput, $matches2);
+
+							$name = $matches2[1][0];
+							$language = $matches2[3][0];
+
+							$item["name"] = $fullOutput;
+							$item["language"] = $language;
+							$isMatched = $matches[0][0] === $output;
+						}
+					}
+				}
 
 				if ($isMatched) {
-					$item["status"] = "success";
+					$item["status"] = "pass";
 				} else {
 					$item["status"] = "fail";
 				}
 
 				$item["output"] = $output;
-				$item["id"] = $idMatches[0];
+				if ($idMatches != null && $idMatches[0] != null) {
+					$item["id"] = $idMatches[0];
+				}
 			}
+			$item["fileName"] = $fileName;
 
 			array_push($data, $item);
 		}
