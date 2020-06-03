24be57b56c069986f4d1771edb95b6ccc6e91b93
wisestMe <wyhzest@gmail.com>
2020-06-03 16:24:23 +0100

    modify output message

--- scripts/HNG-03249.js
+++ scripts/HNG-03249.js
@@ -3,4 +3,4 @@ const ID = 'HNG-03249';
 const language = 'JavaScript';
 const email = 'wyhzest@gmail.com';
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ${ID} using ${language} for stage 2 task. ${email}`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email}`);
