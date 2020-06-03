f9d27adb4e97e8e3d685acc397a5bd9c1297d6ae
Sophie Obomighie <sophiecodes@gmail.com>
2020-06-03 20:28:12 +0100

    Updated the code base and changes to php from c#

---
+++ scripts/HNG-01621.php
@@ -0,0 +1,10 @@
+<?php
+$Full_Name = "Sophie Obomighie";
+$Id = "HNG-01621";
+$language = "php";
+
+$script = "Hello World, this is ".$Full_Name." with HNGi7 ID ".$Id."  using ".$language." for stage 2 task";
+
+echo $script;
+?>
+<?php
