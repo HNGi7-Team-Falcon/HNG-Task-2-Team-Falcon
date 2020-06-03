9e378cce49a63773efdf0e438ac607fe95e5fbc4
Gt-osa <64644665+Gt-osa@users.noreply.github.com>
2020-06-03 16:25:22 +0100

    Update HNG-01984.py
    I output my details using python programming language.

--- scripts/HNG-01984.py
+++ scripts/HNG-01984.py
@@ -1,1 +1,3 @@
-print("Hello World, This is Godstime Agholor with HNGi7 ID HNG-01984 using python for stage 2")
+details = "Hello World, this is Godstime Agholor with HNGi7 ID HNG-01984 using Python for stage 2 task. agholorgodstime18@gmail.com"
+
+print(details)
