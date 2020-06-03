18f8747819aea23186b9fdfa82676d27c61a7313
alishaJ-tech <66273195+alishaJ-tech@users.noreply.github.com>
2020-06-03 20:16:47 +0100

    Update hng04499.py

--- scripts/hng04499.py
+++ scripts/hng04499.py
@@ -10,8 +10,7 @@ d2 = my_dict.get("hng_id", " ")
 d3 = my_dict.get("language", " ")
 d4 = my_dict.get("email", " ")
 
-If "name" and "hng_id" and "language" and "email" and "status" in my_dict:
-   print("Hello world, this is " , d1, "with HNGi7 ID " , d2, "using " , d3, "for Stage 2 task. My mail is " , d4 )
+print("Hello world, this is " , d1, "with HNGi7 ID " , d2, "using " , d3, "for Stage 2 task. My mail is " , d4 )
    
 #using dictionaries
  
