6a3097ecec046d928eb9168a1f7fe2dc9ddc5eb7
tolulawson <55362043+tolulawson@users.noreply.github.com>
2020-06-04 15:25:33 +0100

    Submissions (#1131)
    * -- Task2 HNG-04084 - Return requested string
    
    * -- Log to console
    
    Co-authored-by: I F E DI L I ⚡ <ashawinta@gmail.com>

--- scripts/HNG-04084.js
+++ scripts/HNG-04084.js
@@ -1,1 +1,3 @@
-return "Hello World, this is Tolu Lawson with HNGi7 ID HNG-04084 using JavaScript for stage 2 task. lawsontolu1@gmail.com";
+
+console.log ("Hello World, this is Tolu Lawson with HNGi7 ID HNG-04084 using JavaScript for stage 2 task. lawsontolu1@gmail.com");
+
