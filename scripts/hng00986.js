3733819c7ca20283159f8f5d796bb07b889d5e5b
Christian Asarhasa <jamiesplendour@gmail.com>
2020-06-04 19:54:45 +0100

    Update hng00986.js

--- scripts/hng00986.js
+++ scripts/hng00986.js
@@ -7,7 +7,7 @@ var text2 = "with HNGi7 ID, ";
 var id = "HNG-00986 ";
 var text3 = "using ";
 var language = "Javascript ";
-var text4 = "for Stage 2 Task. ";
+var text4 = "for Stage 2 task. ";
 var email = "jamiesplendour@gmail.com.";
 
 console.log(text1 + fullName + text2 + id + text3 + language + text4 + email);
