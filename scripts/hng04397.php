7ff94640c14446623ab5d925c529fed84311c884
ifeanyi23 <57657242+ifeanyi23@users.noreply.github.com>
2020-06-04 09:04:39 +0100

    Update hng04397.php

--- scripts/hng04397.php
+++ scripts/hng04397.php
@@ -1,9 +1,11 @@
 <?php
 $fullname = "Okorie Joseph Ifeanyichukwu";
 $id = "HNG-04397";
-$language = "php";
+$email = "josephokorie32@gmail.com";
+$language = "PHP";
 
 
-echo "Hello World, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language. " for stage 2 task";
+echo "Hello World, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language. " for stage 2 task. " .$email
 
 ?>
+
