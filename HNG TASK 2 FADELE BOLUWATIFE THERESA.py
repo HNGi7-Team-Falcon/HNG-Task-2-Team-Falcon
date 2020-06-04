236266b0f742d578ff3518beb204691ffec74a48
Tess-hacker <64714004+Tess-hacker@users.noreply.github.com>
2020-06-04 21:11:34 +0100

    Created a variable and printed it.

--- HNG TASK 2 FADELE BOLUWATIFE THERESA.py
+++ HNG TASK 2 FADELE BOLUWATIFE THERESA.py
@@ -1,4 +1,2 @@
-greeting = "Hello World, this is Fadele Boluwatife Theresa with HNGi7 ID HNG - 02654 using Python for the Stage 2 task"
-email = "theresafadele@gmail.com"
+greeting = "Hello World, this is Fadele Boluwatife Theresa with HNGi7 ID HNG - 02654 using Python for the Stage 2 task. theresafadele@gmail.com "
 print (greeting)
-print (email)
