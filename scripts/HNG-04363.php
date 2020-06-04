df16abf9db5801175f53e89e6ea818a6e14645f1
Johnexzy <obajohn75@gmail.com>
2020-06-04 19:35:05 +0100

    Create HNG-04363.php

---
+++ scripts/HNG-04363.php
@@ -0,0 +1,7 @@
+<?php
+  $name = "Oba John";
+  $myid = "HNG-04363";
+  $myEmail = "obajohn75@gmail.com";
+  $myLangUsed = "PHP";
+  echo "Hello World, this is ".$name." with HNGi7 ID ".$myid." using ".$myLangUsed." for stage 2 task. ".$myEmail;
+?>
