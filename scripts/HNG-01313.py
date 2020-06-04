8ea46624eb2ab6199007f42683e4a0a9ecdb9858
Praise <opraise139@gmail.com>
2020-06-04 18:14:06 +0100

    removed hyphen in name

--- scripts/HNG-01313.py
+++ scripts/HNG-01313.py
@@ -3,8 +3,8 @@
 # CREATED BY ROTIMI
 ################################
 
-my_details = "Hello World, this is {full_name} with HNGi7 ID {id} using {language} for stage 2 task. {email}".format(
-	full_name = 'Bola-Rotimi Praise.O',
+my_details = "Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task. {email}".format(
+	name = 'Bola Rotimi Praise',
 	id = 'HNG-01313',
 	language = 'Python',
 	email = 'opraise139@gmail.com',
