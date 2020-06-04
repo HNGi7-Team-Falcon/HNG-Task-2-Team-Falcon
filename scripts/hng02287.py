da61bc56983f86a44173ce708a85bcf019e53b0c
Olamidun <kolapoolamidun@gmail.com>
2020-06-04 11:40:14 +0100

    edited my email address

--- scripts/hng02287.py
+++ scripts/hng02287.py
@@ -1,5 +1,5 @@
 name = "Kolapo Opeoluwa Olamidun"
 language = "Python"
-email_address = "Kolapo Opeoluwa Olamidun"
+email_address = "Kolapoolamidun@gmail.com"
 greeting = "Hello World, this {} with HNGi7 ID HNG-02287 using {} for stage 2 task. {}"
 print(greeting.format(name, language, email_address))
