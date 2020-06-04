e703bbf093a5af53f148a3232edab2da9da19518
Adebisi ayomide <38351524+Alsaheem@users.noreply.github.com>
2020-06-04 08:06:13 +0100

    Fixed errors

--- scripts/HNG-02946.py
+++ scripts/HNG-02946.py
@@ -4,5 +4,5 @@ hng_id = 'HNG-02946'
 email = 'adebisiayomide07@gmail.com'
 language ='python'
 
-print(' Hello World, this is {} with ID {}  a,nd email {}  using {} for the stage 2 task'.format(name,hng_id,email,language))
+print(' Hello World, this is {} with ID {}  and email {}  using {} for the stage 2 task'.format(name,hng_id,email,language))
 
