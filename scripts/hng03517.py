3566ad7735286678b18ea669771c17d89473128f
musharaf <sarafaolalekan7@gmail.com>
2020-06-04 17:33:36 +0100

    edited file to pass test

--- scripts/hng03517.py
+++ scripts/hng03517.py
@@ -1,9 +1,6 @@
-def printInfo(my_name, my_id, my_language, my_email_address):
-     print "Hello World ! \n"
-     print "My Name is : ", my_name
-     print "My Id is:", my_id
-     print "the Language used for the task is: ", my_language
-     print "My email Addressis :", my_email_address
-     return;
-     
-printInfo('Olalekan Sarafa','HNG-03517', 'Python', 'Sarafaolalekan7@gmail.com')
+def printInfo(my_name, my_id, my_language):
+    
+
+     print( "Hello World,","this is",my_name, "with HNGi7 ID", my_id, "using", my_language, "for stage 2 task");
+     return; 
+printInfo('OLALEKAN SARAFA','HNG-03517', 'Python');
