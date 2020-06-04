38f9e8969f0805fa1acb8c3062d2027c276314c9
caleb1-valo <65986150+caleb1-valo@users.noreply.github.com>
2020-06-04 23:42:10 +0100

    HNG-05620.js
    my task 2 file

---
+++ scripts/HNG-05620.js
@@ -0,0 +1,6 @@
+function hng(fullname,id,language,email) {
+	return('Hello World,this is' + " " + fullname +' '+ 'with HNGi7ID'+' '+ id +' '+ 'using' + ' ' + language +' '+ 'for stage 2 task.' + email 
+   )
+};
+let intern = hng('Ifedayo Valentine','HNG-05620','Javascript','vcaleb01@gmail.com');
+console.log(intern);
