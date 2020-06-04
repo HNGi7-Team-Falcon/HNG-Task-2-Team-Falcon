11b97789c5fe4b9b8c1e885e9064effb1363db6c
Mariam Omohsomi David-Alozie <51507155+virtuoushands@users.noreply.github.com>
2020-06-04 16:17:25 +0100

    Create HNG-05887.py
    I defined each variable using python programming language and that demands my putting each word/ phrase in quotation mark to define them as strings. I also used the print() output command to bring forth the result which is expected to be 
    :
    'Hello World, this is Mariam David-Alozie, ID: HNG-05887, email: virtuoushand@gmail.com. I am using python for stage 2 task.
    I  initially input my code on a python command and run it to be sure the result will be as expected. Thank you.

---
+++ scripts/HNG-05887.py
@@ -0,0 +1,7 @@
+greeting = ('Hello World this is')
+fullnames = ('Mariam David-Alozie')
+id = ('ID: HNG-05887')
+email = ('email:virtuoushand@gmail.com.')
+activity = ('I am using python for stage 2 task')
+instruction = [ greeting, fullnames, id, email, activity] 
+print(instruction)
