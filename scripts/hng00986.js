9ae7228174beebb1a969384273581866e18719e0
Christian Asarhasa <jamiesplendour@gmail.com>
2020-06-04 08:38:49 +0100

    Update hng00986.js
    Made corrections to var full name string

--- scripts/hng00986.js
+++ scripts/hng00986.js
@@ -2,7 +2,7 @@
 //Echo(Print) them at the end using the console.log() Function. Simple:
 
 var text1 = "Hello World, this is ";
-var fullName = "Christian Asarhasa ;
+var fullName = "Christian Asarhasa ";
 var text2 = "with HNGi7 ID, ";
 var id = "HNG-000986 ";
 var text3 = "using ";
