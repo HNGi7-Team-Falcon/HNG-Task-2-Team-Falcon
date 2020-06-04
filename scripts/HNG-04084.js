1083b96ed2721d15df56608cef0b9ca16735e9a4
Tolu Lawson <lawsontolu1@gmail.com>
2020-06-04 15:03:32 +0100

    -- Log to console

--- scripts/HNG-04084.js
+++ scripts/HNG-04084.js
@@ -1,1 +1,1 @@
-return "Hello World, this is Tolu Lawson with HNGi7 ID HNG-04084 using JavaScript for stage 2 task. lawsontolu1@gmail.com";
+console.log ("Hello World, this is Tolu Lawson with HNGi7 ID HNG-04084 using JavaScript for stage 2 task. lawsontolu1@gmail.com");
