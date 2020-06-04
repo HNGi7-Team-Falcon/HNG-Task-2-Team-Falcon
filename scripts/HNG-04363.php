4276c876c3eb51cdc89eed2fa658da2117380876
Johnexzy <obajohn75@gmail.com>
2020-06-04 19:39:33 +0100

    Update HNG-04363.php

--- scripts/HNG-04363.php
+++ scripts/HNG-04363.php
@@ -1,7 +1,3 @@
 <?php
-  $name = "Oba John";
-  $myid = "HNG-04363";
-  $myEmail = "obajohn75@gmail.com";
-  $myLangUsed = "PHP";
-  echo "Hello World, this is ".$name." with HNGi7 ID ".$myid." using ".$myLangUsed." for stage 2 task. ".$myEmail;
+  echo "Hello World, this is Oba John with HNGi7 ID HNG-04363 using PHP for stage 2 task. obajohn75@gmail.com";
 ?>
