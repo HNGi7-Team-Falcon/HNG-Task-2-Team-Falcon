50979c45ac9cb3e17eded90b6852a1cdeff1a73e
ifeanyi23 <57657242+ifeanyi23@users.noreply.github.com>
2020-06-04 09:44:04 +0100

    Created my first php script

---
+++ hng04397.php
@@ -0,0 +1,10 @@
+<?php
+$fullname = "Okorie Joseph Ifeanyichukwu";
+$id = "HNG-04397";
+$email = "josephokorie32@gmail.com";
+$language = "PHP";
+
+
+echo "Hello World, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language. " for stage 2 task. " .$email
+
+?>
