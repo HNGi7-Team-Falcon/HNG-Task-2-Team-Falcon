76d47d423e16f27e962b260badb12509c7395f90
Comfort <tinashenyati@yahoo.com>
2020-06-04 16:28:23 +0200

    Merge changes
    made changes to the email

--- scripts/hng03622.js
+++ scripts/hng03622.js
@@ -1,13 +1,10 @@
-<script>
 var fullName = "Comfort Nyatsine";
 var id = "HNG-03622";
 var email = "comfynyatsine@gmail.com";
 var language = "JavaScript";
 
 function myTask(){
-	        let stageTwo_Task = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
-        	return stageTwo_Task;
-}
+      return console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
+};
+myTask();
 
-console.log(myTask()) 
-</script>
