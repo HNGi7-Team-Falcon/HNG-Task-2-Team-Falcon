91196529bc7130de74a6cf202bf7a92d342205b0
keloncode <46829890+keloncode@users.noreply.github.com>
2020-06-04 09:20:00 -0700

    Add files via upload
     a recently updated file for commit

--- HNG-05278.js
+++ HNG-05278.js
@@ -1,7 +1,5 @@
-
- let myName ="this is Emmanuel Kel Obi"
+let myName ="this is Emmanuel Kel Obi"
  let myId = "HNG-05278"
- let myEmail = "emmanuelobika5@gmail.com"
- let myData = "Hello World," + myName + "," + myId + ','+ myEmail+ "," +"using javascript for step 2 task."
+ let myData = "Hello World," + myName + " with " + myId + " using javascript for step 2 task."
 
   console.log(myData);
