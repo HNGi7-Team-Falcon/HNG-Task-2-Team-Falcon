6ab920ed34b5f527378b527138f17961337d72bd
Anna Ugwu <annakelechi@gmail.com>
2020-06-04 10:32:58 -0700

    Update HNG-02989.py
    included email

--- HNG-02989.py
+++ HNG-02989.py
@@ -1,5 +1,6 @@
 full_name = "Ugwu Anna Kenechi"
 id_name = "HNG-02989"
 language_name = "Python"
+e_mail = "annakelechi@gmail.com"
 
-print(f"Hello World, this is {full_name} with HNGi7 ID {id_name} using {language_name} for stage 2 task", flush = True)
+print(f"Hello World, this is {full_name} with HNGi7 ID {id_name} using {language_name} for stage 2 task. {e_mail}", flush = True)
