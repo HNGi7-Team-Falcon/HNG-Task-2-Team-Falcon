c36ade380de75b4f63b823d6ccb7f25cc7fb9bab
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-04 16:31:14 +0100

    chore

--- index.php
+++ index.php
@@ -264,7 +264,6 @@
 				?>
 			</tbody>
 		</table>
-			.$bootstrap
 		</div>
 	</body>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
