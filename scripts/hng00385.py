96da17b681dd2818a5857859b02425136054477f
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-06-04 13:41:54 +0100

    Bug: formatted my strrings properly

--- scripts/hng00385.py
+++ scripts/hng00385.py
@@ -1,7 +1,6 @@
-hello = 'Hello World!'
 name = 'Oluwadamilola Sonaike'
 id = 'HNG-00385'
 pro_lang = 'Python'
 email = 'damiso15@yahoo.com'
 
-print(f'{hello} This is {name} with HNGi7, my ID is {id}. I am using {pro_lang} for my stage 2 task.')
+print(f'Hello World, this is {name} with HNGi7 ID {id} using {pro_lang} for stage 2 task. {email}')
