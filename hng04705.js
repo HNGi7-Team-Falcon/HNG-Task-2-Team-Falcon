3b57de0a62b66a935f6d13aa5ca3165e1cfb6922
Apetuezekiel <62860477+Apetuezekiel@users.noreply.github.com>
2020-06-04 17:11:22 +0100

    Add files via upload
    HNG-04705 submission
    This is my updated submission. i ran tests on my previous one and it failed. This however works just fine

---
+++ hng04705.js
@@ -0,0 +1,12 @@
+const myName = "Apetu Olayori Ezekiel";
+const id = "HNG-04705";
+const spec_language = "JavaScript";
+const email = "apetuzee@gmail.com";
+
+(function () {
+  console.log(
+    `Hello World, this is ${myName} with HNGi7 ID ${id} using ${spec_language} for stage 2 task. ${email}`
+  );
+})();
+
+/* I created a const variable amd binded an object containinf all the necessary personal information as requested in the task directives. I then created an IIFE (immediately invoked function expression) that logs out a concatenated string of all the myInfo object information in one meaningful senntence*/
