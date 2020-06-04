54d74dbcbb1712c2ee0cb60e756c6f717a69fc08
Ifeanyi <doc3coder@gmail.com>
2020-06-04 19:13:55 +0100

    HNG 05138 Submission for Ifeanyi

--- scripts/HNG-05138.js
+++ scripts/HNG-05138.js
@@ -1,4 +1,8 @@
-const Hng_task2 = () =>
-  "Hello World, this is Ifeanyi Muogbo with HNGi7 ID HNG-05138 using Javascript for stage 2 task. ifytechspace@gmail.com";
-
+const Hng_task2 = () => {
+  let fullName = "Ifeanyi Muogbo";
+  let id = "HNG-05138";
+  let language = "Javascript";
+  let email = "ifytechspace@gmail.com";
+  return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+};
 console.log(Hng_task2());
