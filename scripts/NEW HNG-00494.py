d171412c768a34e497d92aac7fe0463dd69a20d7
Vivyanne-504 <66225228+Vivyanne-504@users.noreply.github.com>
2020-06-04 18:30:43 +0100

    Update NEW HNG-00494.py

--- scripts/NEW HNG-00494.py
+++ scripts/NEW HNG-00494.py
@@ -3,4 +3,4 @@ HNG_ID='HNGi7 ID HNG-00494'
 language= 'python '
 email= 'viviannwosu05@gmail.com'
 print('Hello World, this is '+ full_name + ' with ' + HNG_ID
-      +' using ' + language + 'for stage 2 task.' + email)
+      +' using ' + language + ' for stage 2 task.' + email)
