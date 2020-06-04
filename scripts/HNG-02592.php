5c2824974f97b60facdd0cf67d2a72c05ef5df6d
Olatunji <o.omogbemile@gmail.com>
2020-06-04 18:16:29 +0100

    My Stage 2 task corrected.

--- scripts/HNG-02592.php
+++ scripts/HNG-02592.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stage 2 Assignment</title>
-</head>
-
-<body>
 
     <?php
-
 
     // variable declaration and Initializing
 
@@ -19,15 +9,9 @@
     $email = "o.omogbemile@gmail.com";
     $language = "php";
     ?>
-    <div>
+
         <?php //call variables into output text
+
+        echo "Hello World, this is " . $names . " with HNGi7 ID: " . $id . " using " . $language . " for my stage 2 task. My email is : " . $email . "";
+
         ?>
-        <h1> <?php echo "Hello World! <br> My name is " . $names . " with " . $id . " using " . $language . " for stage 2 task. <br> This is my email: " . $email . "";
-                ?> </h1>
-    </div>
-
-
-
-</body>
-
-</html>
