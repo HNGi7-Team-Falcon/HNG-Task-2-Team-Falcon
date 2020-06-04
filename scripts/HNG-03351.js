08ea70ebd483b40eb4a52f2df6d37ca2bfe7624c
Teedari <godfredderi47@gmail.com>
2020-06-04 02:31:30 +0000

    submission HNG-03351

---
+++ scripts/HNG-03351.js
@@ -0,0 +1,9 @@
+const fullname = 'Godfred Dari';
+const language = 'Javascript';
+const email = 'godfredderi47@gmail.com';
+const HNG_ID = 'HNG-03351';
+
+
+console.log(
+  `Hello World, this is ${fullname} with HNGi7 ID ${HNG_ID} using ${language} for stage 2 task. ${email}`
+);
