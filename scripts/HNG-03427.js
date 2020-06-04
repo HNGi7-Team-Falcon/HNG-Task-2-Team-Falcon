f5486e692cfa9ba67c72767fd73f94a7bc503123
cpscott1 <thacam18gmail.com>
2020-06-04 03:36:52 -0500

    testing

--- scripts/HNG-03427.js
+++ scripts/HNG-03427.js
@@ -1,3 +1,3 @@
-const script = 'Hello world, this is Cameron Perry with HNGi7 ID 03427 using JS for stage 2 task djcam25@gmail.com'
+const script = "Hello world, this is Cameron Perry with HNGi7 ID 03427 using JS for stage 2 task djcam25@gmail.com"
 
 console.log(script)
