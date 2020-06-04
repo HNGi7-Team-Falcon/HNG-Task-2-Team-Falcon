83ef6394c6c1c5e389acb7ffbefb0d42493f3fa0
Olamidun <kolapoolamidun@gmail.com>
2020-06-04 01:05:59 +0100

    corrected ID format in hng02287.py file

--- scripts/hng02287.py
+++ scripts/hng02287.py
@@ -1,2 +1,5 @@
-greeting = "Hello World, this Kolapo Opeoluwa Olamidun with HNGi7 ID 02287 using Python for stage 2 task. kolapoolamidun@gmail.com"
-print(greeting)
+name = "Kolapo Opeoluwa Olamidun"
+language = "Python"
+email_address = "Kolapo Opeoluwa Olamidun"
+greeting = "Hello World, this {} with HNGi7 ID HNG-02287 using {} for stage 2 task. {}"
+print(greeting.format(name, language, email_address))
