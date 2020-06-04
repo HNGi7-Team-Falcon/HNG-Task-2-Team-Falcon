9527a9bab7fe99432e31f1c27ab553780dd44a62
kareem fawas <phawazzzy@gmail.com>
2020-06-04 04:27:34 +0100

    corrections

--- scripts/HNG-01154.js
+++ scripts/HNG-01154.js
@@ -3,4 +3,4 @@ const HNGID = 'HNG-01154';
 const email ='ayomideadebogun@gmailcom';
 const language = 'Javascript';
 
-console.log(`Hello World,this is ${fullName} with HNGi7 ID ${HNGID} using ${language} for stage 2 task.${email}`);
+console.log(`Hello World,this is ${fullName} with HNGi7 ID ${HNGID} using ${language} for stage 2 task. ${email}`);
