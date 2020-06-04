a7d95ab5ac934db77e1729163fcbec3c587df127
Balmhiz <66218908+Balmhiz@users.noreply.github.com>
2020-06-04 16:58:20 +0100

    updated HNG-01537
    Completed stage 2 task for team Falcon

--- HNG-01537.js
+++ HNG-01537.js
@@ -1,7 +1,8 @@
-var fullname = "Bello Mueez Omobolaji";
-var ID = "HNG-01537";
-var language = "Javascript";
-var email = "omobolajimuhammedbello@gmail.com";
-var result = "Hello World, this is ${fullname} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email}";
+const fullname = 'Bello Mueez';
+const ID = 'HNG-01537';
+const language = 'Javascript';
+const mail = 'omobolajimuhammedbello@gmail.com';
+
+const result = 'Hello World, this is ' + fullname + ' with HNGi7 ID '+ ID + ' using ' + language + ' for stage 2 task. ' + mail;
 
 console.log(result);
