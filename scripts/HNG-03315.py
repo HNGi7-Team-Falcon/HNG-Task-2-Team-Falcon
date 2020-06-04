dc9571a590db4cc6320f9774d41aa800c4364721
youngGaf <youngabuhafiz@gmail.com>
2020-06-04 10:15:44 +0100

    updated task 2

--- scripts/HNG-03315.py
+++ scripts/HNG-03315.py
@@ -3,7 +3,7 @@ def introduction():
     ID = "HNG-03315"
     language = "Python"
     email = "abdulateef.gafar@gmail.com"
-    my_intro = "Hello world, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task. " + email
+    my_intro = "Hello World, this is " + name + " with HNGi7 ID " + ID + " using " + language + " for stage 2 task. " + email
     return my_intro
 
-print(introduction(), flush=True)
+print(introduction())
