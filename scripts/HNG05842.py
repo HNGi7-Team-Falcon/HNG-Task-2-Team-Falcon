a3d3f147661491b3d8d3f4d7db203a4be37ad95a
Lawrence Ayobami <58933502+SirLaw-commits@users.noreply.github.com>
2020-06-04 12:52:23 +0100

    Update HNG05842.py

--- scripts/HNG05842.py
+++ scripts/HNG05842.py
@@ -1,1 +1,1 @@
-print("Hello Wold, this is Lawrence Ayobami with HNG05842 using python for stage 2. ayobamilawrence@gmail.com ")
+print("Hello Wold, this is Lawrence Ayobami with HNGi7 ID HNG-05842 using python for stage 2 task. ayobamilawrence@gmail.com ")
