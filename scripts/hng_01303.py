a4280c498a2b07ffa50e80ac4fe15aa4ba5531ac
astroyummy <66243974+astroyummy@users.noreply.github.com>
2020-06-04 19:45:44 +0530

    Update hng_01303.py
    UPDATED USING VARIABLES

--- scripts/hng_01303.py
+++ scripts/hng_01303.py
@@ -1,1 +1,7 @@
-print("Hello world,this is Kankanala Yamini with HNGi7 ID HNG-01303 using PYTHON language for stage 2 task.yaminichowdary11111999@gmail.com")
+#Hello,this is Yamini
+#Task:stage 2 assignment 
+uname="Kankanala Yamini"
+identification="HNG-01303"
+lan="python"
+email="yaminichowdary11111999@gmail.com"
+print("Hello world,this is",+uname +"with HNGi7 ID" +identification +"using" +lan +"for stage 2 task." +email)
