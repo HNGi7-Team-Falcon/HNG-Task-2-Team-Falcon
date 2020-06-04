106608b4a7df20069c4fed951c5252ca0877e907
meduna femi <femimeduna@gmail.com>
2020-06-04 19:56:53 +0100

    fixed error

--- scripts/HNG-01155.js
+++ scripts/HNG-01155.js
@@ -3,7 +3,7 @@ function introduction() {
   let id = "HNG-01155";
   let email = "femimeduna@gmail.com";
   let language = "Javascript";
-  let intro = `Hello World this is ${myName} with HNGI7-ID ${id} using ${language} for stage 2 task.${email}`;
+  let intro = `Hello World, this is ${myName} with HNGI7-ID ${id} using ${language} for stage 2 task.${email}`;
 
   return intro;
 }
