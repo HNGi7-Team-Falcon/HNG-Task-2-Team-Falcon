faa2ccf29540e7e7f456b891de44b2f34b5a98ea
Comfort <tinashenyati@yahoo.com>
2020-06-04 10:29:57 +0200

    Making a correction to the "World"
    I had used a small letter w instead of 'W'

--- scripts/HNG-03622.js
+++ scripts/HNG-03622.js
@@ -1,12 +1,12 @@
 <script>
 var fullName = "Comfort Nyatsine";
-var HNGi7_ID = "HNG-03622";
+var id = "HNG-03622";
 var email = "comfynyatsine@gmail.com";
 var language = "JavaScript";
 
 function myTask(){
-		let stageTwo_Task = "Hello world, this is " + fullName + " with HNGi7 ID " + HNGi7_ID + " using " + language + " for stage 2 task"; 
-        return stageTwo_Task;
+	        let stageTwo_Task = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+        	return stageTwo_Task;
 }
 
 console.log(myTask()) 
