46354d8b27ae9601d4b9f11d17bd7ae250f36203
smartprincipal <52776101+smartprincipal@users.noreply.github.com>
2020-06-04 22:10:28 +0100

    Create HNG-06672.php
    Second task submission

---
+++ scripts/HNG-06672.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>HNG-06672</title>
+</head>
+<body>
+
+<?php 
+echo "<Hello World, this is Johnson Oluwayemi with HNGi7 ID HNG-06672 using PHP for stage 2 task. johnsonoluwayemibunmi@gmail.com";
+?>
+  
+
+</body>
+</html>
