39a33695b0c84939992aa433cff5e5d74f981ee2
Emanuel1997 <omisakin205@gmail.com>
2020-06-04 14:51:43 +0100

    Update hng05811.php

--- scripts/hng05811.php
+++ scripts/hng05811.php
@@ -1,10 +1,7 @@
 <?php
-$name = "Omisakin Oluwapelumi";
-$id = "05811";
-$language = "PHP";
-$email = "omisakin205@gmail.com";
-echo "Hello World,
-this is $name,
-with HNGi7 ID {$id},
-using {$language}  with email {$email} for stage 2 task";
+$name = 'Omisakin Oluwapelumi';
+$id = 'HNG-05811';
+$language = 'PHP';
+$email = 'omisakin205@gmail.com';
+echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task. {$email}";
 ?>
