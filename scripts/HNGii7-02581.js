3ee29c09a1d8dab8e4c03c5744c0df6a5dff5e02
Emmanuel Chijioke <manuelckay@gmail.com>
2020-06-04 14:50:58 +0100

    Delete this please

--- scripts/HNGii7-02581.js
+++
@@ -1,8 +0,0 @@
-var fullName = " Emmanuel Chijioke";
-var hngiid = " HNG-02581 ";
-var email = newFunction()
-var language = "javascript";
-function newFunction() {
-	return " manuelckay@gmail.com";
-}
-	console.log("Hello World, this is " + fullName ", " + "with HNG ID " + hngiid, " using " + language, " for stage 2 task.")
