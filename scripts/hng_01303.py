dcc83df89dc80090eb21cd171acdd0a3e20b526e
astroyummy <66243974+astroyummy@users.noreply.github.com>
2020-06-04 14:09:08 +0530

    Update hng_01303.py

--- scripts/hng_01303.py
+++ scripts/hng_01303.py
@@ -1,1 +1,1 @@
-print("Hello world,this is Kankanala Yamini with HNGi7 ID HNG-01303 using Python language for stage 2 task.yaminichowdary11111999@gmail.com")
+print("Hello world,this is Kankanala Yamini with HNGi7 ID HNG-01303 using PYPYTHON language for stage 2 task.yaminichowdary11111999@gmail.com")
