2ed88b19c3cf7d5db494f552b7b42ad8b3d359f8
ikeadeoyin <ikeadeoyin@gmail.com>
2020-06-04 23:55:14 +0100

    my task 2 submission

---
+++ scripts/HNG-01506.py
@@ -0,0 +1,11 @@
+def task_2():
+    name = "Oyindamola Olatunji"
+    HNG_ID = "HNG-01506"
+    language = "Python"
+    email= "ikeadeoyin@gmail.com"
+    info ="Hello World, this is " + name + " with HNGi7 ID " + HNG_ID + " using " + language + " for stage 2 task." + email
+    print(info)
+
+
+task_2()
+    
