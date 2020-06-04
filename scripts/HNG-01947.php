ab5643abc26aba3425f4a7c3e1de0989f60df1fe
Oluwasegun Ibidokun <segunibidokun@gmail.com>
2020-06-04 19:57:55 +0100

    Refactored output string to the required format

--- scripts/HNG-01947.php
+++ scripts/HNG-01947.php
@@ -4,6 +4,6 @@ $name = 'Oluwasegun Ibidokun';
 $id = 'HNG-01947';
 $language = 'PHP';
 $email = 'segunibidokun@gmail.com';
-echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task my{$email} email";
+echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task". $email;
 
 ?>
