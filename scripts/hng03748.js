876903d61613fb608ef5290f22cff1d903b54589
Dellyson <isiakalukmandellyson@gmail.com>
2020-06-04 12:57:41 +0100

    Task updated

--- scripts/hng03748.js
+++ scripts/hng03748.js
@@ -1,4 +1,4 @@
-function intro(name, id, lang) {
-  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
+function intro(name, id, lang, email) {
+  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task. Email ${email}`;
 }
-console.log(intro("Lukman Isiaka", "HNG-03748", "JavaScript"));
+console.log(intro("Lukman Isiaka", "HNG-03748", "JavaScript", "isiakalukmandellyson@gmail.com"));
