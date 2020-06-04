106612f603f17d287e0c5cf7e41ad1023dfca8e4
milindkrishna <54195987+milindkrishna@users.noreply.github.com>
2020-06-04 10:44:04 +0530

    Create hng00344.php

---
+++ scripts/hng00344.php
@@ -0,0 +1,10 @@
+<? php
+
+$name = " Milind krishna " ;
+$hngid = " 00344 ";
+$language = " PHP " ;
+$mail = " milindkrishna69233@gmail.com " ;
+$message = "Hello World! this is". $name. "with hngi7id =". $hngid."using". $language. "for stage two task".$mail;
+echo $message ;
+
+?>
