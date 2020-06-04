a142fb442faf95cb2028e6149b390bc065fb0d41
adah <adaugooku01@gmail.com>
2020-06-04 08:33:39 +0100

    Changed from document.write to console.log

--- scripts/hng05293.js
+++ scripts/hng05293.js
@@ -1,1 +1,2 @@
-document.write("<h3>Hello World, this is Oku Emmanuella Adaugo with HNGi7 ID 05293 using javascript for stage 2 task. <br> You can also send me a mail via adaugooku01@gmail.com.<h3>");
+console.log("<h3>Hello World, this is Oku Emmanuella Adaugo with HNGi7 ID 05293 using javascript for stage 2 task. <br> You can also send me a mail via adaugooku01@gmail.com.<h3>");
+
