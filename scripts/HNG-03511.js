047e81ffb88e087d24cddf00f9527b58813678e3
Josh Anderson <joshiandersonk69@gmail.com>
2020-06-04 18:11:30 +0100

    JoshAndy-HNG-03511 javascript

--- scripts/HNG-03511.js
+++ scripts/HNG-03511.js
@@ -1,3 +1,4 @@
+var name  = 'Josh Anderson';
+var email = 'joshiandersonk69@gmail.com';
 
-
-console.log("Hello World, this is Josh Anderson, with HNGi7 iD HNG-03511 using javascript for stage 2 task. joshiandersonk69@gmail.com");
+console.log(`Hello World, this is ${name}, with HNGi7 iD HNG-03511 using javascript for stage 2 task, ${email}`);
