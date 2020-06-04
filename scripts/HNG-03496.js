bd4d73ad16d0d4cec2e71d89a6976ebd8e2597c2
Jonathan Enudeme <47423105+Joefamouze@users.noreply.github.com>
2020-06-04 09:55:53 +0100

    modified javascript file

--- scripts/HNG-03496.js
+++ scripts/HNG-03496.js
@@ -7,5 +7,4 @@ let language = "javascript";
 let email = "twoejoe5@gmail.com";
 
 let task = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email} `;
-
 console.log(task);
