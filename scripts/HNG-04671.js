07574ac765c73a63bc5a4b3cfc99c5ce2453a169
Noble Mungu <mungunoble@gmail.com>
2020-06-04 13:20:55 +0300

    update task 2

--- scripts/HNG-04671.js
+++ scripts/HNG-04671.js
@@ -3,5 +3,5 @@ let name, id, email, language;
   id = "HNG-04671";
   email = "mungunoble@gmail.com";
   language = "Javascript";
-  let read= `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}";
-  console.log(read)
+  let output= `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}";
+  console.log(output)
