0e5a37c522a7c49208398ad3c52da3419530ea5d
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2020-06-04 16:24:48 +0100

    Corrected my Script

--- scripts/HNG-04569.py
+++ scripts/HNG-04569.py
@@ -1,2 +1,3 @@
 email = "ayomilotunde02@gmail.com"
-print("Hello World, this is Adeoye Ayomide with HNGi7 ID HNG-04569 using Python for stage 2 task. Email:"+email)
+
+print("Hello World, this is Adeoye Ayomide with HNGi7 ID HNG-04569 using Python for stage 2 task. Email:" + email)
