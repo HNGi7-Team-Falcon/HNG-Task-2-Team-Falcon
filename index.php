e2e6d52861cfc61fbe4a74dae5e4418eb264c7cb
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-04 04:11:30 +0100

    modified app, using new code

--- index.php
+++ index.php
@@ -1,245 +1,225 @@
 <?php
+	$template = "/^Hello World, this is [\w\s]+ with HNGi7 ID HNG-\d{1,} using \w.* for stage 2 task/";
+	$idRegex = "/(HNG[-{0,}][\d]+)/";
+	$emailRegex = "/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/i";
+	$languageRegex = "/using \[{0,1}(\w+[^\s]+)/i";
+	$nameRegex = "/this is \[{0,1}([\w+,\s]+)]{0,1} with/i";
 
-$url = $_SERVER['REQUEST_URI'];
-$response_type = substr(strrchr($url, "?"), 1);
+	$supported_json = '{
+		"py": "python",
+		"js": "node",
+		"php": "php",
+		"rb": "irb",
+		"java": "java",
+		"kt": "kotlinc",
+		"kts": "kotlinc",
+		"dart": "dart"
+	}'; # currently supported types should be updated
+	$supported_map = json_decode($supported_json, true); # convert to json object to work with
 
-$directory = 'scripts';
+	# Retrive the runtime engine name
+	function getRuntime($fileName) {;
+		global $supported_map;
 
-function get_string_between($string, $start, $end){
-    $string = ' ' . $string;
-    $ini = strpos($string, $start);
-    if ($ini == 0) return '';
-    $ini += strlen($start);
-    $len = strpos($string, $end, $ini) - $ini;
-    return substr($string, $ini, $len);
-}
+		$tokens = explode(".", $fileName); // split file name into [fileName, extension];
+		if (isset($tokens[1])) {
+			$ext = $tokens[1]; // extension
+			if ($ext && isset($supported_map[strtolower($ext)])) {
+				$runtime = $supported_map[strtolower($ext)]; // Get the name of the runtime
+				return $runtime;
+			}
+		}
+
+		return null;
+	}
+ 
+
+	$path = "scripts";
+	$files = scandir($path);
+
+	$data =  array();
+	$totalCount = 0;
+	$passCount = 0;
+	$failCount = 0;
+
+	foreach ($files as $key => $fileName) {
+
+		$filePath = "./$path/$fileName";
+
+		if (!is_dir($filePath)) {
+			$item = array();
+			$totalCount++;
+
+			$runtime = getRuntime("$fileName");
+
+			// echo $fileName;
+			if ($runtime) {
+
+				set_time_limit(5);
+				$output = shell_exec("$runtime $filePath 2>&1"); # Execute script and assign result
+				if (is_null($output)) {
+
+					$item["status"] = "fail";
+					$item["output"] = "unable to run script";
+					$item["name"] = $fileName;
+					$failCount++;
+
+				} else {
+
+					$isMatched = preg_match($template, $output, $matches) === 1;
+
+					if ($isMatched) {
+						$item["status"] = "pass";
+						$item["output"] = $matches[0];
+						$passCount++;
+					} else {
+						$item["status"] = "fail";
+						$item["output"] = $output;
+						$failCount++;
+					}
+
+				}
+				// extract id
+				preg_match($idRegex, $output, $idMatches);
+				if (isset($idMatches[0])) {
+					$item["id"] = $idMatches[0];
+				}
+
+				// extract name 
+				preg_match($nameRegex, $output, $nameMatches);
+				if (isset($nameMatches[1])) {
+					$item["name"] = $nameMatches[1];
+				} else {
+					$item["name"] = $fileName;
+				}
+
+				// extract language
+				preg_match($languageRegex, $output, $languageMatches);
+				if (isset($languageMatches[1])) {
+					$item["language"] = $languageMatches[1];
+				}
 
 
-function extract_email($string) {
-    preg_match('/\b[^\s]+@[\w\d.-]+/', $string, $match);
-    return $match[0];
-}
+				// extract email
+				preg_match($emailRegex, $output, $emailMatches);
+				if (isset($emailMatches[0])) {
+					$item["email"] = $emailMatches[0];
+				}
 
-// str_replace($search, $replace, $subject)
+			} else {
+				$item["name"] = $fileName;
+				$item["output"] = "File not supported";
+				$item["status"] = "fail";
+			}
 
-$files = array_slice(scandir($directory),3);
-$length = count($files);
-$noOfPasses = 0;
-$noOfFailures = 0;
+			// fileName
+			$item["fileName"] = $fileName;
+
+			array_push($data, $item);
+		}
+	}
+
+	$isJson = false;
+	if(isset($_SERVER["QUERY_STRING"])) {
+	 	$queryStr = $_SERVER["QUERY_STRING"];
+	 	$isJson = $queryStr == "json";
+	}
+
+	if ($isJson) {
+		header("Content-Type: application/json");
+		echo json_encode($data);
+	} else {
+		htmlFormat($data);
+	}
+
+	function htmlFormat($data) {
+		global $totalCount;
+		global $passCount;
+		global $failCount;
+
+		$bootstrap = '
+				<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+				<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>';
+
+		$head = '
+		<head>
+			<title>Team Falcon</title>
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		</head>
+		';
+
+		$rows = getRows($data);
+
+		$table = '
+		<table class="table table-bordered table-hover">
+			<thead class="thead-dark">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Name</th>
+					<th scope="col">Output</th>
+					<th scope="col">Status</th>
+				</tr>
+			</thead>
+			<tbody>'
+				.$rows.
+			'</tbody>
+		</table>';
+
+		$stats = '
+		<h1 class="text-center">Team Falcon</h1>
+		<table class="table">
+			<thead>
+				<tr class="text-center">
+					<th scope="col">Submitted</th>
+					<th scope="col">Passed</th>
+					<th scope="col">Failed</th>
+				</tr>
+			</thead>
+			<tbody>'
+			.'<tr>'
+				.'<td class="col-4 table-info text-center">'.$totalCount.'</td>'
+				.'<td class="col-4 table-success text-center">'.$passCount.'</td>'
+				.'<td class="col-4 table-danger text-center">'.$failCount.'</td>'
+			.'</tr>'
+			.'</tbody>
+		</table>';
+
+		echo "
+		<!DOCTYPE html>
+		<html>"
+			.$head
+			."<body>
+					<div class=container>"
+				.$stats
+				.$table
+				.$bootstrap
+				."</div>
+			</body>
+		</html>";
+	}
+
+	function getRows($items) {
+		$rows = "";
+		foreach ($items as $key => $item) {
+			$row = getRow($item); 
+			$rows = $rows.$row;
+		}
+		return $rows;
+	}
+
+	$counter = 0;
+	function getRow($item) {
+		global $counter;
+		$fail = $item["status"] == "fail";
+		$class = $fail ? "text-danger" : "'text-success'";
+		$counter++;
+		return "<tr>"
+			."<th scope='row'>".$counter."</th>"
+			."<td class=".$class.">".$item["name"]."</td>"
+			."<td><samp>".htmlspecialchars($item["output"])."</samp></td>"
+			."<td class=".$class.">".strtoupper($item["status"])."</td>"
+			."</tr>";
+	}
 
 ?>
-
-<?php
-if ($response_type == 'json') {
-    $result = array();
-    $result['length'] = $length;
-
-
-    foreach (glob($directory . "/*.*") as $file) {
-
-
-        # Get file Extension
-        $filename = str_ireplace('scripts/', '', $file);
-        $ext = strtolower(substr(strrchr($filename, "."), 1));
-
-        # Command for different
-        if ($ext == 'php') {
-            $response = exec("$ext $file 2>&1", $output);
-        } else if ($ext == 'js') {
-            $response = exec("node $file 2>&1", $output);
-        } else if ($ext == 'py') {
-            $response = exec("python $file 2>&1", $output);
-        } else {
-            $reason = 'Invalid file type';
-        }
-
-        $internName = trim(get_string_between($response, 'this is', 'with'));
-        $internID = $id = trim(get_string_between($response, 'ID', 'using'));
-        $language = trim(get_string_between($response, 'using', 'for'));
-        $email = extract_email($response);
-
-        $newResponse = (string)str_replace($email, "", $response);
-
-        # Check status of response
-        $passCondition1 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task ";
-        $passCondition2 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task.";
-        $passCondition3 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task";
-        $passCondition4 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task. ";
-
-        if (($passCondition1 == $newResponse) || ($passCondition2 == $newResponse) || ($passCondition3 == $newResponse) || ($passCondition4 == $newResponse)) {
-            $status = 'pass';
-            $noOfPasses++;
-        } else {
-            $noOfFailures++;
-            $status = 'fail';
-        }
-
-
-        $result['Pass'] = $noOfPasses;
-        $result['Fail'] = $noOfFailures;
-        $data = new stdClass();
-        //$data->file = $value;
-        $data->output = $newResponse;
-        $data->owner = $internName;
-        $data->id = $internID;
-        $data->email = $email;
-        $data->language = $language;
-        $data->status = @$status;
-        $result[] = $data;
-
-    }
-    echo json_encode($result, JSON_PRETTY_PRINT);
-    die();
-}
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <title>Team Falcon</title>
-
-    <style>
-        #btn {
-            border: 1px solid #ddd;
-        }
-
-    </style>
-
-</head>
-<body>
-
-<div class="container">
-    <div class="row mt-5">
-        <div class="col-lg-4">
-            <button class="btn btn-light badge-primary" id="btn">Submitted <span id="badge"><?php echo $counter; ?></span></button>
-        </div>
-        <div class="col-lg-4">
-            <button class="btn btn-light badge-success" id="btn">Passes  <span id="badge"><?php echo $passed; ?></span></button>
-        </div>
-        <div class="col-lg-4">
-            <button class="btn btn-light badge-danger" id="btn">Fails <span id="badge"><?php echo $failed; ?></span></button>
-        </div>
-    </div>
-    <div class="row mt-2">
-        <div class="col-lg-12">
-            <h3 class="text-center">Team Falcon</h3>
-            <div class="table-responsive">
-                <table class="table table-bordered">
-                    <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Message</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Comments</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    $counter = 0;
-                    $passed = 0;
-                    $failed = 0;
-                    foreach (glob($directory."/*.*") as $file)  {
-                        $counter++;
-
-                        # Get file Extension
-                        $filename = str_ireplace('scripts/','',$file);
-                        $ext = strtolower(substr(strrchr($filename, "."), 1));
-
-                        # Command for different
-                        if ($ext == 'php') {
-                            $response = exec("$ext $file 2>&1", $output);
-                        } else if ($ext == 'js') {
-                            $response = exec("node $file 2>&1", $output);
-                        } else if ($ext == 'py') {
-                            $response = exec("python $file 2>&1", $output);
-                        } else {
-                            $reason = 'Invalid file type';
-                        }
-
-                        $internName = trim(get_string_between($response, 'this is', 'with'));
-                        $internID = trim(get_string_between($response, 'ID', 'using'));
-                        $language = trim(get_string_between($response, 'using', 'for'));
-                        $email = extract_email($response);
-
-                        $newResponse = (String) str_replace($email, "",$response);
-
-                        # Check status of response
-                        $passCondition1 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task ";
-                        $passCondition2 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task.";
-                        $passCondition3 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task";
-                        $passCondition4 = "Hello World, this is {$internName} with HNGi7 ID {$internID} using {$language} for stage 2 task. ";
-
-
-                        if (($passCondition1 == $newResponse) || ($passCondition2 == $newResponse) || ($passCondition3 == $newResponse) || ($passCondition4 == $newResponse)) {
-                            $stat = 'pass';
-
-                            if (strpos($internID, 'HNG-') !== false) {
-                                $status = '<b class="text-success">pass</b>';
-                                $passed++;
-                                $stat = 'pass';
-
-                            } else {
-                                $message = '<b class="text-danger">Add HNG to your ID</b>';
-                                $status = '<b class="text-danger">Fail</b>';
-                                $stat = 'fail';
-                            }
-
-
-                        } else {
-                            $failed++;
-                            $message = '<b class="text-danger">Incorrect string passed</b>';
-                            $status = '<b class="text-danger">Fail</b>';
-                            $stat = 'fail';
-                        }
-
-                        ?>
-                        <tr>
-                            <td><?php echo $counter; ?></td>
-                            <td><?php echo $internName;  ?></td>
-                            <td><?php echo $newResponse;  ?></td>
-                            <td><?php echo $status;  ?></td> <!-- -->
-                            <?php
-                                if ($stat == 'fail') {
-                                    ?>
-                                    <td><?php echo $message;  ?></td> <!-- -->
-                                    <?php
-                                } else {
-                                    ?>
-                                    <td><b class="text-success">Well done</b></td>
-                                <?php
-                                }
-                            ?>
-
-                        </tr>
-                        <?
-                    }
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-        crossorigin="anonymous"></script>
-</body>
-</html>
