f3398d3706239cad75a1ba32184346fa3dd5c176
Osakinle Thomas <Thomasdreh@gmail.com>
2020-06-04 12:52:09 -0400

    added Python

---
+++ scripts/thomas.py
@@ -0,0 +1,14 @@
+details = {'file': "thomas.py",
+           'name': "Osakinle Oluwadare Thomas",
+           'id': "HNG-00937",
+           'output': "Hello World, this is Osakinle Oluwadare Thomas with HNGi7 ID HNG-00937 using Python for stage 2 task",
+           'email': "Thomasdreh@gmail.com",
+           'language': "Python",
+           'status': "Pass",}
+
+output = print(f"Hello World, this is {details['name']} with HNGi7 ID {details['id']} using {details['language']} for stage 2 task. {details['email']}")
+#this is the string we are to display
+
+
+def myresult():
+    return output
