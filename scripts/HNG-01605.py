67f21de432e0e81ab652e4db69ee218ee2c4c0b4
Amerage <54468823+Amerage@users.noreply.github.com>
2020-06-04 11:41:19 +0100

    Update HNG-01605.py

--- scripts/HNG-01605.py
+++ scripts/HNG-01605.py
@@ -4,4 +4,4 @@ fullname = "Akinyemi Akinwunmi"
 id = "01605"
 language = "Python"
 email = "akinyemiwunmi@gmail.com"
-print(f"Hello World, this is {fullname} with HNGi7 ID {id} using {language} for stage 2 task. {email}" )
+print(f"Hello world, this is {fullname} with HNGi7 ID {id} using {language} for stage 2 task. {email}" )
