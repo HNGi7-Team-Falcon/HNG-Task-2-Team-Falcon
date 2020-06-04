5756a8bb2f96bdfa3dd02fa9d4dcbee277cfd619
oshooo <oshoridwan@gmail.com>
2020-06-04 14:35:36 +0100

    Updated case of type 2

--- scripts/HNG-02472.js
+++ scripts/HNG-02472.js
@@ -6,4 +6,4 @@ let intern = {
     "email": "ridwanosho@outlook.com"
 };
 
-console.log(`Hello World, This is ${intern.firstName} ${intern.lastName} with HNGi7 ID ${intern.ID} using ${intern.favLanguage} for stage 2 task. ${intern.email}`);
+console.log(`Hello World, this is ${intern.firstName} ${intern.lastName} with HNGi7 ID ${intern.ID} using ${intern.favLanguage} for stage 2 task. ${intern.email}`);
