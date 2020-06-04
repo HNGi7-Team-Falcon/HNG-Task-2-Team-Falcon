e99167be4e66283471aa6d97ebdc8b7ae7220d3d
Emmanuel Chijioke <manuelckay@gmail.com>
2020-06-04 09:32:31 +0100

    Update

--- scripts/HNG-02581.js
+++ scripts/HNG-02581.js
@@ -3,5 +3,5 @@ const name = " Emmanuel Chijioke ";
 const id = "HNG-02581";
 const language = "javascript";
 const email = "manuelckay@gmail.com";
-const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email} `;
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
 console.log(output);
