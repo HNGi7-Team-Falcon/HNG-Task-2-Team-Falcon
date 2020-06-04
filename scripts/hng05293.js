9375199c87151833390791297af8ffdb5de0398f
adah <adaugooku01@gmail.com>
2020-06-04 08:53:54 +0100

    Adjusted my email address

--- scripts/hng05293.js
+++ scripts/hng05293.js
@@ -1,2 +1,2 @@
-console.log("<h3>Hello World, this is Oku Emmanuella Adaugo with HNGi7 ID 05293 using javascript for stage 2 task. <br> You can also send me a mail via adaugooku01@gmail.com.<h3>");
+console.log("<h3>Hello World, this is Oku Emmanuella Adaugo with HNGi7 ID 05293 using javascript for stage 2 task. <br> adaugooku01@gmail.com.<h3>");
 
