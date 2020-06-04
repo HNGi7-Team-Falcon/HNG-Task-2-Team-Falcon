ce0ef50c289be452dea20ef59c49f2ab199c6a31
GASTONE DERECK ALEX <gastondereck@gmail.com>
2020-06-04 17:13:17 +0300

    HNGi7 second task

---
+++ scripts/HNG-02020.js
@@ -0,0 +1,11 @@
+let name = "Gastone Alex";
+let hngId = "HNG-02020";
+let language = "JavaScript";
+let email = " gastondereck@gmail.com";
+
+hngFunction = () => {
+let result = console.log(`Hello World,this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task. ${email}`);
+return result;
+}
+
+hngFunction();
