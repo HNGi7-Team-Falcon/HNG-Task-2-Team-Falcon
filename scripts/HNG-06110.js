f04f7d01c6cfa8c86ab87e70e48e96a71c54732f
dogubo mamuzo joshua <51678953+emasontech@users.noreply.github.com>
2020-06-04 07:36:05 +0100

    HNG-06110.js

---
+++ scripts/HNG-06110.js
@@ -0,0 +1,6 @@
+let fullName = "Dogubo Mamuzo Joshua";
+const ID = "HNG-06110";
+const language = " nodeJs/javaScript";
+const email = "emason.tech@gmail.com";
+console.log (`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email}`);
+alert(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email}`);
