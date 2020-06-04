1aeb758af0de3a224f16d2dd88fc4add94eee045
Joel55 <51995266+Joel55@users.noreply.github.com>
2020-06-04 13:54:48 +0100

    Add files via upload
    updated repo for PHP with hng board id HNG-02416

---
+++ HNG-02416.php
@@ -0,0 +1,15 @@
+<?php
+
+$surname = "Omoniyi";
+$second_name = "Joel";
+$last_name = "Ayoola";
+$id = "HNG-02416";
+$language = "PHP";
+$email = "omoniyijoel10@gmail.com";
+
+$script = "Hello World, i am ".$surname." " .$second_name." " .$last_name." with HNGi7 ID ".$id." using ".$language." for task 2 ".$email;
+
+echo $script;
+
+
+?>
