7f5bd835aacb94ac7c8e02f27faf27c14417fe99
Okoli Eucharia Chidiebere <euriaokoli90@gmail.com>
2020-06-04 09:01:00 +0100

    Update hng00750.js

--- scripts/hng00750.js
+++ scripts/hng00750.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 console.log("Hello World, this is Eucharia Chidiebere Okoli with HNGi7 ID HNG-00750 using JavaScript for stage 2 task. euriaokoli90@gmail.com");
-=======
-console.log("Hello World, this is Eucharia Chidiebere Okoli with HNGi7 ID 00750 using JavaScript for stage 2 task");
->>>>>>> d47673926228910ae01f1ddef8d9cdf1aa0cf240
