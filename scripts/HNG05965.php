f80962ccad42ebb5d745d717d94051a44658182f
Etukudo Emmanuel <etukzbong@gmail.com>
2020-06-04 01:01:53 +0100

    changed my script to PHP because it is accepted by my taem

---
+++ scripts/HNG05965.php
@@ -0,0 +1,14 @@
+<?php
+
+//STAGE 2 TASK
+
+$internName = "Etukudo Emmanuel";
+$internID = "HNG-05965";
+$internLanguage = "PHP";
+
+$statement = "Hello World, this is ".$internName." with HNGi7 ID ".$internID." using ".$internLanguage." for stage 2 task";
+
+echo $statement;
+
+
+?>
