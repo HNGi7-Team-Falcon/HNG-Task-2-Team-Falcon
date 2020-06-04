57ccd29bdc044482c330602a09be660db6e62995
benedict <bmsteve96@gmail.com>
2020-06-04 23:45:54 +0300

    resubmission for task 2

---
+++ scripts/HNG-05062.js
@@ -0,0 +1,10 @@
+// @author Benedict Steven
+
+const introduction = (fullName, ID, language, email) => {
+  let text = console.log(
+    `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email}.`
+  );
+  return text;
+};
+
+introduction("Benedict Steven", "HNG-05062", "JavaScript", "bmsteve96@gmail.com")
