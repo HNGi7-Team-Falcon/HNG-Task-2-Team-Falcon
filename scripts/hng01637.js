ff29bb1b0487fb0c2c80b4d7c9cabfccf2c8a0de
Adetayo Akinsanya <48031746+UnkleTayo@users.noreply.github.com>
2020-06-04 13:56:05 +0100

    Update hng01637.js
    Please merge

--- scripts/hng01637.js
+++ scripts/hng01637.js
@@ -1,15 +1,10 @@
-let name = 'Adetayo Akinsanya',
+let fullname = 'Akinsanya Adetayo',
   id = 'HNG-01637',
-  email = 'akinsanyaadetayo@gmail.com',
   language = 'JavaScript';
 
-const myInfo = () => {
-
-  let description = `Hello world, this is ${name} with HNGi7-ID ${id} using ${language} for stage 2 task. ${email}`;
-
-
-  console.log(description);
-  return description;
+const myOutput = () => {
+  console.log(
+    `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  );
 };
-
-myInfo();
+myOutput();
