98a29e06ab85631aafcdbcee17faac3a2d675a49
Marho-dev <66222282+Marho-dev@users.noreply.github.com>
2020-06-04 17:41:22 +0100

    Stephenson

---
+++ scripts/HNG-00027.py
@@ -0,0 +1,8 @@
+import json
+
+email= "momohkeshi@yahoo.com"
+output= "Hello World, this is Stephenson Momoh with HNGi7 ID HNG-00027 using JavaScript for stage 2 task. momohkeshi@yahoo.com"
+file ='output.json'
+with open(file, 'w') as f_obj:
+  json.dump(output, f_obj)
+  print(output)
