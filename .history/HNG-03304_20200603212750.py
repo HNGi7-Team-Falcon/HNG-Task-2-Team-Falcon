7201f2733893dd59c525aecf86018966c3487fdd
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-04 05:25:07 +0100

    keep tidy

--- .history/HNG-03304_20200603212750.py
+++
@@ -1,5 +0,0 @@
-full_name = "Chandra Sekhar Mullu"
-internship_id = "HNG-03304"
-language_used = "Python"
-print(
-    f"Hello World, this is {full_name} with HNGi7 ID {internship_id} using {language_used} for stage 2 task")
