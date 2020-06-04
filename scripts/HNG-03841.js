4d25cd6df3482d79259c7fc868d310f8187178f2
Moses <mosesobike@gmail.com>
2020-06-04 13:55:00 +0100

    Update HNG-03841.js
    Bug fix

--- scripts/HNG-03841.js
+++ scripts/HNG-03841.js
@@ -2,5 +2,5 @@
   const name = "Moses Obike";
   const id ="HNG-O3841";
   const lang ="Javascript";
-  const email= "mosesobike@gmail.com;
+  const email= "mosesobike@gmail.com";
    console.log(`Hello World,this is ${name} with HNI7 ID ${id} using ${lang}for stage 2 task.${email}`);
