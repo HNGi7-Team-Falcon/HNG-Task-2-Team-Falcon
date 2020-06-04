f9efa209041fcb1902e93f6b212e16b9f64a390c
Milindkrishna <40000951+Milindkrishna98@users.noreply.github.com>
2020-06-05 00:34:17 +0530

    Update hng_00344.php

--- scripts/hng_00344.php
+++ scripts/hng_00344.php
@@ -1,8 +1,7 @@
 <?php
-$name = "Milind krishna";
-$hngi7_id = " 00344";
-$language = "PHP" ;
-$mail = "milindkrishna69233@gmail.com";
-$message = "Hello World! this is ". $name. " with hngi7id = ". $hngi7_id." using ". $language. " for stage two task ".$mail;
-echo "$message" ;
+$uname = "Milind krishna";
+$identification = " HNG-00344";
+$lan = "PHP" ;
+$output = "Hello World, this is ". $uname. " with HNG ID ".$identification . " using ". $lan. " for stage 2 task." ;
+echo $output;
 ?>
