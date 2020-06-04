8a65bdba8f4e1f416ba2e6336586ec487ce8ca06
thomasRita <thomasrita98@yahoo.com>
2020-06-04 07:40:09 +0100

    update script

--- scripts/hng00282.js
+++ scripts/hng00282.js
@@ -1,1 +1,1 @@
-console.log('Hello World, Thomas Rita with HNGi7 ID 00282 using Javascript for stage 2 task. auxano42@gmail.com')
+console.log('Hello World, this is Thomas Rita with HNGi7 ID HNG-00282 using Javascript for stage 2 task. auxano42@gmail.com')
