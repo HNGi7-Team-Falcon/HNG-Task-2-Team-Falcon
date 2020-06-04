0bcd54dd021dd26ddb0ff6a6a31ed6acd7f68eb1
toriola998 <66295251+toriola998@users.noreply.github.com>
2020-06-04 01:46:23 +0100

    Update HNG-00557.js
    Task submission

--- .history/scripts/HNG-00557.js
+++ .history/scripts/HNG-00557.js
@@ -3,11 +3,13 @@ const intern = {
     "lastName": "Toriola",
     "ID": "HNG-00557",
     "language": "NodeJS"
+    "E-mail": "faaedah@gmail.com",
 }
 
 console.log(intern)
 fullname = intern.firstName + " " + intern.lastName;
-id = intern.ID;
+id = intern.ID; 
 language = intern.language;
+email = intern.email;
 
-console.log("Hello World , this is " + fullname + " " + " with HNGi7 ID: " + id + " using " + language + " for stage 2 task");
+console.log("Hello World , this is " + fullname + " " + " with HNGi7 ID: " + id + " using " + language + " for stage 2 task. Email:" + email);
