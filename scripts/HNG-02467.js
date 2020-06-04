5f4d286fcf93687e4a02c1b24fe5e957cafb1df4
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-04 05:43:12 +0100

    update task2 without email error

--- scripts/HNG-02467.js
+++ scripts/HNG-02467.js
@@ -3,4 +3,4 @@ var id = 'HNG-02467';
 var language = 'JavaScript';
 var email = 'kidrolex19@gmail.com';
 
-console.log('Hello world, this is ' + fullname + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task .' + email)
+console.log('Hello world, this is ' + fullname + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task' )
