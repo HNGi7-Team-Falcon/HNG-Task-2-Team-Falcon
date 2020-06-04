555b18241aa5eb9a8e7f435e45d99a534d953462
mariolawrence07 <obidikelawrence@yahoo.com>
2020-06-04 18:02:06 +0100

    resubmission

---
+++ scripts/law.js
@@ -0,0 +1,10 @@
+let fullname = 'Obidike Lawrence',
+      hngid  =  'HNGi7 ID HNG-01239',
+      email  ='obidikelawrence11@gmail.com',
+    language = 'JavaScript';
+
+    function Task2 (){
+       return `Hello World, this is ${fullname} with ${hngid} using ${language} for stage 2 task`
+    };
+
+    console.log(Task2());
