d11a77258bdc14e3dd6f465cba3e5ba9d7fa568c
Aanuoluwapo Babajide <46856621+anubabajide@users.noreply.github.com>
2020-06-04 14:49:27 +0100

    Update hng04397.php

--- scripts/hng04397.php
+++ scripts/hng04397.php
@@ -4,6 +4,6 @@ $id = "HNG-04397";
 $language = "php";
 
 
-echo "Hello world, this is " . $fullname. " with HNGi7-ID " .$id. " using " .$language. " for stage 2 task";
+echo "Hello world, this is " . $fullname. " with HNGi7 ID " .$id. " using " .$language. " for stage 2 task. josephokorie32@gmail.com";
 
 ?>
