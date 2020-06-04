319a97bb051ede854e9b610bcbd4174dd04c0bea
Thomas-byt <Thomasdreh@gmail.com>
2020-06-04 12:04:24 -0400

    Dare Thomas (#1210)
    * added my HNG ID + Full name in the folder(MyHNGTask2C#)
    
    * Added falcon task HnG2
    
    * Added task2 files
    
    * added task2
    
    * Added TaskMe HNG
    
    * edited
    
    Co-authored-by: Solomon Ajayi <temmyjay001@gmail.com>

--- scripts/mytaskC#/Program.cs
+++ scripts/mytaskC#/Program.cs
@@ -6,7 +6,7 @@ namespace mytaskC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, this is [Osakinle Dare Thomas] with HNGi7 ID [00937] using [C#] for stage 2 task.[Thomasdreh@gmail.com]");
+            Console.WriteLine("Hello World, this is Osakinle Dare Thomas with HNGi7 ID 00937 using C# for stage 2 task. Thomasdreh@gmail.com");
         }
     }
 }
