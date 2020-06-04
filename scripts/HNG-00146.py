89479d5c0b4405cc58b9ac72e8fd28fb0b214f3c
Deji Ajibola <47859940+youthtrouble@users.noreply.github.com>
2020-06-04 16:23:49 +0100

    Submissions (#1188)
    * fixed format
    
    * minor fix
    
    * corrected format
    
    * fixed stuff
    
    * fixed stuff
    
    Co-authored-by: Olatoye Daniel <36578083+timmyCoder@users.noreply.github.com>

--- scripts/HNG-00146.py
+++ scripts/HNG-00146.py
@@ -1,4 +1,10 @@
 fullname = "Deji Ajibola"
+ID = "HNG-00146"
+language = "Python"
+
+stuff = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}"
+
+print(stuff.format(fullname, ID, language, email),flush = True)
 id = "HNG-00146"
 language = "python"
 email = "harjhibolar@gmail.com"
