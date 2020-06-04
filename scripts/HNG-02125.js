671367eef99477c54d8483422f59e5c31121c6dd
shotolu <65109833+shotolu@users.noreply.github.com>
2020-06-04 10:44:28 +0100

    Update HNG-02125.js

--- scripts/HNG-02125.js
+++ scripts/HNG-02125.js
@@ -4,6 +4,6 @@
         var mail= "paulshotolu@gmail.com"
 
 		function paulfunc() {
-		   return ('Hello World, this '+ name +' with HNGi7 ID '+ hngid +' using '+ lang +' for stage 2 task. '+ mail +'');
+		   return ('Hello World, this is '+ name +' with HNGi7 ID '+ hngid +' using '+ lang +' for stage 2 task. '+ mail +'');
 		}
 		console.log(paulfunc());
