e6f73b47e96110e5be90412c4d563a26b8da409e
Kelechi Precious Nwachukwu <59584014+PluckyPrecious@users.noreply.github.com>
2020-06-04 09:40:54 +0100

    Create HNG03042.py
    Defined simple operator  strings to be executed using  double quotes.

---
+++ HNG03042.py
@@ -0,0 +1,7 @@
+greeting = "Hello World,"
+
+introduction = "this is Kelechi Precious Nwachukwu with HNGi7 ID-03042 using Python for stage 2 task. prekel27@gmail.com"
+
+greetingintroduction = greeting + " " + introduction
+
+print(greetingintroduction,flush=True)
