6552988b3fd3fe0208ba265e2ab3ccd1669573da
Sophie Obomighie <sophiecodes@gmail.com>
2020-06-04 08:46:39 +0100

    Update to HNG-01621
    Added email

--- scripts/HNG-01621.php
+++ scripts/HNG-01621.php
@@ -2,8 +2,8 @@
 $My_Name = "Sophie Obomighie";
 $Id = "HNG-01621";
 $language = "php";
-
-$script = "Hello World, this is {$My_Name} with HNGi7 ID {$Id}  using {$language} for stage 2 task";
+$Email ="sophiecodes@gmail.com";
+$script = "Hello World, this is {$My_Name} with HNGi7 ID {$Id}  using {$language} for stage 2 task. {$Email}";
 
 echo $script;
 ?>
