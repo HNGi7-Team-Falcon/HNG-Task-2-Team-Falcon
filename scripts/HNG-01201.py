db7b86b2d236f392655792b80766753bcc040516
Ayomide Odukoya <39050021+ayomideodukoya@users.noreply.github.com>
2020-06-04 09:23:22 +0100

    made changes

--- scripts/HNG-01201.py
+++ scripts/HNG-01201.py
@@ -7,7 +7,7 @@ info = {
     'email':'odukoyaayomide81@hotmail.com'
 }
 
-string =(f"Hello World, this is [{info['fname']}] [{info['lname']}] with HNGi7 ID [{info['Id']}] using [{info['lang']}] for stage 2 task. {info['email']}") 
+string =(f"Hello World, this is {info['fname']} {info['lname']} with HNGi7 ID {info['Id']} using {info['lang']} for stage 2 task. {info['email']}") 
 print(string) #output string
 
     
