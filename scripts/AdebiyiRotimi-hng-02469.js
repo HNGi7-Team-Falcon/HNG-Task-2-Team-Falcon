85017cfa3c4bc32a90b1c1db4b0e69ecc317938b
Rotimi Adebiyi <r.s.adebiyi@gmail.com>
2020-06-04 18:12:52 +0100

    Changes to script

--- scripts/AdebiyiRotimi-hng-02469.js
+++ scripts/AdebiyiRotimi-hng-02469.js
@@ -1,11 +1,11 @@
-name = "Rotimi Adebiyi";
-hngId = "HNG-02469";
-language = "JavaScript";
-email = " r.s.adebiyi@gmail.com";
+let name = "Rotimi Adebiyi";
+let hngId = "HNG-02469";
+let language = "JavaScript";
+let email = " r.s.adebiyi@gmail.com";
 
 function hngReturn(){
 
-    output = console.log("Hello World, this is "+ name +" with HNG ID "+ hngId + " using " + language + " for stage 2 task." + email + ".");
+    output = console.log("Hello World, this is "+ name +" with HNGi7 ID "+ hngId + " using " + language + " for stage 2 task. + email");
 
     return output;
 }
