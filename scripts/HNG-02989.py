0f55e0516aaa8867e59042573094682648aee827
annakenechi <66427605+annakenechi@users.noreply.github.com>
2020-06-04 11:47:26 -0700

    Update HNG-02989.py

--- scripts/HNG-02989.py
+++ scripts/HNG-02989.py
@@ -1,1 +1,3 @@
-print("Hello World, this is Ugwu Anna Kenechi with HNGi7 ID HNG-02989 using Python for stage 2 task. annakelechi@gmail.com", flush = True)
+email_name = "annakelechi@gmail.com"
+print("Hello World, this is Ugwu Anna Kenechi with HNGi7 ID HNG-02989 using Python for stage 2 task", flush = True)
+print(email_name)
