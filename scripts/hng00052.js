5139710786cc6110e6995439ff26108cae99d809
dayoyanju <adedayoadeyanju.aa@gmail.com>
2020-06-04 02:09:46 +0100

    Created the file hng00052.js

---
+++ scripts/hng00052.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <script>
+            console.log('Hello World, this is Adedayo Adeyanju with HNGi7 ID HNG-00052 using JavaScript for stage 2 task. adedayoadeyanju.aa@gmail.com');
+        </script>
+    </body>
+</html>
