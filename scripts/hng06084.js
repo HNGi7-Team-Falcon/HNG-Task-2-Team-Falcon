96aba214d598cacedf31eab3dbecf2b49a6ae03d
Solomon Ajayi <temmyjay001@gmail.com>
2020-06-04 02:32:34 +0100

    HNG-06084.js

--- scripts/hng06084.js
+++ scripts/hng06084.js
@@ -1,5 +1,6 @@
 const FULL_NAME = `Ajayi solomon`;
 const HNGID = `HNG-06084`;
 const EMAIL = `temmyjay001@gmail.com`;
+const LANGUAGE = `Javascript`;
 
-console.log(`Hello World, this is ${FULL_NAME} with HNGi7 ID ${HNGID}  using Javascript for stage 2 task. ${EMAIL}`);
+console.log(`Hello World, this is ${FULL_NAME} with HNGi7 ID ${HNGID}  using ${LANGUAGE} for stage 2 task. ${EMAIL}`);
