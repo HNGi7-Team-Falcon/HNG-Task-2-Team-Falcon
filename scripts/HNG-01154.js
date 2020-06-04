667d943b1ef87854664e00dd03edc45bee24bf6b
Ayomide <ayomideadebogun@gmail.com>
2020-06-03 22:50:03 -0400

    adebogun ayomide/HNG-01154 submission

---
+++ scripts/HNG-01154.js
@@ -0,0 +1,6 @@
+const fullName ='ADEBOGUN AYOMIDE';
+const HNGID = 'HNG-01154.js';
+const email ='ayomideadebogun@gmailcom';
+const language = 'Javascript';
+
+console.log('Hello World,this is ${Adebogun Ayomide} with HNGi7 ID ${HNG-01154} using ${Javascript} for stage 2 task.${ayomideadebogun@gmail.com} ');
