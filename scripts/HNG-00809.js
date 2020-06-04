e29b60e8fbaefdbd05ee45f6b468bc7da9cedbe6
moe_rayo <48064606+MoeRayo@users.noreply.github.com>
2020-06-04 06:54:24 +0100

    Update HNG-00809.js

--- scripts/HNG-00809.js
+++ scripts/HNG-00809.js
@@ -4,6 +4,8 @@ let hngId  = "HNG-00809";
 let lang = "javaScript";
 
 function description(){
-    return `Hello world, this is ${name} with HNGi7 ID of ${hngId} using ${lang} for stage 2 task. ${email}.`;
-};
-console.log(description());
+    console.log(`Hello world, this is ${name} with HNGi7 ID of ${hngId} using ${lang} for stage 2 task. ${email}`);
+}
+description();
+
+
