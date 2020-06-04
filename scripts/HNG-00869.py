1f2da693c5938b44aaa8e90f0c232320451c2329
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-04 17:21:49 +0100

    fix

--- scripts/HNG-00869.py
+++
@@ -1,8 +0,0 @@
-#HNG Task-2 for team falcon
-
-
-Hng_Details = input(
-    "Hello world this is Adeyemi Oluwatobi with HNGi7 ID of HNG-00869, using python language for stage 2 task. my email is tobinuel155@gmail.com ")
-
-print(Hng_Details)
-
