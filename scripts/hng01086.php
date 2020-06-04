3f14e626db74d77a977999581a5488387efc6fee
Ekene Adim <61980292+michealadim@users.noreply.github.com>
2020-06-04 11:40:06 +0100

    Update hng01086.php

--- scripts/hng01086.php
+++ scripts/hng01086.php
@@ -1,6 +1,1 @@
-<?php 
-echo "Hello World,
-this is Adim Ekene 
-with HNGi7 ID HNG-01086 
-using PHP for stage 2 task.
-ekeneadim@gmail.com"; ?>
+<?php echo "Hello World, this is Adim Ekene with HNGi7 ID HNG-01086 using PHP for stage 2 task. ekeneadim@gmail.com"; ?>
