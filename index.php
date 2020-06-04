fd3101d3978901ab5a5e487833db5896bd3557d1
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-04 16:40:35 +0100

    catch possible exception

--- index.php
+++ index.php
@@ -136,6 +136,7 @@
 		<body>
 			<div class=container>
 			<h1 class="text-center">Team Falcon</h1>
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 			<table class="table">
 				<thead>
 					<tr class="text-center">
@@ -175,7 +176,11 @@
 							// echo $fileName;
 							if ($runtime) {
 
-								$output = shell_exec("$runtime $filePath 2>&1 < input_for_scripts"); # Execute script and assign result
+								try {
+									$output = shell_exec("$runtime $filePath 2>&1 < input_for_scripts"); # Execute script and assign result
+								} catch(Exeception $e) {
+									$output = null;
+								}
 								if (is_null($output)) {
 
 									$item["status"] = "fail";
@@ -257,7 +262,6 @@
 		</table>
 		</div>
 	</body>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
