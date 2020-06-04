3c369ff92086f86a5be3c6e3aacd8c7b8e9370f0
Fabamise Adeolu <fabamiseadeolu@gmail.com>
2020-06-04 13:25:00 +0100

    file update

--- scripts/HNG-00244.php
+++ scripts/HNG-00244.php
@@ -6,6 +6,6 @@
 	$language = "PHP";
 
 
-	echo "Hello World, this is {$name} with HNGi7 ID {$hng_id} using {$language} for stage 2 task. {email}";
+	echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task. {email}";
   
 ?>
