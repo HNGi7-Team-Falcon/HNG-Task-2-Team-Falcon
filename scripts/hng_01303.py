764ba97f97ad21df5df8d64c35f4750760045059
astroyummy <66243974+astroyummy@users.noreply.github.com>
2020-06-04 20:12:50 +0530

    updated1
    updated  with exact spaces

--- scripts/hng_01303.py
+++ scripts/hng_01303.py
@@ -4,4 +4,4 @@ uname="Kankanala Yamini"
 identification="HNG-01303"
 lan="python"
 email="yaminichowdary11111999@gmail.com"
-print("Hello world,this is",+ uname +"with HNGi7 ID"+ identification +"using"+ lan +"for stage 2 task." + email)
+print("Hello World, this is " + uName + " with HNGi7 ID " + identification + " using " + lan + " for stage 2 task. " + email)
