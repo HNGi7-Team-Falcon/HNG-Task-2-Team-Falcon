ff48a487c3e79f7dd24183e2788fbf0e1c5e1d7f
Akinsola-M0 <54712894+Akinsola-M0@users.noreply.github.com>
2020-06-04 15:20:45 +0100

    Update HNG00710.js

--- scripts/HNG00710.js
+++ scripts/HNG00710.js
@@ -5,7 +5,7 @@ let Email = 'olulare6@gmail.com';
 
 function returnString()
 {
-   const message = 'Hello World, this is '+ Name + ' with HNGi7 ID '+ Id+' using '+Language+' for stage 2 task. '+ Email;
+   const message = 'Hello World, this is '+ Name + ' with HNGi7 ID '+ Id+' using '+Language+' for stage 2 task. '+Email;
     return message;
 }
 
