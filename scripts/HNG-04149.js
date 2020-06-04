69c3f7fd7b71e5e18ad86bd8af64b47edb3b5b4f
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-05 00:57:42 +0100

    Merge branch 'deploy' into alternate-dev
    # Conflicts:
    #	README.md
    #	index.php
    #	package.json
    #	scripts/HNG-00134.js
    #	scripts/HNG-00244.php
    #	scripts/HNG-01183.php
    #	scripts/HNG-02048.py
    #	scripts/HNG-03346.js
    #	scripts/HNG-03670.php
    #	scripts/HNG-04240.php
    #	scripts/HNG-04869.js
    #	scripts/HNG-05448.js
    #	scripts/HNG-05571.py
    #	scripts/Ugo.py
    #	scripts/hng-00055.py
    #	scripts/hng-00972.py
    #	scripts/hng-01552.py
    #	scripts/hng00953.js
    #	scripts/hng01415
    #	scripts/hng01415.php
    #	scripts/hng01428.js
    #	scripts/hng02330.js
    #	scripts/hng02755.py
    #	scripts/hng03347.py
    #	scripts/hng03536.js
    #	scripts/hng04921.js
    #	scripts/hng05372.js
    #	scripts/hng05695.php
    #	scripts/hng06084.js

--- scripts/HNG-04149.js
+++ scripts/HNG-04149.js
@@ -1,9 +1,7 @@
-const fullName = "Favour Nifemi F";
+const fullName = "Favour Nifemi F"
+const id = "HNG-04149"
+const language = "Javascript"
+const email = "shoppernife@gmail.com"
+ 
+console.log (`Hello world, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`); 
 
-
-const id = "HNG-04149";
-const language = "Javascript";
-const email = "shoppernife@gmail.com";
-
-
-console.log(`Hello world, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}.`);
