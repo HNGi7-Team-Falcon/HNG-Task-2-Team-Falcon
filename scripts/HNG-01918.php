5e6a863ad6c837e7cba1235be591d80d63beb884
Abiodun Olanlokun <34736423+ah3eyy@users.noreply.github.com>
2020-06-04 02:04:18 +0100

    fix: hng stage 1 task: letter case issues

--- scripts/HNG-01918.php
+++ scripts/HNG-01918.php
@@ -9,4 +9,4 @@ $language = 'PHP';
 
 $email = 'abiodunolanlokunbakinde@gmail.com';
 
-echo("Hello word, this is " . $name . " with HNGi7 ID " . $hng_id . " using " . $language . " fir stage 2 task. " . $email);
+echo("Hello Word, this is " . $name . " with HNGi7 ID " . $hng_id . " using " . $language . " for stage 2 task. " . $email);
