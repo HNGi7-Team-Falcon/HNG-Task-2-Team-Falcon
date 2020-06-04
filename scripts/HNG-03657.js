9ea39462459cbcc4bd53b5743674450926184658
psalmyjay <31113987+psalmy2595@users.noreply.github.com>
2020-06-04 07:23:17 +0100

    Update HNG-03657.js

--- scripts/HNG-03657.js
+++ scripts/HNG-03657.js
@@ -4,8 +4,7 @@ const info = () => {
     const hng_id = `HNG-03657`;
     const language = `JavaScript`;
     const email = `samuelj4real17@gmail.com`
-    const result = `Hello World, This is ${full_name} with HNGi7 ID ${hng_id} using ${language} for stage 1 task ${email} `;
-    //document.write(result);
+    const result = `Hello World, This is ${full_name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task. ${email} `;
     console.log(result);
 }
 
