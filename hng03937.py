5c2b0f7f8b46ff91d75d30a0863a3f009beca091
AlahiraJ <63448638+AlahiraJ@users.noreply.github.com>
2020-06-04 13:06:15 +0100

    updated script

---
+++ hng03937.py
@@ -0,0 +1,8 @@
+def print_info():
+    """
+    script to print personal information
+    """
+    
+    print("Hello World, this is Alahira Jeffrey Calvin with HNGi7 ID  HNG-03937 using python for stage 2 task")
+
+print_info()
