a0beb382145366ddb1cb4d1574add60f86c1128c
Basheeroh M. Akinpelu <52384451+basheeroh@users.noreply.github.com>
2020-06-04 14:57:55 +0100

    HNG task with id HNG-01037

---
+++ scripts/HNG-01037.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HNG-TASK-TWO-TEAM-FALCON</title>
+</head>
+<body>
+    <?php
+    $fullname = "Akinpelu Basheeroh";
+    $hngid = "HNG-01037";
+    $language = "PHP";
+    $email = "basheeroh22@gmail.com";
+        echo '<p>Hello World, this is [fullname] with HNGi7 ID [hngid] using [language] for stage 2 task. [email]</p>'; 
+        ?> 
+</body>
+</html>
