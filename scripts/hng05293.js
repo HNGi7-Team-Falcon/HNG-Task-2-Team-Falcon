1a352de17793ff087ff964140f33fcbbd29cdb1a
adah <adaugooku01@gmail.com>
2020-06-04 04:09:01 +0100

    Create hng05293.js

---
+++ scripts/hng05293.js
@@ -0,0 +1,1 @@
+document.write("<h3>Hello World, this is Oku Emmanuella Adaugo with HNGi7 ID 05293 using javascript for stage 2 task. <br> You can also send me a mail via adaugooku01@gmail.com.<h3>");
