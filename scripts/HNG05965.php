7515b9179657b8d8d7e6a1945389c6e5a90942d1
Etukudo Emmanuel <etukzbong@gmail.com>
2020-06-04 01:26:00 +0100

    added my email to the script

--- scripts/HNG05965.php
+++ scripts/HNG05965.php
@@ -5,8 +5,9 @@
 $internName = "Etukudo Emmanuel";
 $internID = "HNG-05965";
 $internLanguage = "PHP";
+$internEmail = "etukzbong@gmail.com";
 
-$statement = "Hello World, this is ".$internName." with HNGi7 ID ".$internID." using ".$internLanguage." for stage 2 task";
+$statement = "Hello World, this is ".$internName." with HNGi7 ID ".$internID." using ".$internLanguage." for stage 2 task. ".$internEmail;
 
 echo $statement;
 
