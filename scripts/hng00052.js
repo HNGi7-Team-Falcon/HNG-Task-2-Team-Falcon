4d5e702a987912f0989703a49f50e935c3b1ef52
Feranmi Ade <42687078+techiefer@users.noreply.github.com>
2020-06-04 03:49:36 +0100

    Update hng00052.js

--- scripts/hng00052.js
+++ scripts/hng00052.js
@@ -1,8 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <script>
-            console.log('Hello World, this is Adedayo Adeyanju with HNGi7 ID HNG-00052 using JavaScript for stage 2 task. adedayoadeyanju.aa@gmail.com');
-        </script>
-    </body>
-</html>
+console.log('Hello World, this is Adedayo Adeyanju with HNGi7 ID HNG-00052 using JavaScript for stage 2 task. adedayoadeyanju.aa@gmail.com');
+
