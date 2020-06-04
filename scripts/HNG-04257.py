ee432837907bcda3aa6c96e792647be669b91bd1
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-06-05 02:44:08 +0300

    Add email at end of string

--- scripts/HNG-04257.py
+++ scripts/HNG-04257.py
@@ -3,7 +3,7 @@ def task_two():
     ID = "04257"
     language = "Python"
     email = "ndirangumindo@gmail.com"
-    return "Hello World, this is "+full_name+" with HNGi7 ID "+ID+" using "+language+" for stage 2 task"
+    return "Hello World, this is "+full_name+" with HNGi7 ID "+ID+" using "+language+" for stage 2 task. "+email
 
 
 print(task_two())
