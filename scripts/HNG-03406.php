64e8deb1644d533bf245048bc12c7865bcb16b98
Victor Okeke <34918555+okekevicktur@users.noreply.github.com>
2020-06-05 03:35:47 -1100

    Commited to the right branch
    My old commit was on the wrong branch. PLEASE MERGE MY pr

---
+++ scripts/HNG-03406.php
@@ -0,0 +1,7 @@
+<?php
+  $fname=  "Okeke Victor Emeka";
+  $id= "HNG-03406";
+  $lang=  "PHP";
+  $email=  "Okekevicktur@gmail.com";
+  echo "Hello World, this is $fname with HNGi7 ID $id using $lang for stage 2 task. $email";
+?>
