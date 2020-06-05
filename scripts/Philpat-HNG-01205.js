475b7c55dfcd57f311df7c1a6f2a627c1aab3d6f
philpat <66218833+philpat@users.noreply.github.com>
2020-06-05 12:43:12 +0100

    Create Philpat-HNG-01205.js

---
+++ scripts/Philpat-HNG-01205.js
@@ -0,0 +1,6 @@
+const name = "Patience Bassey"
+const id = "HNG-01205"
+const language = "JavaScript"
+const email = "patlyn2020@gmail.com"
+const result = "Hello World," + " "+  "this is" +" " +  name + " "+ "with HNGi7 ID" + " " + id +" " + "and email"+" " + email +" " +"using"  +" "+ language + " "+ "for stage 2 task"
+console.log(result)
