ff918a418fff59981c835ce1ef84d0ac7d35766c
V33ee <50486062+V33ee@users.noreply.github.com>
2020-06-06 04:31:57 +1300

    Update HNG-00255.php

--- scripts/HNG-00255.php
+++ scripts/HNG-00255.php
@@ -1,10 +1,20 @@
 master
  <?php
-       $myOutput = "Hello World, this is Abolarin Victor with HNGI7 ID HNG-00255 using php for stage 2 task.  iam.vee01@gmail.com";
+       $myName = "Abolarin Victor";
+       $myId = "HNG-00255";
+       $myLanguage = "Php";
+       $myEmail = "Iam.vee01@gmail.com";
+       $myOutput = "Hello World, this is {$myName} with HNGi7 ID {$myId} using {$myLanguage} for stage 2 task. {$myEmail} ";
        echo $myOutput; 
+?>
       
 
 <?php 
-echo "Hello World, this is Abolarin Victor with HNGi7 ID HNG-00255 using Php for stage 2 task."
+       $myName = "Abolarin Victor";
+       $myId = "HNG-00255";
+       $myLanguage = "Php";
+       $myEmail = "Iam.vee01@gmail.com";
+       $myOutput = "Hello World, this is {$myName} with HNGi7 ID {$myId} using {$myLanguage} for stage 2 task. {$myEmail} ";
+       echo $myOutput;
 submissions
 ?>
