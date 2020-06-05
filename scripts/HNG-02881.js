295fdb4e76d28fa6c909894f43c6f8bbca4f2f2c
yhuakim <47617723+yhuakim@users.noreply.github.com>
2020-06-05 20:22:52 +0100

    edited welcome string

--- scripts/HNG-02881.js
+++ scripts/HNG-02881.js
@@ -6,6 +6,6 @@ const myBio = {
 
 let {name, hng_id, email} = myBio;
 
-let welcomeString = `Hello World, this is ${name} with HNGi7-ID, ${hng_id} using Javascript for stage 2 task.${email}`;
+let welcomeString = `Hello World, this is ${name} with HNGi7 ID, ${hng_id} using Javascript for stage 2 task.${email}`;
 
 console.log(welcomeString);
