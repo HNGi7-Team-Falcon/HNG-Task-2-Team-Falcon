9c1cec6865f10266af92f4a32956ae1b6196484f
0m0hsam <omohsam81@gmail.com>
2020-06-05 12:04:19 +0100

    HNG7-02502-Task-2

---
+++ scripts/HNG-02502Task2.py
@@ -0,0 +1,13 @@
+cmd=True
+def user(cmd):
+	comment='Program Closed thanks for checking\nPlease grade me well!'
+	if cmd :
+		name='Samuel Jonathan'
+		id='HNG-02502'
+		language='Python'
+		email='omohsam81@gmail.com'
+		print(f"Hello world, this is { name} with id {id} using {language} email {email}")
+		print(comment)	
+
+		
+user(cmd)
