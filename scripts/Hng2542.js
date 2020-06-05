fc6eea417c7d42b7c76d767eb1fd1adc61b2edca
Bolu Tejumola <tejumolabolu@gmail.com>
2020-06-05 20:49:48 +0100

    fixed a profile failure

--- scripts/Hng2542.js
+++ scripts/Hng2542.js
@@ -2,23 +2,20 @@ function Hng_task2() {
   const name = "Tejumola Bolu";
   const id = "HNG-2542";
   const language = "JavaScript";
-  const Email = "tejumolabolu@gmail.com";
   console.log(
     "Hello World, this is" +
       " " +
       name +
       " " +
-      "with HNGi7_ID:" +
+    "with HNGi7 ID" +
+      " " +
       id +
       " " +
       "using" +
       " " +
       language +
       " " +
-      "for stage 2 task." +
-      " " +
-      "email:" +
-      Email
+      "for stage 2 task"
   );
 }
 Hng_task2();
