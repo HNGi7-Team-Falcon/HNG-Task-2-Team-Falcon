4c3b6605ec9b23ee71e23e29365d1349cd0e9b79
Azubuike Nnamdi Clement <35131360+CtrlAltFix-Softwares@users.noreply.github.com>
2020-06-05 22:50:20 +0100

    Update hng000006.js

--- hng000006.js
+++ hng000006.js
@@ -8,7 +8,7 @@ var language = "JavaScript";
 
 var email = "blessedmarcel1@gmail.com";. 
 
- return("Hello World, this is" + fullName + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task. " + email );
+return("Hello World, this is" + fullName + " with HNGi7 ID " + hngId + " using " + language + " for stage 2 task. " + email );
 
 }
 
