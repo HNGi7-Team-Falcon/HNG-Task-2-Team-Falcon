b4e72c9084a6439c7c4ceb766a2252fa2a0f1be9
Alvan_C <40849464+Talk2Alvan@users.noreply.github.com>
2020-06-05 08:15:53 +0100

    hng02941.js

---
+++ hng02941.js
@@ -0,0 +1,6 @@
+var fullname = "Achonwa Chiemezuo"
+  		var HNGid = "HNG-02941"
+  		var language = "Javascript"
+  		var email = "alvanmexix@gmail.com"
+
+  		console.log("Hello World, this is " + fullname + " with HNGi7 ID "+ HNGid + " using "+ language + " for stage 2 task. " + email);
