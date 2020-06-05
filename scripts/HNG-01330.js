46381326e428109539d1af7c9225a4e879a9d850
Victor Adesanya <60039313+Vicdam@users.noreply.github.com>
2020-06-05 11:36:30 +0100

    adjusted indentation (#1603)

--- scripts/HNG-01330.js
+++ scripts/HNG-01330.js
@@ -3,4 +3,4 @@ let id = 'HNG-01330';
 let lang = 'JavaScript';
 let email ='victoroadesanya@gmail.com';
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task. ${email}`);
+console.log (`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task. ${email}`);
