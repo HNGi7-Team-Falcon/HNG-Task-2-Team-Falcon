075faaccd4bc5f0682ac9f613d040b7a05e55523
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-05 22:31:24 +0100

    Update HNG-00467.js
    Commit for Abdulmujeeb

--- scripts/HNG-00467.js
+++ scripts/HNG-00467.js
@@ -1,5 +1,6 @@
 let fullName = "Muhammad-Olodo Abdulmujeeb Opeyemi" 
 let id = "HNG-00467"
 let language = "JavaScript"
-let output = `Hello World, this is ${fullName} with hng id ${id} with language ${language} olodoopeyemi@gmail.com'
+let email= "olodoopeyemi@gmail.com"
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
 console.log(output)
