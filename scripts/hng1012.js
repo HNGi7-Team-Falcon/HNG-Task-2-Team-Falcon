3b7c3a735a9a53120159586050319b022b4dc787
Victor Ikechukwu <61970374+vectaaa@users.noreply.github.com>
2020-06-05 11:19:49 +0100

    Update HNG-01012

--- scripts/hng1012.js
+++ scripts/hng1012.js
@@ -1,6 +1,6 @@
 const fName = "Onuchukwu Victor";
 const fId = "HNG-01012";
 const fLanguage = "JavaScript";
-const fEmail = "victoronuchukwu16@gmai.com";
+const fEmail = "victoronuchukwu16@gmail.com";
 
 console.log(`Hello World, this is ${fName} with HNGi7 ID ${fId} using ${fLanguage} for stage 2 task. ${fEmail}`)
