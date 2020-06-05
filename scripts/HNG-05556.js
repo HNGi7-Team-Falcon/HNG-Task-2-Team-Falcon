70ef0f16ef5f36f247bf5de91879d7fb2cabc5b3
Soumya Choudhary <56254035+super-beam@users.noreply.github.com>
2020-06-05 23:59:22 +0530

    Create HNG-05556.js

---
+++ scripts/HNG-05556.js
@@ -0,0 +1,12 @@
+
+
+
+const userName = "Soumya";
+const hngId = "HNG-05556";
+const lang = "javascript";
+const email = "soumyachoudhary1999@gmail.com"
+
+function task () {
+    return `Hi HNG,I am ${userName} with HNGi7 ID ${hngId} and this is my ${lang} for stage 2 task. Thank you. `  
+}
+console.log(task()) 
