662214134526e80129368251467efb75e00d2881
unknown <you@example.com>
2020-06-05 20:50:34 +0300

    Program to display information about HNG

---
+++ scripts/hng05671.js
@@ -0,0 +1,7 @@
+const fullName ="Elineka Maleo";
+const ID = "HNG-05671";
+const language = "Javascript"
+
+
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`)
