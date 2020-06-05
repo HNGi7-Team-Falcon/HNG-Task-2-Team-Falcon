0e4e0345da597993b01fbefc3044cb2c7125cc93
francis144 <61960582+francis144@users.noreply.github.com>
2020-06-05 11:21:48 +0100

    Adding my script  to team task 2

---
+++ HNG00794.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Francis Udoh";
+$hngID = "HNG-00794";
+$language = "PHP";
+$emailAddress = "saint4frank@gmail.com";
+
+echo "Hello World, this is $name with the HNGi7 ID $hngID using $language for stage 2 task. $emailAddress"; 
