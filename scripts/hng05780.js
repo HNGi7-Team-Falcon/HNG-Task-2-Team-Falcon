98bf193944d5aae1cacef7eb84e89fccd5876fe5
Oluwafunmilayo Dada <45949325+Ariiieee@users.noreply.github.com>
2020-06-05 01:38:46 +0100

    hng05780.js
    Task 2 for HNGi7

--- scripts/hng05780.js
+++ scripts/hng05780.js
@@ -1,6 +1,5 @@
-let fullName = 'Oluwafunmilayo Dada';
+const fullName = 'Oluwafunmilayo Dada';
 const email = 'ariiiearie@gmail.com';
-const hngId = '05780';
-const language = 'Javascript';
-console.log('Hello World, this is Oluwafunmilayo Dada with HNGi7 ID HNG-05780 
-             using for stage 2 task using Javascript for stage 2 task ariiieaarie@gmail.com'); 
+const hngId = 'HNG-05780';
+const Language = 'Javascript';
+console.log('Hello World, this is ' +fullName+ ' with HNGi7 ID ' +hngId+ ' using ' +Language+ ' for stage 2 task. ' +email);
