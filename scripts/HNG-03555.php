7ea6fb548534a0eeb2a9a5bdcf7b060a269158bb
jeremiahiro <jeremiahiro@gmail.com>
2020-06-05 18:56:42 +0100

    updated HNG-03555

--- scripts/HNG-03555.php
+++ scripts/HNG-03555.php
@@ -1,3 +1,8 @@
-<?php 
-    echo '<p>Hello World! This is <strong>Jeremiah Favour Iromaka</strong> with HNGi7 ID <strong>HNG-03555</strong> using <strong>PHP</strong> for stage 2 task. <strong>Jeremiahiro@gmail.com</strong></p>'; 
-?> 
+<?php  
+    $fullName = "Jeremiah Favour Iromaka";
+    $hngID = "HNG-03555";
+    $language = "PHP(Laravel)";
+    $emailAddress = "jeremiahiro@gmail.com";
+
+    echo "Hello World, this is $fullName with HNGi7 ID $hngID using $language for stage 2 task. $emailAddress";
+?>
