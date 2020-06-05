2219bb0f9a4a293b4ce617bb28281bc23b6fedda
Festus Samuel <sfestus75@yahoo.com>
2020-06-05 07:42:39 +0100

    my result for task2 updated

--- scripts/HNG_04468.py
+++ scripts/HNG_04468.py
@@ -1,1 +1,3 @@
-print("Hello world,this is Festus Samuel with HNGi7 ID HNG-04468 using Python language for stage 2 task. sfestus75@yahoo.com")
+
+
+print("Hello World, this is Festus Samuel with HNGi7 ID HNG-04468 using Python for stage 2 task. sfestus75@yahoo.com")
