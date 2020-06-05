d71f844b940e37a6e768b60a39556e4e02a361c3
cybercode2000 <damicodd123@gmail.com>
2020-06-05 11:18:00 -0400

    Task 2 submission

---
+++ scripts/HNG-02320.js
@@ -0,0 +1,11 @@
+const HNG02320 = () => {
+  let fullName = "Awopetu Rasheed Oluwadamilare";
+  let id = "HNG-02320";
+  let language = "Javascript";
+  let email = "damicodd123@gmail.com";
+  let completeCredentials = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`;
+
+    return completeCredentials;    
+}
+
+console.log(HNG02320());
