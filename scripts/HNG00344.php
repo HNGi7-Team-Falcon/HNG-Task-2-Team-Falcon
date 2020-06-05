2ce2ac2c7defc91cace678c10e9150fb0dbf5240
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-05 11:52:50 +0100

    resturcture

--- scripts/HNG00344.php
+++
@@ -1,7 +0,0 @@
-<?php
-$uname = "Milind krishna";
-$identification = " HNG-00344";
-$lan = "PHP" ;
-$output = "Hello World, this is ". $uname. " with HNG ID ".$identification . " using ". $lan. " for stage 2 task" ;
-echo $output;
-?>
