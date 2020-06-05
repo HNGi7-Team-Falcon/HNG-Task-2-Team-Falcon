1d980a1a3c21da40981dd91100cfd6e402bf9350
OLADOTUN AKINWUMI <65426552+Dondardar@users.noreply.github.com>
2020-06-05 12:12:04 +0100

    HNG-01436.py

---
+++ HNG-01436.py
@@ -0,0 +1,6 @@
+fullName = "Akinwumi oladotun"
+ID = "HNG-01436"
+language = "Python"
+email = "dotunak@gmail.com"
+
+print("Hello World, this is " +fullName+" with HNGi7 ID "+ID+" using "+language+" for stage 2 task. "+email)
