6a8f64068a631d5ccb967f268b1ca7bca9b0cd21
kennedy <31958582+kennedyyakubu@users.noreply.github.com>
2020-06-05 16:36:04 +0100

    please help me scale through
    Wrote a simple program using python.

---
+++ scripts/HNG-05567.py
@@ -0,0 +1,6 @@
+#HNGi7 stage 2 task
+name = 'Afemikhe Yakubu'
+hngi7_id = 'HNG-05567'
+language = 'python'
+email = 'kennedyyakubu56@gmail.com'
+print(f'Hello World, this is {name} with HNGi7 ID {hngi7_id} using {language} for stage 2 task. {email}',flush=True)
