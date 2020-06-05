54ec097a817befe68ff85d9d45c2bb50fcbb412b
Jeremiah Iro <Jeremiahiro@gmail.com>
2020-06-05 23:35:32 +0100

    Update from HNG-03555
    Please confirm if this is right this time

--- scripts/HNG-03555.php
+++ scripts/HNG-03555.php
@@ -1,9 +1,8 @@
-<?php 
+<?php  
     $fullName = "Jeremiah Favour Iromaka";
-    $HNGID = "HNG-03555";
+    $hngID = "HNG-03555";
     $language = "PHP(Laravel)";
     $emailAddress = "jeremiahiro@gmail.com";
 
-    echo "Hello World, This is $fullName with HNGi7 ID $HNGID using $language for stage 2 task. $emailAddress";
-
-?> 
+    echo "Hello World, this is $fullName with HNGi7 ID $hngID using $language for stage 2 task. $emailAddress";
+?>
