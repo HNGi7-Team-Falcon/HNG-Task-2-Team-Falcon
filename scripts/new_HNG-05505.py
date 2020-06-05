e319b1b4b0178ac65cff0fb599a1f38b969b0ef5
Afosystem <61983414+Afosystem@users.noreply.github.com>
2020-06-05 04:03:12 -0700

    Update and rename HNG-05505 to new_HNG-05505.py

--- scripts/new_HNG-05505.py
+++ scripts/new_HNG-05505.py
@@ -1,5 +1,5 @@
 name = 'Ibrahim Afolabi'
-id = "05505"
+id = "HNG-05505"
 language = 'pyton'
 email ='afolability@gmail.com'
 print(f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task.{email}")
