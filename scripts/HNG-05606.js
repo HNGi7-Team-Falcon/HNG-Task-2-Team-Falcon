6d1044a5ad82b7db334c80b646b857d03423e316
saudi-1 <zaidat.sanusi@gmail.com>
2020-06-05 09:43:12 +0100

    Kindly add Zaidat Sanusi's updated file

--- scripts/HNG-05606.js
+++ scripts/HNG-05606.js
@@ -1,1 +1,1 @@
-console.log ("This is Zaidat Sanusi with ID HNG-05606 using javascript to complete task 2. sjzaidat@gmail.com")
+console.log (" Hello World. This is Zaidat Sanusi with ID HNG-05606 using javascript to complete task 2. sjzaidat@gmail.com")
