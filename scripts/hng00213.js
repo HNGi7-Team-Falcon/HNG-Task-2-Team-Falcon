2c6a4cccafcfd501c7250fab3a5628d1272069f0
John Rotgak <info2rotgak@gmail.com>
2020-06-05 01:01:54 +0100

    My hng task 2

---
+++ scripts/hng00213.js
@@ -0,0 +1,11 @@
+var fname = "John";
+var lname = "Rotgak";
+var ID = "HNG-00213"
+var language = "javascript";
+var email = "info2rotgak@gmail.com";
+
+
+console.log("Hello World, this is " + fullname + " " + " with HNGi7 ID " +  ID + " using " + language + " for stage 2 task " + " " + email);
+
+
+
