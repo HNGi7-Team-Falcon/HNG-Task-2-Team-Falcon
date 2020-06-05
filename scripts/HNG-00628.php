d95fdb21e657f899b8cd5ceea5cc642eefb19703
jgeorge583 <66241361+jgeorge583@users.noreply.github.com>
2020-06-05 15:28:24 +0100

    Add files via upload

---
+++ scripts/HNG-00628.php
@@ -0,0 +1,13 @@
+<?php
+
+$fullName = "Joy George";
+$id = "HNG-00628";
+$serverLanguage = "PHP";
+$email = "georgejoy713@gmail.com";
+
+//echo '<strong> php output without referencing variables:</strong><br>';
+//echo 'Hello World, this is Joy George with HNGi7 ID 00628 using PHP language for stage 2 task. georgejoy@gmail.com';
+//echo '<strong> php output with variables referenced:</strong><br>';
+echo 'Hello World, this is '.$fullName.' with HNGi7 ID '.$id.' using '.$serverLanguage.' language for stage 2 task. '.$email.'';
+
+?>
