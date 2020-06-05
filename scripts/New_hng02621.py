e48afdfa0e41a20e8100d8ebd8ab30d72b10788b
McChizzy <66224357+McChizzy@users.noreply.github.com>
2020-06-05 23:04:47 +0100

    Fresh Edit
    I removed brackets for script

--- scripts/New_hng02621.py
+++ scripts/New_hng02621.py
@@ -3,4 +3,4 @@ Language = 'Python'
 id = "HNG-02621"
 email = "ikechex@gmail.com"
 
-print("Hello World, this is", (Name), "with HNGi7 ID", (id), "using", (Language), "for stage 2 test.", (email), flush=True)
+print("Hello World, this is", Name, "with HNGi7 ID", id, "using", Language, "for stage 2 test.", email, flush=True)
