1dedfe0165bed8f9f2cdda742ae48e7aad78cfa9
Bello Yakub <hyaaquub@gmail.com>
2020-06-05 18:20:27 +0100

    update HNG-00830

--- scripts/HNG-00830.php
+++ scripts/HNG-00830.php
@@ -1,7 +1,7 @@
-   <?php
+ <?php
     $fullname = "Bello Yakub";
     $id = "HNG-00830";
     $language = "PHP";
     $email = "hyaaquub@gmail.com";
-        echo 'Hello World, this is '.$fullname.' with HNGi7 ID '.$id.' using '.$language.' for stage 2 task.';
+        echo 'Hello World, this is '.$fullname.' with HNGi7 ID '.$id.' using '.$language.' for stage 2 task. '.$email.'';
         ?>
