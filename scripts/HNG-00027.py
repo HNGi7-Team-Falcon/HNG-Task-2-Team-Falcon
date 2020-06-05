cd842fa495914861d589189bcf21fab572239159
stemoo <66222793+stemoo@users.noreply.github.com>
2020-06-05 10:49:17 +0100

    Delete HNG-00027.py

--- scripts/HNG-00027.py
+++
@@ -1,9 +0,0 @@
-import json
-
-email= "momohkeshi@yahoo.com"
-output= "Hello World, this is Stephenson Momoh with HNGi7 ID HNG-00027 using Python for stage 2 task. momohkeshi@yahoo.com"
-file ='output.json'
-with open(file, 'w') as f_obj:
-  json.dump(output, f_obj)
-  print(output)
-  
