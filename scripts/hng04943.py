fd6c40661d5b07013a8bbdcbaa062cf35d2474f9
Joseph Deborah <64703725+Debbie-rinsola24@users.noreply.github.com>
2020-06-05 22:08:27 +0100

    Update hng04943.py

--- scripts/hng04943.py
+++ scripts/hng04943.py
@@ -1,4 +1,5 @@
   print("Hello World")
-  print(Name:(This is Joseph Deborah))
-  print(HNG ID[HNG 04943)
-  print(Using [PY]for stage 2 task)
+  print(Name:("This is Joseph Deborah"))
+  print("HNG ID[HNG 04943")
+  print("Using [PY]for stage 2 task")
+  Print ("Email: rinsolatiti@gmail.com")
