77b1a2580f749aa3645c1ea33edc573726bdeae7
Oluwatosin017 <66261149+Oluwatosin017@users.noreply.github.com>
2020-06-05 12:42:20 +0100

    Create hng01224.py

---
+++ scripts/hng01224.py
@@ -0,0 +1,8 @@
+name = "Oluwatosin Taiwo"
+myid = "HNG-01224"
+language = "Python"
+email = "ttvicktor1@gmail.com"
+
+result = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}"
+
+print(result.format(name, myid, language, email),flush = True)
