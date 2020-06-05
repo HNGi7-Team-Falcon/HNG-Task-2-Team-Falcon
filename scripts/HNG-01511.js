69387ada2c7ad30a596b41b4407c4c25a1f02dfd
Chinzzyy26 <66222710+Chinzzyy26@users.noreply.github.com>
2020-06-05 15:07:21 -0400

    Update HNG-01511.js
    put in email variable

--- scripts/HNG-01511.js
+++ scripts/HNG-01511.js
@@ -1,4 +1,5 @@
-const fullName = 'Helen Chinedu';
-const id = 'HNG-01511';
+const fullName = 'Chinedu Helen';
+const id = 'HNG-01511'; 
 const language = 'Javascript';
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.helenchinedu26@gmail.com`)
+const email = 'helenchinedu26@gmail.com';
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
