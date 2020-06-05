68102320929235daab036ab50220eaa824d64f92
howtobuildx <65424681+howtobuildx@users.noreply.github.com>
2020-06-05 06:26:15 +0100

    HNGi7-00047 created commit for task2

---
+++ scripts/HNG-00047.js
@@ -0,0 +1,6 @@
+let name = "Rufai",
+    id = "HNG-00047",
+    lang = "Javascript",
+    email = "howtobuildx@gmail.com";
+   
+console.log(`Hello World, this is ${name} with HNGi7 ID  ${id}  using $(lang) for stage 2 task. ${email}`)
