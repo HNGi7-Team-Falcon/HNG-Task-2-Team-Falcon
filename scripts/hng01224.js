5b42f1380028d885a5ecce9ceb8ed3ea530da1a1
Oluwatosin017 <66261149+Oluwatosin017@users.noreply.github.com>
2020-06-06 00:26:26 +0100

    Update and rename from hng 01224.py to hng 01224.js
    I changed my code from python to JavaScript

---
+++ scripts/hng01224.js
@@ -0,0 +1,11 @@
+function calling() {
+  let hngId = "HNG-02188";
+  let name = "Oluwatosin Taiwo";
+  let stack = "Javascript";
+  let email = "ttvicktor1@gmail.com";
+
+  console.log(
+    `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${stack} for stage 2 task`
+  );
+}
+calling();
