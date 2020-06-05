803359a3e7a97235211e2ce4faccd331287e6f2c
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-05 11:28:03 +0100

    reverse hng-03639

--- scripts/hng-03639.js
+++ scripts/hng-03639.js
@@ -1,12 +1,12 @@
 var bio = {
         name: 'David Ogunniran',
-        hngId: '03639',
+        hngId: 'HNG-03639',
         lang: 'Javascript',
         email: 'ayodeleogunniran@gmail.com'
     }
 
 function result() {
-    console.log ( 'Hello World, this is ' + bio.name + ' with HNGi7 ID ' + bio.hngId + ' using ' + bio.lang + ' for stage 2 task ' + bio.email)
+    console.log ( 'Hello World, this is ' + bio.name + ' with HNGi7 ID ' + bio.hngId + ' using ' + bio.lang + ' for stage 2 task. ' + bio.email)
     return true
 }
 
