de25a00460b2260092f22ea0d5b42200d4901c1e
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-06 00:30:27 +0100

    Create HNG-00467.js
    New file from Abdulmujeeb

---
+++ HNG-00467.js
@@ -0,0 +1,6 @@
+let name = "MuhammadOlodo Abdulmujeeb"
+let id = "HNG-00467"
+let language = "JavaScript"
+let email = "olodoopeyemi@gmail.com"
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
+console.log(output)
