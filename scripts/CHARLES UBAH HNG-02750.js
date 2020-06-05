2468dfcf8e8e48e39a0fa8ba7a751ccef4c41fc1
Charlie <charlesubah47@gmail.com>
2020-06-05 06:59:14 +0100

    MY TASK2

--- scripts/CHARLES UBAH HNG-02750.js
+++ scripts/CHARLES UBAH HNG-02750.js
@@ -1,12 +1,1 @@
-name = "CHARLES UBAH";
-hngId = "HNG-02750";
-language = "JavaScript";
-email = " charlesubah47@gmail.com";
-
-function hngReturn(){
-
-    output = console.log("Hello World, this is "+ name +" with HNG ID "+ hngId + " using " + language + " for stage 2 task." + email + ".");
-
-    return output;
-}
-hngReturn();
+console.log("Hello World, this is CHARLES UBAH with HNGi7 ID HNG-02750 using javascript for stage 2 task. charlesubah47@gmail.com");
