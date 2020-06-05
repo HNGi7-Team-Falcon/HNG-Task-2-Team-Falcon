9f171db220f17d052f6e40d1e6c3e932f474bcb4
Micheal Nwigwe <66284919+icecolddeveloper@users.noreply.github.com>
2020-06-05 12:14:44 +0100

    HNG-05019.py
    changes to the code

--- scripts/HNG-05019.py
+++ scripts/HNG-05019.py
@@ -1,7 +1,7 @@
-full_name = 'Nwigwe Ifeanyi Michael'
+full_name = 'Nwigwe Michael Ifeanyi'
 hng_id = 'HNG-05019'
 language = 'Python'
 email = 'rubzyg@gmail.com'
-msg = f'Hello World, this is {full_name} with HNGi7 id {hng_id} using {language} for stage 2 task. {email}'
 
-print(msg)
+output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task {email}'
+print(output)
