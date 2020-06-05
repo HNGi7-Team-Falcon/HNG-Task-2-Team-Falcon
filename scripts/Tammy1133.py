a3a6b1ba01da3fc12830d7a3af2bc2d7c69c3a32
Tammy1133 <66282873+Tammy1133@users.noreply.github.com>
2020-06-05 03:46:58 -0700

    I created a new python file in this repo which outputs my details.

---
+++ scripts/Tammy1133.py
@@ -0,0 +1,5 @@
+name = "Ajayi Oluwatamilore"
+ID = "HNG-05144"
+email = "ajayitamilore@gmail.com"
+language = "python"
+print(f'Hello world, my name is {name}, ID {ID}, email-{email}, and i used {language} to output this message.')
