ec0cdbbee2235724ad5f0415a7a752bbe94c5856
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-05 21:52:08 +0100

    Create HNG-00467.js

---
+++ scripts/HNG-00467.js
@@ -0,0 +1,5 @@
+let fullName = "Muhammad-Olodo Abdulmujeeb Opeyemi" 
+let id = "HNG-00467"
+let language = "JavaScript"
+let output = `Hello World, this is ${fullName} with hng id ${id} with language ${language} olodoopeyemi@gmail.com'
+console.log(output)
