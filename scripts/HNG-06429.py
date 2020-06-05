f2f0128f0c66ac93de9070368a8aa666b42d0098
Isaacadejuwon <65286512+Isaacadejuwon@users.noreply.github.com>
2020-06-05 12:18:21 +0100

    Delete HNG-06429.py

--- scripts/HNG-06429.py
+++
@@ -1,6 +0,0 @@
-fullname= "Isaac Adejuwon"
-HNGi7ID= "HNG-06429"
-language= "Python"
-email= "isaacadejuwon27@gmail.com"
-print ('Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. Email%s' %(fullname, HNGi7ID, language, email))
-
