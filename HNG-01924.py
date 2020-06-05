52a1aaa147e764b6924f82031152dc1546a863a4
ebony9ja <ebony9ja@gmail.com>
2020-06-05 21:09:48 +0100

    this is HNG-01924  task2 commit made with python language

---
+++ HNG-01924.py
@@ -0,0 +1,6 @@
+
+
+print ("Hello World, this is OLUKAYODE DISU with HNGi7 ID HNG-01924 using Python for stage 2 task. ebony9ja@gmail.com")
+
+
+
--- hng01924.py
+++
@@ -1,4 +0,0 @@
-
-
-print ("Hello World, this is Olukayode Disu with HNG-01924 using python for stage 2 task")
-
