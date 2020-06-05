00b4f6b5bf049b56174593d057db806f92fa154a
demigodng <38119621+demigodng@users.noreply.github.com>
2020-06-05 11:53:30 -0400

    Update and rename HNG05053.PHP to HNG05053.php

--- HNG05053.php
+++ HNG05053.php
@@ -5,5 +5,5 @@ $HND_iD = 05053;
 $language = 'php';
 $email = "topkattx@gmail.com";
 
-echo "Hello World, this is [$Full_Name] with HNGi7 ID [$HND_iD] using [$language] for stage 2 task. Email [$email]";
+echo "Hello World, this is $Full_Name with HNGi7 ID $HND_iD using $language for stage 2 task. Email $email";
 ?>
