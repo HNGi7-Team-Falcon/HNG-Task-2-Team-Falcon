bf7e37b8327437a74962f96c36e673fccaa32de8
Olufemi Odedeyi <15-52HA046@students.unilorin.edu.ng>
2020-06-05 12:22:27 +0100

    HNG-01684
    Hi this is my submission

---
+++ scripts/HNG-01684.py
@@ -0,0 +1,7 @@
+full_name = "ODEDEYI Olufemi Francis"
+hng_ID = "HNG-01684"
+my_script_lang = "Python"
+my_mail = "olufemiodedeyi@gmail.com"
+
+output_string = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}". format(full_name, hng_ID , my_script_lang, my_mail)
+print(output_string)
