671856ad619b5eb6e51f95dfd6f3d8f9cd144e2a
EstherAmara <52838618+EstherAmara@users.noreply.github.com>
2020-06-05 20:44:51 +0100

    added email, ID and full name

---
+++ scripts/hng05652.php
@@ -0,0 +1,9 @@
+<?php
+    $hngId = "HNG-05652";
+    $fullName = "Esther Ukandu";
+    $languageOfChoice = "php";
+    $emailAddress = "esthere.amara@gmail.com";
+
+    echo "Hello World, this is ".$fullName." with HNGi7 ID ".$hngId." using ".$languageOfChoice." for stage 2 task. ".$emailAddress;
+
+?>
