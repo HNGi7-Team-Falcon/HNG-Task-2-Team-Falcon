aa629f82ab3885ee70fd982ed208e5b2e8ed086f
Bolu Tejumola <tejumolabolu@gmail.com>
2020-06-05 23:14:57 +0100

    added email

--- scripts/Hng2542.js
+++ scripts/Hng2542.js
@@ -2,6 +2,7 @@ function Hng_task2() {
   const name = "Tejumola Bolu";
   const id = "HNG-2542";
   const language = "JavaScript";
+  const email = "tejumolabolu@gmail.com"
   console.log(
     "Hello World, this is" +
       " " +
@@ -15,7 +16,9 @@ function Hng_task2() {
       " " +
       language +
       " " +
-      "for stage 2 task"
+    "for stage 2 task" +
+     " " +
+    email
   );
 }
 Hng_task2();
