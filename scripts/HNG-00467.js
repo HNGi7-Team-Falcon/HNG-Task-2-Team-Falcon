27f0eacdc8fd7ce7db1c54c4b55046f72d3bd9de
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-06 00:21:03 +0100

    Delete HNG-00467.js

--- scripts/HNG-00467.js
+++
@@ -1,6 +0,0 @@
-let fullName = "Muhammad-Olodo Abdulmujeeb Opeyemi" 
-let id = "HNG-00467"
-let language = "JavaScript"
-let email= "olodoopeyemi@gmail.com"
-let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
-console.log(output)
