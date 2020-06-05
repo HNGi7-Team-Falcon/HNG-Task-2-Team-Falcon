bbd6adf53015f8ea4fe7a3d12feb717c2d7d51d3
Lawrence Ayobami <58933502+SirLaw-commits@users.noreply.github.com>
2020-06-05 09:11:55 +0100

    Update HNG-05842.py

--- scripts/HNG-05842.py
+++ scripts/HNG-05842.py
@@ -3,7 +3,7 @@ id_name = "HNG-05842"
 language_name = "Python"
 email = "ayobamilawrence@gmail.com"
 
-print("Hello World, this is {full_name} with HNGi7 ID id_name} using {language_name} for stage 2 task. {email} ")
+print(f"Hello World, this is {full_name} with HNGi7 ID {id_name} using {language_name} for stage 2 task. {email} ", flush = True)
 
 
 
