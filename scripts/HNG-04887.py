9ee266ce25f84ac9ce767fdd496a702a5af7a8ad
Elpis405 <66330854+Elpis405@users.noreply.github.com>
2020-06-05 11:06:54 +0100

    Update HNG-04887.py

--- scripts/HNG-04887.py
+++ scripts/HNG-04887.py
@@ -20,7 +20,7 @@ language = 'Python'
 
 #print section
 
-task = f'Hello World, this is {fullname} with HNGi7 ID {hng_id} using {language} for stage 2 task'
+task = f'Hello World, this is {fullname} with HNGi7 ID {hng_id} using {language} for stage 2 task. elpis405@gmail.com'
 
 print(task)
 
