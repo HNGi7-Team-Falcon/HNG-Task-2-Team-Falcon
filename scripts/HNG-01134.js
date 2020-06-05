ee432dc9b2321f3552f3c5ea163ee3cf8b74cf42
Ayoseun <48213053+Ayoseun@users.noreply.github.com>
2020-06-05 20:52:46 +0100

    HNG-01134
    need for merge

--- scripts/HNG-01134.js
+++ scripts/HNG-01134.js
@@ -2,10 +2,10 @@ theTask = () => {
     let task = {
         id: "Ayo Solomon",
         hid: "HNG-01134",
-        lang: "Javascript",
+        langu: "Javascript",
         email: "ayoseunsolomon@gmail.com",
     }
-    let needed = `Hello World, this is ${task.id} with HNGi7 ID ${task.hid} using ${task.lang} for stage 2 Task`
-    console.log(needed)
+    let needed = `Hello World, this is ${task.id} with HNGi7 ID ${task.hid} using ${task.langu} for stage 2 task.`
+    console.log(needed);
 }
 theTask()
