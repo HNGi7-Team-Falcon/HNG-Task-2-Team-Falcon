c9d9bb1b5f8bb608e12780f1bbb908eaa8e558f6
Victor Ikechukwu <61970374+vectaaa@users.noreply.github.com>
2020-06-05 04:30:53 +0100

    Update HNG-1012

--- scripts/hng1012.php
+++ scripts/hng1012.php
@@ -1,14 +1,9 @@
-
 <?php
-
 $name = "Onuchukwu Victor";
 $id = "1012";
 $email = "victoronuchukwu16@gmail.com";
 $language = "php";
 
-
 echo "Hello World, this is " . $name .  " with HNGi7 ID HNG-" 
-. $id . " using "  . $language . " for stage 2 task ";
-
-
+. $id . " using "  . $language . " for stage 2 task " . $email .;
 ?>
