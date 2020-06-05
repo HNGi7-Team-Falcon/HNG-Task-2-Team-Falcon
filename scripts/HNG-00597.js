e03813921e9d198802f37db2bfd12770e169936a
Dchriz-ops <65752550+Dchriz-ops@users.noreply.github.com>
2020-06-05 11:44:11 +0100

    Updated HNG-00597
    Loadiing

--- scripts/HNG-00597.js
+++ scripts/HNG-00597.js
@@ -8,7 +8,7 @@ const email = "christian.dirisu@gmail.com";
 
 const task = () => {
 
-    return `Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+    return `Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`
 
 };
 
