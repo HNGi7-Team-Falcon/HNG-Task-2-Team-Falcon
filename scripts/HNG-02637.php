b45278c294913efbb763b1e8ddba4bf6bab8161f
Joseph Ero <erojoseph94@gmail.com>
2020-06-05 12:14:19 +0100

    added variables

--- scripts/HNG-02637.php
+++ scripts/HNG-02637.php
@@ -1,3 +1,10 @@
 <?php
-echo 'Hello World, this is Joseph Ero with HNGi7 ID HNG-02637 using PHP for stage 2 task. erojoseph94@gmail.com';
+//declare variables
+$name = "Joseph Ero";
+$id = "HNG-02637";
+$language = "PHP";
+$email = "erojoseph94@gmail.com";
+
+//echo string with variables value
+echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task. ".$email;?>
 ?>
