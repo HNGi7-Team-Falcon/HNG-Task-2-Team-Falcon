1020d62cafa2f4c2fa0f1fe50e396259758fe7f7
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-05 11:13:43 +0100

    added new files

--- scripts/HNG-01947.php
+++ scripts/HNG-01947.php
@@ -4,6 +4,6 @@ $name = 'Oluwasegun Ibidokun';
 $id = 'HNG-01947';
 $language = 'PHP';
 
-echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task. segunibidokun@gmail.com";
+echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task";
 
 ?>
