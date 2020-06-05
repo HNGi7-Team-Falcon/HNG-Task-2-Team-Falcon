cd42bd8ef62ca9c0d0e3eb24c372e7e7cc6c461a
alishaJ-tech <66273195+alishaJ-tech@users.noreply.github.com>
2020-06-05 13:23:53 +0100

    Update HNG-00998.py

--- scripts/HNG-00998.py
+++ scripts/HNG-00998.py
@@ -1,13 +1,16 @@
-first_name = "Bolaji"
-last_name ="Alabi"
-id = "HNG-00998"
+lname = "Juma"
+fname ="Aisha"
+hng_id = "HNG-04499"
 language ="Python"
-email = "alabibolaji78@gmail.com"
+email = "alishadesua1@gmail.com"
+flush = true;
 
 
-def printData ():
-    full_name = (first_name+" "+ last_name);
-    print("Hello World, this is "+ full_name + " with HNGi7 ID " + id + " using " + language +" for stage 2 task.", email, flush=True)
-    return;
+def mydata ():
 
-printData()
+    print("Hello World, this is "+ name + " with HNGi7 ID " + hng_id + " using " + language +" for stage 2 task")
+   
+
+mydata()
+ 
+ 
