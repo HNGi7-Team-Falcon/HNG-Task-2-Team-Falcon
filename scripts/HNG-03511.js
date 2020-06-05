f5c8367254e8dd140286ff16bd21e3092fff7113
Joshm16 <65626720+Joshm16@users.noreply.github.com>
2020-06-05 11:17:42 +0100

    Changed code

--- scripts/HNG-03511.js
+++ scripts/HNG-03511.js
@@ -1,4 +1,8 @@
-var name  = 'Josh Anderson';
-var email = 'joshiandersonk69@gmail.com';
+let name  = 'Josh Anderson'
+let id = '-03511';
+let email = 'joshiandersonk69@gmail.com';
 
-console.log(`Hello World, this is ${name}, with HNGi7 iD HNG-03511 using javascript for stage 2 task, ${email}`);
+
+val  = 'Hello World, this is '+ name + ' ' + `with HNGi7 ID HNG${id} using JAVASCRIPT for stage 2 task. ${email}`;
+
+console.log(val);
