e0f27a3b8aaca02d08ab713cce4a8a610345bce4
0m0hsam <60308962+0m0hsam@users.noreply.github.com>
2020-06-05 13:20:40 +0100

    update to hng-02802-task-2

--- scripts/HNG-02502Task2.py
+++ scripts/HNG-02502Task2.py
@@ -1,13 +1,11 @@
 cmd=True
 def user(cmd):
-	comment='Program Closed thanks for checking\nPlease grade me well!'
 	if cmd :
 		name='Samuel Jonathan'
 		id='HNG-02502'
 		language='Python'
 		email='omohsam81@gmail.com'
 		print(f"Hello world, this is { name} with id {id} using {language} email {email}")
-		print(comment)	
 
 		
 user(cmd)
