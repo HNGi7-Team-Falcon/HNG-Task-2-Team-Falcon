200af6c439af9599d15772c34dd85a1c82400397
ade-damola <66269309+ade-damola@users.noreply.github.com>
2020-06-05 11:18:59 +0100

    Delete hng-04149.js

--- scripts/hng-04149.js
+++
@@ -1,7 +0,0 @@
-const fullName = "Favour Nifemi F"
-const id = "HNG-04149"
-const language = "Javascript"
-const email = "shoppernife@gmail.com"
- 
-console.log (`Hello world, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`); 
-
