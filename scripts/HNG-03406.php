092cbae49ce632907c958653b272cc3d78ad67f8
Victor Okeke <34918555+okekevicktur@users.noreply.github.com>
2020-06-06 05:26:18 -1100

    update HNG-03406.php

--- scripts/HNG-03406.php
+++ scripts/HNG-03406.php
@@ -3,12 +3,5 @@
   $id= "HNG-03406";
   $lang=  "PHP";
   $email=  "Okekevicktur@gmail.com";
-<<<<<<< HEAD
-  
-  echo "Hello World, this is  $fname with HNGi7 ID  $id using $lang for stage 2 task. $email";
-=======
-
   echo "Hello World, this is $fname with HNGi7 ID $id using $lang for stage 2 task. $email";
-
->>>>>>> submissions
 ?>
