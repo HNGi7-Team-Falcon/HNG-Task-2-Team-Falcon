746028a18e9d384c233c35a7a7d4b15db58f3bc3
Olaphoxy <54592642+olaphoxy@users.noreply.github.com>
2020-06-06 08:41:37 +0100

    Update HNG-05830.js

--- scripts/HNG-05830.js
+++ scripts/HNG-05830.js
@@ -1,21 +1,18 @@
 (
     function greeting() {
-
         let sFullName = "Olapo Akinyele";
         let sHngi7Id  = "HNG-05830";
         let sLanguage = "JavaScript";
-        let sEmail    = "horlaps96@gmail.com";
-
+    let sEmail    = "horlaps96@gmail.com"
+     
         let sGreeting = "Hello World, this is "
                       + sFullName
                       + " with HNGi7 ID "
                       + sHngi7Id
-                      + " and email "
-                      + sEmail
                       + " using "
                       + sLanguage
-                      + " for stage 2 task";
-
+                      + " for stage 2 task. "
+              + sEmail;
         console.log( sGreeting );
     }
 ) ();
