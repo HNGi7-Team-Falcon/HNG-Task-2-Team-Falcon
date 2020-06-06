fc74b1d81d4ca56b94664f0bf94db07d7a5fcc73
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-06 13:02:07 +0100

    Delete HNG-01785.js

--- scripts/HNG-01785.js
+++
@@ -1,6 +0,0 @@
-var myName = "PHILOMENA OBASI";
-    myID = "HNG-01785";
-    myLanguage = "JAVASCRIPT";
-    myEmail = "philomenaobasi04@gmail.com";
-
-console.log ("Hello World, this is "+ myName + " with HNG i7 ID " + myID + " using " + myLanguage +" for stage 2 task. " + myEmail);
