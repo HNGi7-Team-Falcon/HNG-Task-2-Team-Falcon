2081c2aa783c2db56ab35b8f4f816504e2db568d
Nelson Okoye <Okoye-Nelson@users.noreply.github.com>
2020-06-06 10:11:09 +0100

    Update hng03025.py

--- scripts/hng03025.py
+++ scripts/hng03025.py
@@ -1,1 +1,8 @@
-print("Hello World, this is Nelson Okoye with HNGi7 ID 03025 using Python for stage 2 task.")
+def hng_task2():
+    name = "Nelson Okoye"
+    identification = "HNG-03025"
+    language = "Python"
+    print("Hello World, this is " + str(name), "with HNGi7 ID " + str(identification), "using " + str(language), "for stage 2 task. nlsnokoye@gmail.com", flush=True)
+
+
+hng_task2()
