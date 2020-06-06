da27f5fbf032b01e15b8a06e91cb84496cc8836a
omeiza58 <66530200+omeiza58@users.noreply.github.com>
2020-06-07 00:26:07 +0100

    Create omeiza58.py

---
+++ omeiza58.py
@@ -0,0 +1,6 @@
+mainmessage= "Hello world"
+name="Bello Omeiza"
+hngid="06828"
+language="python"
+email="omeizabello7@gmail.com"
+print(mainmessage + " this is "+ name +","+ " with HNGi7 ID", hndid + " using "+ language + " for stage 2 task",  email)
