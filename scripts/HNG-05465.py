c06adfeebb060a4144371d1a03ff10d26ee1dc3b
moses ubah <39748749+mohzys23@users.noreply.github.com>
2020-06-06 16:47:24 +0000

    Update HNG-05465.py

--- scripts/HNG-05465.py
+++ scripts/HNG-05465.py
@@ -1,2 +1,2 @@
 full_name = "Ubah Moses Chukwuemeka"   ID = "HNG-05465"    language = "Python"    email = "ubahmohzys@gmail.com"
-   print("Hello World, this is " +full_name+" with HNGi7 ID "+ID+" using "python" for stage 2 task. "+email)
+   print("Hello World, this is " +full_name+" with HNGi7 ID "+ID+" using "+lanugage+" for stage 2 task. "+email)
