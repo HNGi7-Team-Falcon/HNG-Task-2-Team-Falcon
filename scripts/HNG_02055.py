fb846b57f1831811cfd5b7039adcd943dcdc0852
Ayotomide <babalolagbogo@gmail.com>
2020-06-06 11:52:32 +0100

    I created a python file that returns the value in the readme

---
+++ scripts/HNG_02055.py
@@ -0,0 +1,7 @@
+full_name= 'Uzoamaka Nweze'
+HNGi7_ID='HNG-02055'
+language= ' java '
+email='uzonweze@gmail.com'
+
+
+print(f'Hello World, this is {full_name} with HNGi7 ID {HNGi7_ID} using {language} for stage 2 task. {email}',flush=True)
