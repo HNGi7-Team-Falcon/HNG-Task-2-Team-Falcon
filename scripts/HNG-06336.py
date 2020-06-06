f1d6518f1ea9ddbebc15dee66474b70996310eb8
leoscribles <66364376+leoscribles@users.noreply.github.com>
2020-06-06 15:39:29 +0200

    Delete HNG-06336.py

--- scripts/HNG-06336.py
+++
@@ -1,8 +0,0 @@
-fullname = "Leonard Uyanwanne"
-id = "HNG-06336"
-language = "Python"
-email = "leoscribles@gmail.com"
-
-stuff = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}"
-
-print(stuff.format(fullname, id, language, email),flush = True)
