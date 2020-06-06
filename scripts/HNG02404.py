7195e26a87c53825ef4f933a644816ebc8e2f98a
E-wave <62189260+E-wave112@users.noreply.github.com>
2020-06-06 22:56:54 +0100

    Create HNG02404.py

---
+++ scripts/HNG02404.py
@@ -0,0 +1,5 @@
+def task(name, flush = True):
+	return ("Hello World, this is " + name + " with HNGi7 ID HNG-02404 using Python for stage 2 task iyayiemmanuel1@gmail.com")
+
+name = 'Osagie Iyayi'	
+print(task(name, flush = True))
