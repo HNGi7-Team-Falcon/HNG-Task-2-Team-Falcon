8b9a0fdba2a18473bf94331970d2249f5d07ec7f
Godson Abraham <66224805+abgodson@users.noreply.github.com>
2020-06-06 22:55:23 +0100

    Create Updated HNG02708.php

---
+++ Updated HNG02708.php
@@ -0,0 +1,9 @@
+ 
+<?php
+$name = "Abraham Godson";
+$hngID = "HNG-02708";
+$language = "PHP";
+$emailAddress = "godsonabraham8@gmail.com";
+
+ echo ("Hello World, this is $name with the HNGi7 ID $hngID using $language for stage 2 task. $emailAddress"); 
+  ?>
