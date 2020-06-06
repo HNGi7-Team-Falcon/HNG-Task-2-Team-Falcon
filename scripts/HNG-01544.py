456987feeb62bcaf8b3d0d4bcff7ed257a90388d
Samuel5432 <62008635+Samuel5432@users.noreply.github.com>
2020-06-06 02:01:05 +0100

    Updated task 2 completed
    Corrections made

--- scripts/HNG-01544.py
+++ scripts/HNG-01544.py
@@ -1,2 +1,2 @@
 
-print("Hello World, this is Adeniyi Samuel with HNGi7 ID HNG-01544 using Python For stage 2 task ")
+print("Hello World, this is Adeniyi Samuel with HNGi7 ID HNG-01544 using Python for stage 2 task")
