586605cc9eb8e7e0fd29cefeb941b32d85e8fdcd
Favour Oluwanifemi <favournifemifat@gmail.com>
2020-06-06 08:12:46 +0100

    MY updated task

--- scripts/HNG-04149.js
+++ scripts/HNG-04149.js
@@ -4,5 +4,3 @@ const language = "Javascript"
 const email = "shoppernife@gmail.com"
  
 console.log (`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task. ${email}`);
-
-
