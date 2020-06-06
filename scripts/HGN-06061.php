302caf62f00b3c643f5506ad32c66d38b907c0db
mikeuzo <michael_uzoigwe@yahoo.com>
2020-06-07 00:06:11 +0100

    Rewrote my code again

--- scripts/HGN-06061.php
+++ scripts/HGN-06061.php
@@ -1,9 +1,7 @@
 <?php
-		  $full_name = "Michael Uzoigwe";
-		  $ID = "HNG-06061";
-		  $language = "PHP" ;
-		  $txt = "michael_uzoigwe@yahoo.com";
-		  ?>
-		  <?="Hello World, this is $full_name with HNGi7 ID $ID using $language for stage 2 task";?>
-
-
+	$full_name = "Michael Uzoigwe";
+	$ID = "HNG-06061";
+	$language = "PHP" ;
+	$email = "michael_uzoigwe@yahoo.com";
+	echo "Hello World, this is ".$full_name." with HNGi7 ID". $ID." using ".$language." for stage 2 task.".$email;
+?>
