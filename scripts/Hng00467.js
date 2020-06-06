bea3fd2a98b74b8c744b4980b86c92b184aa112b
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-06 08:32:23 +0100

    Update hng00467
    New submission for hng00467 corrected errors

--- scripts/Hng00467.js
+++ scripts/Hng00467.js
@@ -1,4 +1,11 @@
-function info () {
-return ("Hello world, this is Muhammad-Olodo Abdulmujeeb Opeyemi with HNGi7 ID HNG-00467 using JavaScript for Stage 2 task. email address is olodoopeyemi@gmail.com");
-}
-console.log (info());
+var myInfo = {
+  fullName: "Muhammad\-Olodo Abdulmujeeb",
+  hngId: "HNG00467",
+  language: "JavaScript",
+  email: "olodoopeyemi@gmail.com"
+  };
+console.log('Hello this is' + myInfo.fullName + 'Hngi7 id' + myInfo.hngId + 'using' + myInfo.language + 'for stage 2 task' + myInfo.email);
+  
+
+
+
