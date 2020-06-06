24e4a39d8f862f9ff82300a1536a5a9164eed1fd
mcvoltik <49316230+mcvoltik@users.noreply.github.com>
2020-06-06 06:28:15 +0100

    Update HNG-01089.py

--- scripts/HNG-01089.py
+++ scripts/HNG-01089.py
@@ -1,7 +1,7 @@
 
 #This is my Python version of Task 2 for team-falcon
 full_name = 'Michael Ashi'
-HNDid = 'HNG-01089'
+HNGid = 'HNG-01089'
 language_type = 'python'
 email = 'ashimichaelashi@gmail.com'
-print(f'Hello World, this is {full_name} with HNGi7 ID {HNDid} using {language_type} for stage 2 task.\nMy email is: {email}', flush = True)
+print(f'Hello World, this is {full_name} with HNGi7 ID {HNGid} using {language_type} for stage 2 task. {email}', flush = True)
