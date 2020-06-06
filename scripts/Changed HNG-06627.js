932251e20961c0ce1ff5eeb7184a520a5607dc80
TeePlusPlus <57777459+TeePlusPlus@users.noreply.github.com>
2020-06-06 16:47:15 +0100

    Delete Changed HNG-06627.js

--- scripts/Changed HNG-06627.js
+++
@@ -1,5 +0,0 @@
-const fullName = 'Akande Teslim';
-const id = 'HNG-06627';
-const language = 'kotlin';
-
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`)
