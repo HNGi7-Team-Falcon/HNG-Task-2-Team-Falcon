f0624334b91006cb19d3a344e52587341ac25002
Mariam <virtuoushand@gmail.com>
2020-06-06 10:21:26 +0100

    I corrected by removing brackets and quotes

--- scripts/HNG-05887.py
+++ scripts/HNG-05887.py
@@ -1,8 +1,8 @@
-greeting = ('Hello World this is')
-fullnames = ('Mariam David-Alozie')
-id = ('with HNGi7 ID HNG-05887')
-activity = ('using python for stage 2 task.')
-email = ('virtuoushand@gmail.com')
-instruction = [ greeting, fullnames, id, activity, email] 
-print(instruction)
+greeting = 'Hello World this is'
+fullnames = 'Mariam David-Alozie'
+id = 'HNG-05887'
+language = 'python'
+activity = 'using python for stage 2 task'
+email = 'virtuoushand@gmail.com'
 
+print('Hello World this is ' +fullnames+' with HNGi7 ID '+id+' using '+language+' for stage 2 task')
