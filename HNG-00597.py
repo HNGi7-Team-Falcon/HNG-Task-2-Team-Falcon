cc769561f41cbe1dcbb595359d614657319bc303
Samuel Onyijne <samuelord1@gmail.com>
2020-06-06 13:58:45 +0100

    cleaned up

--- HNG-00597.py
+++
@@ -1,6 +0,0 @@
-hng_full_name = 'Christian Dirisu'
-hngid = 'HNG-00597'
-programming_language = 'Python'
-hng_email = 'christian.dirisu@gmail.com'
-
-print(f'Hello World, this is {hng_full_name} with HNGi7 ID {hngid} using {programming_language} for stage 2 task. {hng_email}')
