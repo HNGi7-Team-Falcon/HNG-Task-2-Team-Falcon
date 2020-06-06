a6b0c46f6e6d9ffd8aeed0717a87e80bd9c8d6df
Jesu-Oje <59731052+Jesu-Oje@users.noreply.github.com>
2020-06-06 20:36:11 +0100

    HNG-04785
    I used var to assign my variables i.e name, my email and hng id.

---
+++ scripts/HNG-04785.js
@@ -0,0 +1,5 @@
+var fullName = "Jesu-Oje Ayomipo"
+var myEmail = "ojewalefoh@outlook.com"
+var myId = "HNG-04785"
+var output = `Hello World, this is ${fullName} with HNG7 ID ${myId} using Javascript for stage 2 task sir. ${myEmail}`
+console.log(output)
