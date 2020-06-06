482e07c576aa8678dcfb053e1ec2c496b1887e63
Chelseaquin <uzonweze@gmail.com>
2020-06-06 12:24:26 +0100

    I created a python file that returns the value in the readme

--- scripts/HNG-02055.py
+++ scripts/HNG-02055.py
@@ -4,4 +4,4 @@ Fn=F+" "+L
 ID='HNG-020555'
 Lang='Java'
 Email='uzonweze@gmail.com'
-print ('Hello World, this is'+" "+Fn+" "+'with HNGi7 ID'+" "+ID+" "+'using'+" "+Lang+" "+'for stage 2 task.'+Email)
+print ('Hello World, this is'+" "+Fn+" "+'with HNGi7 ID'+" "+ID+" "+'using'+" "+Lang+" "+'for stage 2 task. '+Email)
