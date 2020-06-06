5c06f69ada06ccda6fc368b38cc95c4b03225c1c
FountainTech <40290906+opman129@users.noreply.github.com>
2020-06-06 08:20:16 +0100

    NEW HNG-04731.py
    Made necessary corrections

---
+++ scripts/Corrected HNG-04731.py
@@ -0,0 +1,7 @@
+
+full_name = 'Jokotagba Opemipo Adesegun'
+hng_id = 'HNG-00056'
+language = 'python'
+email = 'opemipojokotagba@gmail.com'
+
+print('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. {}'.format(full_name, hng_id, language, email))
