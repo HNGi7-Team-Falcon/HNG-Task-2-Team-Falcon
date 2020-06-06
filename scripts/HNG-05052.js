6a37ca0119acbfe16b31e45bd32d3f252dfde5fd
Ahmedtijani Umar <ahmedtijaniumar@gmail.com>
2020-06-06 03:15:35 +0100

    Added script with name id and email

--- scripts/HNG-05052.js
+++ scripts/HNG-05052.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is AhmedTijani Umar with HNGi7 ID HNG-05053 using JavaSccript for stage 2 task. Ahmedtijaniumar@gmail.com")
+console.log("Hello World, this is AhmedTijani Umar with HNGi7 ID HNG-05052 using JavaSccript for stage 2 task. Ahmedtijaniumar@gmail.com")
