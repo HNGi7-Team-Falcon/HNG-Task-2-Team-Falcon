86c243f029f9409562d089159c17c53cc91c19d4
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-06 22:10:55 +0100

    Update HNG00467.js
    Added World and inserted full stop after task

--- scripts/HNG00467.js
+++ scripts/HNG00467.js
@@ -4,4 +4,4 @@ let myInfo = {
   language: "JavaScript",
   email: "olodoopeyemi@gmail.com"
   };
-console.log(' Hello this is ' + myInfo.fullName + ' Hngi7 id ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task ' + myInfo.email);
+console.log(' Hello World, this is ' + myInfo.fullName + ' Hngi7 id ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
