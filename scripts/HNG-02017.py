9f21ae76de0566b197969ee53ea05e9eff3ac4d9
Emmanuella Fatolu <61954156+oceanuella2020@users.noreply.github.com>
2020-06-06 10:59:48 +0100

    Update HNG-02017.py

--- scripts/HNG-02017.py
+++ scripts/HNG-02017.py
@@ -1,1 +1,5 @@
-print("Hello World, this is Fatolu Emmanuella with HNGi7 ID HNG-02017 using Python for stage 2 task. fatoluemmanuella@gmail.com")
+full_name = 'Fatolu Emmanuella'
+HNDid = 'HNG-02017'
+language_type = 'python'
+email = str('fatoluemmanuella@gmail.com')
+print(f'Hello World, this is {full_name} with HNGi7 ID {HNDid} using {language_type} for stage 2 task. {email}', flush = True)
