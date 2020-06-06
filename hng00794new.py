cffb0b07730c13614ff750cea9a04717ecc49747
francis144 <61960582+francis144@users.noreply.github.com>
2020-06-06 14:31:14 +0100

    Adding script to submission

---
+++ hng00794new.py
@@ -0,0 +1,5 @@
+hng_full_name = 'Francis Udoh'
+hngid = 'HNG-00794'
+programming_language = 'Python'
+hng_email = 'saint4frank@gmail.com'
+print(f'Hello World, this is {hng_full_name} with HNGi7 ID {hngid} using {programming_language} for stage 2 task. {hng_email}')
