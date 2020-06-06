37340ebeb013fa63424aeb0963207151021e3e55
Ahmedtijani Umar <ahmedtijaniumar@gmail.com>
2020-06-06 11:19:52 +0100

    Changed name id and email to variables

--- scripts/HNG-05052.js
+++ scripts/HNG-05052.js
@@ -1,1 +1,8 @@
-console.log("Hello World, this is AhmedTijani Umar with HNGi7 ID HNG-05052 using JavaSccript for stage 2 task. Ahmedtijaniumar@gmail.com")
+const Name = "Ahmedtijani Umar";
+const ID = "HNG-05052";
+const Language = "JavaScript";
+const Email = "Ahmedtijaniumar@gmail.com";
+
+console.log(
+  `Hello World, this is ${Name} with HNGi7 ID ${ID} using ${Language} for stage 2 task. ${Email}`
+);
