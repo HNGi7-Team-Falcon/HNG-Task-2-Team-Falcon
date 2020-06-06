3101e80965f3b342c069548142b60abd3ad512af
Peter Amos <66332396+fattpete@users.noreply.github.com>
2020-06-06 16:35:16 +0100

    Update and rename HNGi7-05665 to HNGi7-05665.py
    Testing

--- scripts/HNGi7-05665.php
+++
@@ -1,7 +0,0 @@
-"file" :"file2. php", 
-"output" : "Hello World, this is [name] with HNGi7 [ID] using [language] for stage 2 task" email, 
-"name" : "Peter Amos", 
-"email" : "fattpete6@gmail.com", 
-"Id": "HNGI7 05665", 
-"language" : "PHP", 
-
