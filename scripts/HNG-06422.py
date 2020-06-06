097607985485f06274d76eb7b4efcffcbdd7ab63
kingnoel <66368728+kingnoel@users.noreply.github.com>
2020-06-06 21:12:07 +0100

    Create HNG-06422.py

---
+++ scripts/HNG-06422.py
@@ -0,0 +1,5 @@
+full_name = "Edoh King Immanuel"
+ID = "HNG-06422"
+language = "Python"
+email = "kingno3l@icloud.com"
+print("Hello World, this is" +full_name+" with HNGi7 ID "+ID+" using "+language+" for stage 2 task. "+email)
