aa2baae15b1bf343dd5d66a0397910bb102e162d
Mariam Omohsomi David-Alozie <51507155+virtuoushands@users.noreply.github.com>
2020-06-06 22:21:19 +0100

    Update HNG-05887.py

--- scripts/HNG-05887.py
+++ scripts/HNG-05887.py
@@ -5,4 +5,4 @@ language = 'Python'
 activity = 'using python for stage 2 task'
 email = 'virtuoushand@gmail.com'
 
-print('Hello World, this is ' +fullnames+' with HNGi7 ID '+ids+' using '+language+' for stage 2 task')
+print('Hello World, this is ' +fullnames+' with HNGi7 ID '+ids+' using '+language+' for stage 2 task' +email)
