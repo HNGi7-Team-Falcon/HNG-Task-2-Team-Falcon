7e57b0c269b6ff80057c8e9e00d8bea912775ccb
Tess-hacker <64714004+Tess-hacker@users.noreply.github.com>
2020-06-06 16:49:59 +0100

    Removed spaces
    Let this work please.

--- scripts/HNG-02564.py
+++ scripts/HNG-02564.py
@@ -1,2 +1,2 @@
-greeting = "Hello World,this is Fadele Boluwatife Theresa with HNGi7 ID HNG-02654 using Python for stage 2 tasktheresafadele@gmail.com "
+greeting = "Hello World, this is Fadele Boluwatife Theresa with HNGi7 ID HNG-02654 using python for stage 2 task theresafadele@gmail.com "
 print (greeting)
