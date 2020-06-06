cd39d96825c0c9e56caf255af65f0a016bb2439e
Tess-hacker <64714004+Tess-hacker@users.noreply.github.com>
2020-06-06 17:40:00 +0100

    With the correct formatting
    Please merge. This should definitely work.

---
+++ scripts/HNG-02564.py
@@ -0,0 +1,4 @@
+name = 'Fadele Boluwatife Theresa'
+id = 'HNG-02564'
+email = 'theresafadele@gmail.com'
+print ("Hello World, this is" ,name, "with HNGi7 ID" ,id, "using python for the stage 2 task", email)
