a3fb2424fe63e81c858cb07bc21e174d17f27408
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-06 19:19:40 +0100

    Revert "Gabriel son patch 5"

--- scripts/HNG-01154.js
+++ scripts/HNG-01154.js
@@ -1,2 +1,5 @@
+<<<<<<< HEAD
+console.log("Hello World,this is ADEBOGUN AYOMIDE with HNGi7 ID HNG-01154 using javascript for stage 2 task. ayomideadebogun@gmail.com")
+=======
 console.log("Hello World,this is ADEBOGUN AYOMIDE with HNGi7 ID HNG-01154 using javascript for stage 2 task ayomideadebogun@gmail.com")
-
+>>>>>>> submissions
