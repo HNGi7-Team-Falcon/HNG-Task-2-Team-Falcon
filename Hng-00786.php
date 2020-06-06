09beefb7f15809634a52a6c62309efc976d150cd
Samuel Onyijne <samuelord1@gmail.com>
2020-06-06 09:39:04 +0100

    cleaned

--- Hng-00786.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Hello World, this is Anigbogu Chioma with HNGi7 ID HNG-00786 using PHP for stage 2 task. prisca$luv2@gmail.com";
-?>
--- hng00881.py
+++
@@ -1,1 +0,0 @@
-print("Hello World, this is Agboso Chinedu Kelvin with HNGi7 ID HNG-00881 using Python for stage 2 task. talisman007.ka@gmail.com")
