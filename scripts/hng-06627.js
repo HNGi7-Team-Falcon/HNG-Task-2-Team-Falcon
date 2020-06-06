7ae49978601b299e244a2f31d54b3dd6a3a753f8
Bakis-world <65019521+Bakis-world@users.noreply.github.com>
2020-06-06 17:32:53 +0100

    Delete hng-06627.js

--- scripts/hng-06627.js
+++
@@ -1,7 +0,0 @@
-let Full_Name ="Akande Teslim";
-let HNG_ID="HNG-06627";
-let language="kotlin"
-let email="teslimakande@gmail.com"
-
-console.log("Hello World, this is ",`${Full_Name}`,"with HNGi7 ID",`${HNG_ID}`,"using",`${language}`,"for stage 2 task.",`${email}`);
-
