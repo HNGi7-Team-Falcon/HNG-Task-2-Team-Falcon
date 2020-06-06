57379db3fedeea4199142cf86a0c91d005c46342
Samuel5432 <62008635+Samuel5432@users.noreply.github.com>
2020-06-06 11:23:24 +0100

    Update HNG-01544.py

--- scripts/HNG-01544.py
+++ scripts/HNG-01544.py
@@ -1,1 +1,5 @@
-print ("Hello World, this is Adeniyi Samuel with HNGi7 ID HNG-01544 using Python For stage 2 task sa7259379@gmail.com ")
+full_Nme = "Adeniyi Samuel"
+HNDid = "HNG-01544"
+lang = "python"
+email = "sa7259379@gmail.com"
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s" %(full_Nme, HNDid, lang, email))
