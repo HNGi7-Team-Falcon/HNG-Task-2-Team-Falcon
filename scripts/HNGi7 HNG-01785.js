dfdb7ee0c495ca5bd2b9d8c3179080996b7128ed
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-06 13:04:15 +0100

    Delete HNGi7 HNG-01785.js

--- scripts/HNGi7 HNG-01785.js
+++
@@ -1,6 +0,0 @@
-var myName = "PHILOMENA OBASI";
-    myID = "HNG-01785";
-    myLanguage = "JAVASCRIPT";
-    myEmail = "philomenaobasi04@gmail.com";
-
-console.log ("Hello World, this is "+ myName + " with HNGi7 ID " + myID + " using " + myLanguage +" for stage 2 task. " + myEmail);
