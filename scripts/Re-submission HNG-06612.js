3510c47baa4872267f316bad62999266e72a8012
Bakis-world <65019521+Bakis-world@users.noreply.github.com>
2020-06-06 17:07:58 +0100

    Delete Re-submission HNG-06612.js

--- scripts/Re-submission HNG-06612.js
+++
@@ -1,4 +0,0 @@
-var name="Bakare Rilwan";
-var id=" HNG-06612";
-var lang="Javascript";
-console.log("Hello world, this is " + name + " with HNGi7 ID " + id + " using " + lang + "for stage 2 task.");   
