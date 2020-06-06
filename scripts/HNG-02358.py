c52bbb1848b310627f7b0b07aa22b4fffdb8f771
faithofomukoro <66223884+faithofomukoro@users.noreply.github.com>
2020-06-06 21:50:11 +0100

    Update HNG-02358.py

--- scripts/HNG-02358.py
+++ scripts/HNG-02358.py
@@ -1,2 +1,1 @@
-
-print("Hello World, this is Faith Ofomukoro with HNGi7 ID HNG-02358 and using python for stage 2 task. princessobasohan2@gmail.com")
+print("Hello World, this is Faith Ofomukoro with HNGi7 ID HNG-02358 using Python for stage 2 task. princessobasohan2@gmail.com")
