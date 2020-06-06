000eee4724ff587ddc4f6f80ea40e5b364687ad4
TeePlusPlus <57777459+TeePlusPlus@users.noreply.github.com>
2020-06-06 16:46:07 +0100

    Delete Change HNG-06612.js

--- scripts/Change HNG-06612.js
+++
@@ -1,5 +0,0 @@
-var name = 'Bakare Rilwan'
-var id = 'HNG-06612'
-var lang = 'JavaScript'
-var email = 'bakis4life@gmail.com'
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.`)
