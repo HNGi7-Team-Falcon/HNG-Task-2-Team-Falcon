46202c046a4c06358f45075bac716079c5548cb7
Oyebanji Oyinkansola <oyebanjiioyinkansola@gmail.com>
2020-06-06 15:36:57 -0700

    Update HNG-00775.js
    updated version

--- HNG-00775.js
+++ HNG-00775.js
@@ -2,4 +2,4 @@ var fullName = "Oyebanji Oyinkansola";
 var id = "HNG-00775";
 var email = "oyebanjiioyinkansola@gmail.com";
 var langType = "Javascript";
-console.log("Hello World, this is " + fullName + " with HNGi7 ID " + id+ " using " + langType + " for stage 2 task. " + email);
+console.log("Hello World, this is " + fullName + " with HNGi7 ID " + id+ " using " + langType + " for stage 2 task.  " + email);
