313ca1b1b6b41bbe5ed5e916032aa486cf86d1f5
Samuel Onyijne <samuelord1@gmail.com>
2020-06-06 02:04:09 +0100

    cleaned

--- HNG-05373.py
+++
@@ -1,8 +0,0 @@
-Fullname = 'CHUKWUNTA, Ernest Chukwuebuka'
-ID = 'HNG-05375'
-P_lang = 'Python'
-E_Mail : 'ernestchukwunta@yahoo.com'
-    
-def hng_task2(Fullname, ID, P_lang):
-    return f"Hello World, this is {Fullname} with HNGi7 ID {ID} using {P_lang} for stage 2 task. {E_Mail}"
-print(hng_task(Fullname, ID, P_lang), flush_True)
