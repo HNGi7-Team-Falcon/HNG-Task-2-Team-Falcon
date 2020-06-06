3871bf4fadf94f2954ce1d35b3d878c0ce8a5ce5
miracleosigwe <60506880+miracleosigwe@users.noreply.github.com>
2020-06-06 00:15:06 -0700

    Update HNG-05624.py

--- scripts/HNG-05624.py
+++ scripts/HNG-05624.py
@@ -1,5 +1,5 @@
-name= Miracle Osigwe
-id = HNG-05624
-lang= Python
-email = jomacconcept@email.com
+name= "Miracle Osigwe"
+id = "HNG-05624"
+lang= "Python"
+email = "jomacconcept@email.com"
 print("Hello World, this is {} with HGNi7 ID {) using {} for stage 2 task. {}".format(name, id, lang, email))
