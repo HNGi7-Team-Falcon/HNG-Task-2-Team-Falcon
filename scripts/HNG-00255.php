37d520f7a8da506b1a95508c7345e464230c9af1
V33ee <50486062+V33ee@users.noreply.github.com>
2020-06-07 09:20:38 +0100

    Update HNG-00255.php

--- scripts/HNG-00255.php
+++ scripts/HNG-00255.php
@@ -5,7 +5,7 @@
        $myLanguage = "PHP";
        $myEmail = "iam.vee01@gmail.com";
    
-       $myOutput = "Hello World, this is $myName with HNGi7 ID $myId using $myLanguage for stage 2 task. $myEmail ";
+       $myOutput = "Hello World, this is $myName with HNGi7 ID $myId using $myLanguage for stage 2 task. $myEmail";
        echo $myOutput;
 ?>
 
