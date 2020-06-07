90192a27fa56243da9aff8c883f0df8f88d4d33a
obarorachael <66247542+obarorachael@users.noreply.github.com>
2020-06-07 15:50:45 +0100

    HNGi7_03817
    A program to return a string

--- scripts/HNG_03817.py
+++ scripts/HNG_03817.py
@@ -1,9 +1,7 @@
-
 full_name = 'Obaro Rachael'
 hng_id = 'HNG-03817'
 language = 'python'
 email = 'email='obaro.rachaels@gmail.com'
-
 
 
 print('Hello World, this is {full_name} with HNGi7 ID {HNGi7_ID} using {language} for stage 2 task. {email}'.format(full_name, hng_id, language, email))
