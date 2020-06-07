0a8a716c67a9e4a2d126f30d63976503f195b764
brpo01 <47898882+brpo01@users.noreply.github.com>
2020-06-07 11:52:16 +0100

    Revert "Please help me merge"

--- scripts/HNG-02564.py
+++ scripts/HNG-02564.py
@@ -1,5 +1,4 @@
 name = 'Fadele Boluwatife Theresa'
 id = 'HNG-02564'
 email = 'theresafadele@gmail.com'
-language = 'python'
-print ("Hello World, this is" ,name, "with HNGi7 ID" ,id, "using" ,language, "for the stage 2 task. ",email)
+print ("Hello World, this is" ,name, "with HNGi7 ID" ,id, "using python for the stage 2 task", email)
