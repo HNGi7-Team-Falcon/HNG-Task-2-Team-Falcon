c644650d964c5fb5623a2d1974d0c7a565bba301
abdurrahman911 <66038921+abdurrahman911@users.noreply.github.com>
2020-06-07 13:39:33 +0100

    Update HNG-01668.js

--- scripts/HNG-01668.js
+++ scripts/HNG-01668.js
@@ -1,2 +1,2 @@
-var project = "Hello World, this is Abdurrahman Musa with HNGi7 ID HNG-01668 using Javascript for stage 2 task";
+var project = "Hello World, this is Abdurrahman Musa with HNGi7 ID HNG-01668 using Javascript for stage 2 task. abdurrahmanmusa911@gmail.com";
  console.log(project);
