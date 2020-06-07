ea880c57b06b3bbfc9d33a549e40561451c61647
vidiems <victory_emueje@yahoo.com>
2020-06-07 14:08:47 +0100

    update

---
+++ scripts/vidiems-HNG-06461.js
@@ -0,0 +1,5 @@
+
+function myFunction(myName, id, myLanguage, myEmail) {
+  return `Hello World, this is ${myName} with ${id} using ${myLanguage} for stage 2 task.${myEmail}`;
+}
+console.log (myFunction("Victory Emueje", "HNG-06461", "Javascript", "victory_emueje@yahoo.com"));
