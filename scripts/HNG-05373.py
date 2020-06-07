4fcd965169d185f85e5e535653a3df74226b24f5
nest2chuks <47100984+nest2chuks@users.noreply.github.com>
2020-06-07 17:30:35 +0100

    Re-Submission

--- scripts/HNG-05373.py
+++ scripts/HNG-05373.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-def hng_task2(Fullname, ID, P_Lang, E_Mail):
-
-    Fullname = "CHUKWUNTA, Ernest Chukwuebuka"
-    ID = "HNG-05375"
-    P_Lang = "Python"
-    E_Mail : "ernestchukwunta@yahoo.com"
-
-    print("Hello World, this is {Fullname} with HNGi7 ID {ID} using {P_Lang} for stage 2 task. {E_Mail}", flush = True)
-=======
-
-
 Fullname = "CHUKWUNTA, Ernest Chukwuebuka"
 ID = "HNG-05375"
 P_Lang = "Python"
@@ -18,4 +6,3 @@ E_Mail = "ernestchukwunta@yahoo.com"
 def hng_task2(Fullname, ID, P_Lang, E_Mail):
     return f"Hello World, this is {Fullname} with HNGi7 ID {ID} using {P_Lang} for stage 2 task. {E_Mail}"
 print(hng_task2(Fullname, ID, P_Lang), flush=True)
->>>>>>> submissions
