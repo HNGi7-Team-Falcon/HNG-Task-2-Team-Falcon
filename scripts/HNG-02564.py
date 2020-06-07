45b83d280162f61a92bf5a544462bc7216b3fe32
Tess-hacker <64714004+Tess-hacker@users.noreply.github.com>
2020-06-07 11:47:26 +0100

    Please help me merge
    And it has to work please.

--- scripts/HNG-02564.py
+++ scripts/HNG-02564.py
@@ -1,4 +1,5 @@
 name = 'Fadele Boluwatife Theresa'
 id = 'HNG-02564'
 email = 'theresafadele@gmail.com'
-print ("Hello World, this is" ,name, "with HNGi7 ID" ,id, "using python for the stage 2 task", email)
+language = 'python'
+print ("Hello World, this is" ,name, "with HNGi7 ID" ,id, "using" ,language, "for the stage 2 task. ",email)
