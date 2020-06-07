c3e029e0abc9594a62504148c8b6062545148836
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-07 12:21:05 +0100

    Update HNG-00467.js
    Removed space before parentheses

--- scripts/HNG-00467.js
+++ scripts/HNG-00467.js
@@ -3,5 +3,5 @@ let myInfo = {
   hngId: "HNG-00467",
   language: "JavaScript",
   email: "olodoopeyemi@gmail.com"
-  };
+};
 console.log('Hello World, this is ' + myInfo.fullName + ' with HNGi7 ID ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
