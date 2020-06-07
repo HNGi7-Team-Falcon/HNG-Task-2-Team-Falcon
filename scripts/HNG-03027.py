02a58a0b1550474bcd9a1aa2ead34a720a4466da
Eze-Okoli <51920587+Eze-Okoli@users.noreply.github.com>
2020-06-07 14:39:06 +0100

    Update HNG-03027.py
    Using Python for stage 2

--- scripts/HNG-03027.py
+++ scripts/HNG-03027.py
@@ -1,1 +1,5 @@
-Print("Hello World, this is Ezenwa Okoli with HNGi7 ID 03027 using python for stage 2 task. eze_okoli@yahoo.com")
+Fullname='Ezenwa Okoli';
+ID='HNG-03027';
+language='python' ;
+email='eze_okoli@yahoo.com';
+print ('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task.{}'.format(Fullname,ID,language,email))
