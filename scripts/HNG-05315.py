ac0bcd774a42a6e4eaedf73a9760cdefc69f1d92
Daniel.fasuba <66290818+Danielfasuba@users.noreply.github.com>
2020-06-07 12:02:09 +0100

    HNG-05315
    updated task 2

--- scripts/HNG-05315.py
+++ scripts/HNG-05315.py
@@ -3,6 +3,6 @@ hng_id = '05315'
 language = 'python'
 email = 'danielfasuba@gmail.com'
 
-string = f"Hello World, this is {full_name} with HNGi7 ID HNG-{hng_id} using python for stage 2 task "
+string = f"Hello World, this is {full_name} with HNGi7 ID HNG-{hng_id} using python for stage 2 task. {email}"
 
 print(string)
