12bd2de821255ccbebb8b1c3a2bf01ddff7a7cf9
Kelechiwisdom <66391630+Kelechiwisdom@users.noreply.github.com>
2020-06-07 23:46:19 +0100

    Create Kelechi.js

---
+++ scripts/Kelechi.js
@@ -0,0 +1,6 @@
+firstName = '[Kelechi]'
+lastName = '[Wisdom]'
+userId = '[HNG-06628]'
+language = '[Python]'
+email = 'kelechiwisdomc@gmail.com'
+print('Hello World, this is ' + firstName +' ' + lastName + ' with HNGi7 ID ' + userId + ' using ' + language + ' for stage 2 ' + email)
