5d0de3265c3b14dfcad2423494343251cf387c16
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-07 01:09:36 +0100

    Update HNG-00467.js
    Changed id to ID

--- scripts/HNG-00467.js
+++ scripts/HNG-00467.js
@@ -4,4 +4,4 @@ let myInfo = {
   language: "JavaScript",
   email: "olodoopeyemi@gmail.com"
   };
-console.log(' Hello World, this is ' + myInfo.fullName + ' with HNGi7 id ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
+console.log(' Hello World, this is ' + myInfo.fullName + ' with HNGi7 ID ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
