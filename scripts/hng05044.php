e84c7481a339e2e1a68f4a19f0076fcff7e60872
dignah95 <34247232+dignah95@users.noreply.github.com>
2020-06-07 11:46:06 -0700

    Update hng05044.php

--- scripts/hng05044.php
+++ scripts/hng05044.php
@@ -5,6 +5,6 @@ $language = "php";
 
 
 
-echo"Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task.";
+echo"Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task";
 
 ?>
