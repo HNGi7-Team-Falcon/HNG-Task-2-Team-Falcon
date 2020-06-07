0d11a5fa4c1d44af1e16ae91d14fe41c252834e9
Basit <basitajao96@gmail.com>
2020-06-07 14:43:15 +0100

    updated file

--- scripts/HNG-03917.js
+++ scripts/HNG-03917.js
@@ -2,6 +2,4 @@ const fullName = "Basit Ajao";
 const hngId = "HNG-03917";
 const language = "javascript";
 
-(function () {
-    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`) 
-})()
+    console.log("Hello World, this is " + fullName + " with HNGi7 ID " + hngId + " using " + language +" for stage 2 task") 
