81983b33e9da23bc8cc4c4d4ab9a717887a0db47
Olodocodes <62020561+Olodocodes@users.noreply.github.com>
2020-06-07 12:12:36 +0100

    Update HNG-00467.js

--- scripts/HNG-00467.js
+++ scripts/HNG-00467.js
@@ -1,7 +1,7 @@
 let myInfo = {
-  fullName: "Muhammad\-Olodo Abdulmujeeb",
+  fullName: "Muhammad Olodo Abdulmujeeb",
   hngId: "HNG-00467",
   language: "JavaScript",
   email: "olodoopeyemi@gmail.com"
   };
-console.log(' Hello World, this is ' + myInfo.fullName + ' with HNGi7 ID ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
+console.log('Hello World, this is ' + myInfo.fullName + ' with HNGi7 ID ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task. ' + myInfo.email);
