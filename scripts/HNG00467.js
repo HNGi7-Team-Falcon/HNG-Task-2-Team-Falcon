68685957a517ade6c64bb99e9a5580b928578387
JoeEdoh <josephedoh77@gmail.com>
2020-06-07 01:17:45 +0100

    Delete HNG00467.js

--- scripts/HNG00467.js
+++
@@ -1,7 +0,0 @@
-let myInfo = {
-  fullName: "Muhammad\-Olodo Abdulmujeeb",
-  hngId: "HNG00467",
-  language: "JavaScript",
-  email: "olodoopeyemi@gmail.com"
-  };
-console.log(' Hello this is ' + myInfo.fullName + ' Hngi7 id ' + myInfo.hngId + ' using ' + myInfo.language + ' for stage 2 task ' + myInfo.email);
