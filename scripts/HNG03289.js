1fee8a48bf01e8c84fe13be84a534fb10c9745ea
chi-design <66226701+chi-design@users.noreply.github.com>
2020-06-07 07:15:57 +0100

    Create HNG03289.js

---
+++ scripts/HNG03289.js
@@ -0,0 +1,5 @@
+let name= "Chioma Ifeanyichukwu"
+let internshipId= "HNG-03289"
+let language= "JavaScript"
+let email= "ifeanyichukwuchioma3@gmail.com"
+console.log("Hello World " + "this is " + name + " with " + internshipId + " using " + language + " and email " + email + " for stage 2 task.") 
