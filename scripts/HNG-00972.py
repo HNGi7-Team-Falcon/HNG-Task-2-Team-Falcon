7dd16fb2ccc9570953c17b55c66366513e6524b7
ChizurumNwachukwu <49405248+ChizurumNwachukwu@users.noreply.github.com>
2020-06-08 08:02:49 +0100

    Update HNG-00972.py again

--- scripts/HNG-00972.py
+++ scripts/HNG-00972.py
@@ -1,2 +1,7 @@
-print ('Hello World, this is Chizurum Nwachukwu with HNGi7 ID HNG-00972 using Python for stage 2 task. chizurumn@gmail.com')
+name = "Chizurum Nwachukwu"
+id = "HNG-00972"
+language = "python"
+email = "chizurumn@gmail.com"
 
+
+print("Hello World, this is" ,name, "with HNGi7 ID" ,id ,"using", language," for stage 2 task." ,email)
