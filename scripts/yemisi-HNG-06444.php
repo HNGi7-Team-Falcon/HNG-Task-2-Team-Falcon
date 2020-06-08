8edfab07452b0761a502ec484ce5f2d7fc959d4d
yemeec <56555943+yemeec@users.noreply.github.com>
2020-06-08 01:45:22 +0100

    Update yemisi-HNG-06444.php

--- scripts/yemisi-HNG-06444.php
+++ scripts/yemisi-HNG-06444.php
@@ -5,7 +5,7 @@ $firstname = 'Oluwayemisi';
 $id = 'HNG-06444';
 $email ='nyaakuyemisi@gmail.com';
 $language = 'PHP';
-$word = 'Hello World, this is '.$lastname .' '.$firstname .' with HNGi7 ID '.$id.' using '.$language.' for stage 2 task';
+$word = 'Hello World, this is '.$lastname.' '.$firstname.' with HNGi7 ID '.$id.' using '.$language.' for stage 2 task';
 
 echo $word;
 
