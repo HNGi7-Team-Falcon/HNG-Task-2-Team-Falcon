e74e0203903773057910d607342d9b24d4973db9
Benedict <bmsteve96@gmail.com>
2020-06-08 11:50:18 +0300

    resubmitting task 2

--- scripts/HNG-05062.js
+++ scripts/HNG-05062.js
@@ -2,7 +2,7 @@
 
 const introduction = (fullName, ID, language, email) => {
   let text = console.log(
-    `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${email} `
+    `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. `
   );
   return text;
 };
