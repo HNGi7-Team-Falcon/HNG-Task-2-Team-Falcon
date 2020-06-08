394378d5a5f741f036bb06c034696e77ccb16935
mutiulahi <42714976+mutiulahi@users.noreply.github.com>
2020-06-08 05:07:25 -0700

    hng06468.php
    updated task 2

--- scripts/hng06468.php
+++ scripts/hng06468.php
@@ -1,8 +1,7 @@
 <?php
   	$HNG_ID = 'HNG-06468';
 	$Fullname = 'Mutiulahi Tesleem Olamilekan';
-	$Email = 'tesleemolamilekan902@gmail.com';
 	$Language = 'PHP';
-	echo "Hello World, this is [$Fullname] with HNGi7 ID [$HNG_ID] using [$Language] and email [$Email] for stage 2 task.";
+	echo "Hello World, this is ".$Fullname." with HNGi7 ID ".$HNG_ID." using ".$Language." for stage 2 task.";
 
 ?>
