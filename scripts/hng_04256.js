6488a86512335a3827106d5d8278bb68d27e365b
devphraseX <wilsonace87@gmail.com>
2020-06-08 04:55:35 +0100

    task2 for hngi7

---
+++ scripts/hng_04256.js
@@ -0,0 +1,11 @@
+const fullName = 'Lawal Ayomide';
+const id = 'HNG-04256';
+const language = 'JavaScript';
+const email = 'wilsonace87@gmail.com';
+
+const output = (fullName, id, language, email) => {
+  const updateTask = `Hello World!, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`;
+   
+  console.log(updateTask);
+};
+output(fullName, id, language, email);
